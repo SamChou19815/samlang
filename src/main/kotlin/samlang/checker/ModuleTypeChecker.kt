c149568239451e41dfee1faa5d670ab545d9d4a5
Sam <sam@developersam.com>
2019-07-09 23:49:32 -0700

    [refactor] Separate program and module type checking

---
+++ src/main/kotlin/samlang/checker/ModuleTypeChecker.kt
@@ -0,0 +1,174 @@
+package samlang.checker
+
+import samlang.ast.Expression
+import samlang.ast.Module
+import samlang.ast.Range
+import samlang.ast.Type
+import samlang.errors.CollisionError
+import samlang.errors.IllegalMethodDefinitionError
+
+internal fun Module.typeCheck(
+    errorCollector: ErrorCollector,
+    typeCheckingContext: TypeCheckingContext
+): Pair<Module?, TypeCheckingContext> {
+    val (_, moduleNameRange, moduleName, moduleTypeDefinition, moduleMembers) = this
+    val (checkedTypeDef, partialContext) = errorCollector.returnNullOnCollectedError {
+        createContextWithCurrentModuleDefOnly(
+            moduleNameRange = moduleNameRange,
+            moduleName = moduleName,
+            moduleTypeDefinition = moduleTypeDefinition,
+            errorCollector = errorCollector,
+            typeCheckingContext = typeCheckingContext
+        )
+    } ?: return null to typeCheckingContext
+    val (fullContext, partiallyCheckedMembers) = errorCollector.returnNullOnCollectedError {
+        processCurrentContextWithMembersAndMethods(
+            moduleRange = moduleNameRange,
+            moduleName = moduleName,
+            moduleMembers = moduleMembers,
+            isUtilModule = moduleTypeDefinition == null,
+            typeCheckingContext = partialContext
+        )
+    } ?: return null to typeCheckingContext
+    val checkedModule = this.copy(
+        typeDefinition = checkedTypeDef,
+        members = partiallyCheckedMembers.map { memberDefinition ->
+            typeCheckMember(
+                member = memberDefinition,
+                errorCollector = errorCollector,
+                typeCheckingContext = fullContext
+            )
+        }
+    )
+    return checkedModule to fullContext
+}
+
+private fun Collection<String>.checkNameCollision(range: Range) {
+    val nameSet = hashSetOf<String>()
+    forEach { name ->
+        if (!nameSet.add(element = name)) {
+            throw CollisionError(collidedName = name, range = range)
+        }
+    }
+}
+
+private fun createContextWithCurrentModuleDefOnly(
+    moduleNameRange: Range,
+    moduleName: String,
+    moduleTypeDefinition: Module.TypeDefinition?,
+    errorCollector: ErrorCollector,
+    typeCheckingContext: TypeCheckingContext
+): Pair<Module.TypeDefinition?, TypeCheckingContext> {
+    // new context with type def but empty members and extensions
+    return if (moduleTypeDefinition == null) {
+        null to typeCheckingContext.addNewEmptyUtilModule(name = moduleName, nameRange = moduleNameRange)
+    } else {
+        val range = moduleTypeDefinition.range
+        val typeParameters = moduleTypeDefinition.typeParameters
+        val (isObject, mappings) = when (moduleTypeDefinition) {
+            is Module.TypeDefinition.ObjectType -> {
+                true to moduleTypeDefinition.mappings
+            }
+            is Module.TypeDefinition.VariantType -> {
+                false to moduleTypeDefinition.mappings
+            }
+        }
+        // check name collisions
+        errorCollector.returnNullOnCollectedError {
+            typeParameters?.checkNameCollision(range = range)
+            mappings.keys.checkNameCollision(range = range)
+            Unit
+        } ?: return null to typeCheckingContext
+        // create checked type def based on a temp
+        typeCheckingContext.addNewModuleTypeDefinition(
+            name = moduleName,
+            nameRange = moduleNameRange,
+            typeDefinitionRange = range,
+            params = typeParameters
+        ) { tempContext ->
+            val checkedMappings = mappings.mapValues { (_, type) ->
+                type.collectPotentialError(errorCollector = errorCollector) {
+                    this.validate(context = tempContext, errorRange = range)
+                }
+            }
+            if (isObject) {
+                Module.TypeDefinition.ObjectType(
+                    range = range,
+                    typeParameters = typeParameters,
+                    mappings = checkedMappings
+                )
+            } else {
+                Module.TypeDefinition.VariantType(
+                    range = range,
+                    typeParameters = typeParameters,
+                    mappings = checkedMappings
+                )
+            }
+        }
+    }
+}
+
+private fun processCurrentContextWithMembersAndMethods(
+    moduleRange: Range,
+    moduleName: String,
+    moduleMembers: List<Module.MemberDefinition>,
+    isUtilModule: Boolean,
+    typeCheckingContext: TypeCheckingContext
+): Pair<TypeCheckingContext, List<Module.MemberDefinition>> {
+    moduleMembers.map { it.name }.checkNameCollision(range = moduleRange)
+    val partiallyCheckedMembers = moduleMembers.map { member ->
+        val typeParameters = member.type.first
+        typeParameters?.checkNameCollision(range = member.range)
+        var newContext = typeParameters
+            ?.let { typeCheckingContext.addLocalGenericTypes(genericTypes = it) }
+            ?: typeCheckingContext
+        if (member.isMethod) {
+            if (isUtilModule) {
+                throw IllegalMethodDefinitionError(
+                    name = moduleName,
+                    range = moduleRange
+                )
+            }
+            val updatedNewCtx = newContext.getCurrentModuleTypeDef()
+                ?.typeParameters
+                ?.let { newContext.addLocalGenericTypes(genericTypes = it) }
+            if (updatedNewCtx != null) {
+                newContext = updatedNewCtx
+            }
+        }
+        val type = member.type.second.validate(context = newContext, errorRange = member.range) as Type.FunctionType
+        member.copy(type = typeParameters to type)
+    }
+    val memberTypeInfo = partiallyCheckedMembers.map { member ->
+        Triple(
+            first = member.name,
+            second = member.isMethod,
+            third = TypeCheckingContext.TypeInfo(
+                isPublic = member.isPublic,
+                typeParams = member.type.first,
+                type = member.type.second
+            )
+        )
+    }
+    val newCtx = typeCheckingContext.addMembersAndMethodsToCurrentModule(members = memberTypeInfo)
+    return newCtx to partiallyCheckedMembers
+}
+
+private fun typeCheckMember(
+    member: Module.MemberDefinition, errorCollector: ErrorCollector, typeCheckingContext: TypeCheckingContext
+): Module.MemberDefinition {
+    val (_, _, _, _, type, value) = member
+    var contextForTypeCheckingValue =
+        if (member.isMethod) typeCheckingContext.addThisType() else typeCheckingContext
+    val typeParameters = type.first
+    if (typeParameters != null) {
+        contextForTypeCheckingValue =
+            contextForTypeCheckingValue.addLocalGenericTypes(genericTypes = typeParameters)
+    }
+    val checkedValue = value.typeCheck(
+        errorCollector = errorCollector,
+        typeCheckingContext = contextForTypeCheckingValue,
+        expectedType = type.second
+    )
+    return member.copy(value = checkedValue as Expression.Lambda)
+}
--- src/main/kotlin/samlang/checker/ProgramTypeChecker.kt
+++ src/main/kotlin/samlang/checker/ProgramTypeChecker.kt
@@ -1,36 +1,22 @@
 package samlang.checker
 
 import samlang.ast.*
-import samlang.ast.Module.MemberDefinition
-import samlang.ast.Module.TypeDefinition
-import samlang.errors.CollisionError
 import samlang.errors.CompileTimeError
-import samlang.errors.IllegalMethodDefinitionError
 import samlang.util.Either
 
 internal object ProgramTypeChecker {
 
-    private fun Collection<String>.checkNameCollision(range: Range) {
-        val nameSet = hashSetOf<String>()
-        forEach { name ->
-            if (!nameSet.add(element = name)) {
-                throw CollisionError(collidedName = name, range = range)
-            }
-        }
-    }
-
     fun typeCheck(program: Program, typeCheckingContext: TypeCheckingContext): Either<Program, List<CompileTimeError>> {
         val checkedModules = arrayListOf<Module>()
-        var currentCtx = typeCheckingContext
+        var currentContext = typeCheckingContext
         val errorCollector = ErrorCollector()
         for (module in program.modules) {
-            val (checkedModule, newCtx) = typeCheck(
-                module = module,
+            val (checkedModule, newCtx) = module.typeCheck(
                 errorCollector = errorCollector,
-                typeCheckingContext = currentCtx
+                typeCheckingContext = currentContext
             )
             checkedModule?.let { checkedModules.add(element = it) }
-            currentCtx = newCtx
+            currentContext = newCtx
         }
         val errors = errorCollector.collectedErrors
         return if (errors.isEmpty()) {
@@ -45,155 +31,5 @@ internal object ProgramTypeChecker {
             is Either.Left -> result.v
             is Either.Right -> throw result.v[0]
         }
-
-    private fun typeCheck(
-        module: Module,
-        errorCollector: ErrorCollector,
-        typeCheckingContext: TypeCheckingContext
-    ): Pair<Module?, TypeCheckingContext> {
-        val (_, moduleNameRange, moduleName, moduleTypeDefinition, moduleMembers) = module
-        val (checkedTypeDef, partialContext) = errorCollector.returnNullOnCollectedError {
-            createContextWithCurrentModuleDefOnly(
-                moduleNameRange = moduleNameRange,
-                moduleName = moduleName,
-                moduleTypeDefinition = moduleTypeDefinition,
-                typeCheckingContext = typeCheckingContext
-            )
-        } ?: return null to typeCheckingContext
-        val (fullContext, partiallyCheckedMembers) = errorCollector.returnNullOnCollectedError {
-            processCurrentContextWithMembersAndMethods(
-                moduleRange = moduleNameRange,
-                moduleName = moduleName,
-                moduleMembers = moduleMembers,
-                isUtilModule = moduleTypeDefinition == null,
-                typeCheckingContext = partialContext
-            )
-        } ?: return null to typeCheckingContext
-        val checkedModule = module.copy(
-            typeDefinition = checkedTypeDef,
-            members = partiallyCheckedMembers.map { memberDefinition ->
-                typeCheckMember(
-                    member = memberDefinition,
-                    errorCollector = errorCollector,
-                    typeCheckingContext = fullContext
-                )
-            }
-        )
-        return checkedModule to fullContext
-    }
-
-    private fun createContextWithCurrentModuleDefOnly(
-        moduleNameRange: Range,
-        moduleName: String,
-        moduleTypeDefinition: TypeDefinition?,
-        typeCheckingContext: TypeCheckingContext
-    ): Pair<TypeDefinition?, TypeCheckingContext> {
-        // new context with type def but empty members and extensions
-        return if (moduleTypeDefinition == null) {
-            null to typeCheckingContext.addNewEmptyUtilModule(name = moduleName, nameRange = moduleNameRange)
-        } else {
-            val range = moduleTypeDefinition.range
-            val typeParameters = moduleTypeDefinition.typeParameters
-            val (isObject, mappings) = when (moduleTypeDefinition) {
-                is TypeDefinition.ObjectType -> {
-                    true to moduleTypeDefinition.mappings
-                }
-                is TypeDefinition.VariantType -> {
-                    false to moduleTypeDefinition.mappings
-                }
-            }
-            // check name collisions
-            typeParameters?.checkNameCollision(range = range)
-            mappings.keys.checkNameCollision(range = range)
-            // create checked type def based on a temp
-            typeCheckingContext.addNewModuleTypeDefinition(
-                name = moduleName,
-                nameRange = moduleNameRange,
-                typeDefinitionRange = range,
-                params = typeParameters
-            ) { tempContext ->
-                val checkedMappings =
-                    mappings.mapValues { (_, type) -> type.validate(context = tempContext, errorRange = range) }
-                if (isObject) {
-                    TypeDefinition.ObjectType(
-                        range = range,
-                        typeParameters = typeParameters,
-                        mappings = checkedMappings
-                    )
-                } else {
-                    TypeDefinition.VariantType(
-                        range = range,
-                        typeParameters = typeParameters,
-                        mappings = checkedMappings
-                    )
-                }
-            }
-        }
-    }
-
-    private fun processCurrentContextWithMembersAndMethods(
-        moduleRange: Range,
-        moduleName: String,
-        moduleMembers: List<MemberDefinition>,
-        isUtilModule: Boolean,
-        typeCheckingContext: TypeCheckingContext
-    ): Pair<TypeCheckingContext, List<MemberDefinition>> {
-        moduleMembers.map { it.name }.checkNameCollision(range = moduleRange)
-        val partiallyCheckedMembers = moduleMembers.map { member ->
-            val typeParameters = member.type.first
-            typeParameters?.checkNameCollision(range = member.range)
-            var newContext = typeParameters
-                ?.let { typeCheckingContext.addLocalGenericTypes(genericTypes = it) }
-                ?: typeCheckingContext
-            if (member.isMethod) {
-                if (isUtilModule) {
-                    throw IllegalMethodDefinitionError(
-                        name = moduleName,
-                        range = moduleRange
-                    )
-                }
-                val updatedNewCtx = newContext.getCurrentModuleTypeDef()
-                    ?.typeParameters
-                    ?.let { newContext.addLocalGenericTypes(genericTypes = it) }
-                if (updatedNewCtx != null) {
-                    newContext = updatedNewCtx
-                }
-            }
-            val type = member.type.second.validate(context = newContext, errorRange = member.range) as Type.FunctionType
-            member.copy(type = typeParameters to type)
-        }
-        val memberTypeInfo = partiallyCheckedMembers.map { member ->
-            Triple(
-                first = member.name,
-                second = member.isMethod,
-                third = TypeCheckingContext.TypeInfo(
-                    isPublic = member.isPublic,
-                    typeParams = member.type.first,
-                    type = member.type.second
-                )
-            )
-        }
-        val newCtx = typeCheckingContext.addMembersAndMethodsToCurrentModule(members = memberTypeInfo)
-        return newCtx to partiallyCheckedMembers
-    }
-
-    private fun typeCheckMember(
-        member: MemberDefinition, errorCollector: ErrorCollector, typeCheckingContext: TypeCheckingContext
-    ): MemberDefinition {
-        val (_, _, _, _, type, value) = member
-        var contextForTypeCheckingValue =
-            if (member.isMethod) typeCheckingContext.addThisType() else typeCheckingContext
-        val typeParameters = type.first
-        if (typeParameters != null) {
-            contextForTypeCheckingValue =
-                contextForTypeCheckingValue.addLocalGenericTypes(genericTypes = typeParameters)
-        }
-        val checkedValue = value.typeCheck(
-            errorCollector = errorCollector,
-            typeCheckingContext = contextForTypeCheckingValue,
-            expectedType = type.second
-        )
-        return member.copy(value = checkedValue as Expression.Lambda)
-    }
 
 }
