c338186ae9ac777057f848a5123d44336f8955f7
Sam <sam@developersam.com>
2019-07-21 18:12:14 -0700

    [test] Add ModuleImportsCheckerTest

---
+++ src/test/kotlin/samlang/checker/ModuleImportsCheckerTest.kt
@@ -0,0 +1,105 @@
+package samlang.checker
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import samlang.ast.ClassDefinition
+import samlang.ast.Module
+import samlang.ast.ModuleMembersImport
+import samlang.ast.ModuleReference
+import samlang.ast.Range
+import samlang.ast.Sources
+
+class ModuleImportsCheckerTest : StringSpec() {
+
+    private fun createMockClass(name: String): ClassDefinition =
+        ClassDefinition(
+            range = Range.DUMMY,
+            name = name,
+            nameRange = Range.DUMMY,
+            typeDefinition = ClassDefinition.TypeDefinition(
+                range = Range.DUMMY,
+                type = ClassDefinition.TypeDefinitionType.OBJECT,
+                typeParameters = null,
+                mappings = emptyMap()
+            ),
+            members = emptyList()
+        )
+
+    private fun createMockModule(
+        name: String,
+        imports: List<Pair<String, List<String>>> = emptyList(),
+        members: List<String> = emptyList()
+    ): Pair<String, Module> =
+        name to Module(
+            imports = imports.map { (name, members) ->
+                ModuleMembersImport(
+                    range = Range.DUMMY,
+                    moduleReference = ModuleReference(range = Range.DUMMY, parts = listOf(name)),
+                    importedMembers = members.map { it to Range.DUMMY }
+                )
+            },
+            classDefinitions = members.map(transform = ::createMockClass)
+        )
+
+    private fun createMockSources(modules: List<Pair<String, Module>>): Sources =
+        Sources(
+            moduleMappings = LinkedHashMap<ModuleReference, Module>().apply {
+                modules.forEach { (name, module) ->
+                    this[ModuleReference(range = Range.DUMMY, parts = listOf(name))] = module
+                }
+            }
+        )
+
+    private fun checkErrors(modules: List<Pair<String, Module>>, errors: List<String>) {
+        val sources = createMockSources(modules = modules)
+        val errorCollector = ErrorCollector()
+        for ((moduleReference, module) in sources.moduleMappings) {
+            val moduleErrorCollector = ErrorCollector()
+            checkUndefinedImportsError(sources = sources, module = module, errorCollector = moduleErrorCollector)
+            errorCollector.addErrorsWithModules(
+                errorCollector = moduleErrorCollector,
+                moduleReference = moduleReference
+            )
+        }
+        errorCollector.collectedErrors.map { it.errorMessage } shouldBe errors
+    }
+
+    init {
+        "Empty sources have no error." {
+            checkErrors(modules = emptyList(), errors = emptyList())
+        }
+        "No import sources have no errors." {
+            checkErrors(
+                modules = listOf(
+                    createMockModule(name = "A"),
+                    createMockModule(name = "B", members = listOf("foo")),
+                    createMockModule(name = "C", members = listOf("bar"))
+                ),
+                errors = listOf()
+            )
+        }
+        "Cyclic dependency causes no errors." {
+            checkErrors(
+                modules = listOf(
+                    createMockModule(name = "A", imports = listOf("B" to listOf("Bar")), members = listOf("Foo")),
+                    createMockModule(name = "B", imports = listOf("A" to listOf("Foo")), members = listOf("Bar"))
+                ),
+                errors = listOf()
+            )
+        }
+        "Missing classes cause errors." {
+            checkErrors(
+                modules = listOf(
+                    createMockModule(name = "A", imports = listOf("B" to listOf("Foo", "Bar"))),
+                    createMockModule(name = "B", imports = listOf("A" to listOf("Foo", "Bar")))
+                ),
+                errors = listOf(
+                    "A.sam:0:0-0:0: [UnresolvedName]: Name `Foo` is not resolved.",
+                    "A.sam:0:0-0:0: [UnresolvedName]: Name `Bar` is not resolved.",
+                    "B.sam:0:0-0:0: [UnresolvedName]: Name `Foo` is not resolved.",
+                    "B.sam:0:0-0:0: [UnresolvedName]: Name `Bar` is not resolved."
+                )
+            )
+        }
+    }
+}
