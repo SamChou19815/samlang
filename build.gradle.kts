4d033bf4830a9ffbd691eb18eaada7d25d4f78d8
Sam <sam@developersam.com>
2019-11-02 23:41:01 -0400

    [dependencies] Bump gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation(project(":samlang-interpreter"))
     implementation(project(":samlang-compiler"))
     implementation(project(":samlang-printer"))
-    implementation(dependencyNotation = "com.google.code.gson:gson:2.8.5")
+    implementation(dependencyNotation = "com.google.code.gson:gson:2.8.6")
     implementation(dependencyNotation = "com.sparkjava:spark-core:2.9.1")
     implementation(dependencyNotation = "com.github.ajalt:clikt:2.1.0")
 }
