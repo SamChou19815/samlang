e6bafe4a296d04241b3be8a2d91bd8bce92bc834
Sam <sam@developersam.com>
2019-12-23 13:28:22 +0800

    [checker] Remove unused methods in `ErrorCollector`

--- samlang-checker/src/main/kotlin/samlang/checker/ErrorCollector.kt
+++ samlang-checker/src/main/kotlin/samlang/checker/ErrorCollector.kt
@@ -5,9 +5,6 @@ import samlang.ast.common.Range
 import samlang.errors.CollisionError
 import samlang.errors.CompileTimeError
 
-internal inline fun <T> T.collectPotentialError(errorCollector: ErrorCollector, crossinline checker: T.() -> T): T =
-    errorCollector.collectPotentialError(unchecked = this, checker = checker)
-
 class ErrorCollector {
 
     val collectedErrors: List<CompileTimeError> get() = _collectedErrors
@@ -27,31 +24,6 @@ class ErrorCollector {
 
     fun reportCollisionError(name: String, range: Range): Unit =
         add(compileTimeError = CollisionError(collidedName = name, range = range))
-
-    internal inline fun <T> collectPotentialError(unchecked: T, crossinline checker: (T) -> T): T =
-        try {
-            checker(unchecked)
-        } catch (compileTimeError: CompileTimeError) {
-            _collectedErrors.add(element = compileTimeError)
-            unchecked
-        }
-
-    internal inline fun <T> check(crossinline checker: () -> T) {
-        try {
-            checker()
-        } catch (compileTimeError: CompileTimeError) {
-            _collectedErrors.add(element = compileTimeError)
-        }
-    }
-
-    internal inline fun <T> passCheck(crossinline checker: () -> T): Boolean =
-        try {
-            checker()
-            true
-        } catch (compileTimeError: CompileTimeError) {
-            _collectedErrors.add(element = compileTimeError)
-            false
-        }
 
     internal inline fun <T> returnNullOnCollectedError(crossinline checker: () -> T): T? =
         try {
