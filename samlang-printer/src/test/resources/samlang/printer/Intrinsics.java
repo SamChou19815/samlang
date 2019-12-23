99a0156eba006804a40e1ba0bc43ae44b3c80778
Sam <sam@developersam.com>
2019-12-23 17:43:38 +0800

    [printer] Update Java samlang Intrinsics to include `Tuple22`

--- samlang-printer/src/test/resources/samlang/printer/Intrinsics.java
+++ samlang-printer/src/test/resources/samlang/printer/Intrinsics.java
@@ -546,6 +546,54 @@ public final class SamlangIntrinsics$ {
       this.value20 = value20;
     }
   }
+  public static final class Tuple22<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21> {
+    public final T0 value0;
+    public final T1 value1;
+    public final T2 value2;
+    public final T3 value3;
+    public final T4 value4;
+    public final T5 value5;
+    public final T6 value6;
+    public final T7 value7;
+    public final T8 value8;
+    public final T9 value9;
+    public final T10 value10;
+    public final T11 value11;
+    public final T12 value12;
+    public final T13 value13;
+    public final T14 value14;
+    public final T15 value15;
+    public final T16 value16;
+    public final T17 value17;
+    public final T18 value18;
+    public final T19 value19;
+    public final T20 value20;
+    public final T21 value21;
+    public Tuple22(T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8, T9 value9, T10 value10, T11 value11, T12 value12, T13 value13, T14 value14, T15 value15, T16 value16, T17 value17, T18 value18, T19 value19, T20 value20, T21 value21) {
+      this.value0 = value0;
+      this.value1 = value1;
+      this.value2 = value2;
+      this.value3 = value3;
+      this.value4 = value4;
+      this.value5 = value5;
+      this.value6 = value6;
+      this.value7 = value7;
+      this.value8 = value8;
+      this.value9 = value9;
+      this.value10 = value10;
+      this.value11 = value11;
+      this.value12 = value12;
+      this.value13 = value13;
+      this.value14 = value14;
+      this.value15 = value15;
+      this.value16 = value16;
+      this.value17 = value17;
+      this.value18 = value18;
+      this.value19 = value19;
+      this.value20 = value20;
+      this.value21 = value21;
+    }
+  }
   public interface Function0<R> { R apply(); }
   public interface Function1<T0, R> {
     R apply(T0 arg0);
