e8785de2f8ff72ef33bfca46da98d6ce15139e92
Sam <sam@developersam.com>
2019-12-25 13:57:36 +0800

    [misc] Rename executable to samlang

--- src/main/kotlin/samlang/cli/RootCommand.kt
+++ src/main/kotlin/samlang/cli/RootCommand.kt
@@ -2,4 +2,4 @@ package samlang.cli
 
 import com.github.ajalt.clikt.core.NoRunCliktCommand
 
-class RootCommand : NoRunCliktCommand(name = "sam")
+class RootCommand : NoRunCliktCommand(name = "samlang")
