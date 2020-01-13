174592c74bc34c0569138c5b9f111c241d8dd946
Sam <sam@developersam.com>
2020-01-13 21:32:46 +0800

    [scripts] Completely rely on the JS script to check x86 code

--- scripts/run-programs.js
+++ scripts/run-programs.js
@@ -1,32 +1,76 @@
 #!/usr/bin/env node
 
+// @ts-check
+
 const fs = require('fs');
 const path = require('path');
 const { spawnSync } = require('child_process');
 
+/**
+ * @param {string} filename
+ * @returns {string}
+ */
+const read = filename => fs.readFileSync(filename).toString();
+
+/**
+ * @param {string} command
+ * @returns {string}
+ */
+const runWithErrorCheck = command => {
+  const result = spawnSync(command);
+  if (result.status !== 0) {
+    throw new Error(`Command \`${command}\` failed with ${result.status}.`);
+  }
+  return result.stdout.toString();
+};
+
 const basePath = './out/x86';
 
-/** @type {string[]} */
-const programs = [];
-fs.readdirSync(basePath).forEach(filename => {
-  if (path.extname(filename) !== '.s') {
-    const fullRelativePath = `${basePath}/${filename}`;
-    try {
-      fs.accessSync(fullRelativePath, fs.constants.X_OK);
-      programs.push(fullRelativePath);
-    } catch (_) {}
+const getPrograms = () => {
+  /** @type {string[]} */
+  const programs = [];
+  fs.readdirSync(basePath).forEach(filename => {
+    if (filename === 'program.s') {
+      return;
+    }
+    if (!filename.startsWith('test.runnable')) {
+      return;
+    }
+    if (path.extname(filename) !== '.s') {
+      const fullRelativePath = `${basePath}/${filename}`;
+      try {
+        fs.accessSync(fullRelativePath, fs.constants.X_OK);
+        programs.push(fullRelativePath);
+      } catch (_) {}
+    }
+  });
+  programs.sort((a, b) => a.localeCompare(b));
+  return programs;
+};
+
+/**
+ * @param {string[]} programs
+ * @returns {string}
+ */
+const interpretPrograms = programs =>
+  programs.map(program => `#${program}\n${runWithErrorCheck(program)}`).join('\n');
+
+/**
+ * @param {string} expected
+ * @param {string} actual
+ * @returns {boolean}
+ */
+const compare = (expected, actual) => {
+  if (expected === actual) {
+    return true;
   }
-});
-programs.sort((a, b) => a.localeCompare(b));
+  console.log('Inconsistency:');
+  console.log(`Actual:\n${actual}`);
+  return false;
+};
 
-const interpretationStart = new Date().getTime();
-
-const interpretationResult = programs
-  .map(program => `#${program}\n${spawnSync(program).stdout.toString()}`)
-  .join('\n');
-fs.writeFileSync('./scripts/result-to-test-against-snapshot.txt', interpretationResult);
-
-const interpretationTime = new Date().getTime() - interpretationStart;
-
-console.log(`Finished running compiled code. Total time: ${interpretationTime}ms.`);
-process.exit(0);
+console.error('Checking generated X86 code...');
+if (!compare(read('./scripts/snapshot.txt'), interpretPrograms(getPrograms()))) {
+  process.exit(1);
+}
+console.error('Generated X86 code is good.');
