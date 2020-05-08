0ddd59adcc6fd2db0905578e64595aceced1f25d
Sam <sam@developersam.com>
2020-05-08 03:03:03 -0400

    [util][kotlin] Migrate util module to kotlin multiplatform

--- samlang-errors/build.gradle.kts
+++ samlang-errors/build.gradle.kts
@@ -12,24 +12,15 @@ kotlin {
                 implementation(project(":samlang-ast"))
             }
         }
-        commonTest {
-            dependencies {
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-common")
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-annotations-common")
-                implementation(dependencyNotation = "io.kotlintest:kotlintest-runner-junit5:3.4.2")
-            }
-        }
         val jvmMain by getting {
             dependencies {
                 implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-stdlib")
             }
         }
-        val jvmTest by getting {}
         val jsMain by getting {
             dependencies {
                 implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-stdlib-js")
             }
         }
-        val jsTest by getting {}
     }
 }
--- samlang-utils/build.gradle.kts
+++ samlang-utils/build.gradle.kts
@@ -1,7 +1,35 @@
 plugins {
-    kotlin(module = "jvm")
+    kotlin(module = "multiplatform")
 }
 
-dependencies {
-    implementation(project(":samlang-errors"))
+kotlin {
+    jvm()
+    js()
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-stdlib-common")
+                implementation(project(":samlang-errors"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-common")
+                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-annotations-common")
+                implementation(dependencyNotation = "io.kotlintest:kotlintest-runner-junit5:3.4.2")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-stdlib")
+            }
+        }
+        val jvmTest by getting {}
+        val jsMain by getting {
+            dependencies {
+                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-stdlib-js")
+            }
+        }
+        val jsTest by getting {}
+    }
 }
