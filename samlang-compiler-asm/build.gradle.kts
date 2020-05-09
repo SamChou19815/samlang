1c73cc0f6d01c559758884ef755483fa88d129c6
Sam <sam@developersam.com>
2020-05-09 00:45:07 -0400

    [gradle] Split compiler module

---
+++ samlang-compiler-asm/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin(module = "jvm")
+}
+
+dependencies {
+    implementation(kotlin(module = "stdlib-jdk8"))
+    implementation(dependencyNotation = "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3")
+    implementation(project(":samlang-ast"))
+    implementation(project(":samlang-analysis"))
+    implementation(project(":samlang-optimization"))
+}
--- samlang-compiler-hir/build.gradle.kts
+++ samlang-compiler-hir/build.gradle.kts
@@ -4,10 +4,7 @@ plugins {
 
 dependencies {
     implementation(kotlin(module = "stdlib-jdk8"))
-    implementation(dependencyNotation = "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3")
     implementation(project(":samlang-ast"))
-    implementation(project(":samlang-analysis"))
-    implementation(project(":samlang-optimization"))
 
     testImplementation(kotlin(module = "reflect"))
     testImplementation(kotlin(module = "test"))
