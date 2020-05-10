7e9014ebf592272221b5ec048947604b10b42543
Sam <sam@developersam.com>
2020-05-10 01:41:55 -0400

    [gradle] Rename generated parser package

--- samlang-parser/build.gradle.kts
+++ samlang-parser/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
                 implementation(kotlin("stdlib-jdk8"))
                 implementation(dependencyNotation = "org.antlr:antlr4:4.8")
                 implementation(dependencyNotation = "org.apache.commons:commons-text:1.6")
-                implementation(project(":samlang-parser-generated"))
+                implementation(project(":samlang-parser-generated-java"))
             }
         }
         val jsMain by getting {
