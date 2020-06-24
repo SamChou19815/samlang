ce4060e9294815a223799c158a8defdc6fc0337a
Sam <sam@developersam.com>
2020-06-23 20:16:41 -0400

    [ast] Add class declaration

---
+++ samlang-ast/src/commonMain/kotlin/samlang/ast/lang/ClassDeclaration.kt
@@ -0,0 +1,27 @@
+package samlang.ast.lang
+
+import samlang.ast.common.Range
+import samlang.ast.common.Type
+import samlang.ast.common.TypeDefinition
+
+data class ClassDeclaration(
+    override val range: Range,
+    override val nameRange: Range,
+    override val name: String,
+    override val isPublic: Boolean,
+    override val typeDefinition: TypeDefinition,
+    override val members: List<MemberDeclaration>
+) : ClassInterface<ClassDeclaration.MemberDeclaration> {
+
+    data class MemberDeclaration(
+        override val range: Range,
+        override val isPublic: Boolean,
+        override val isMethod: Boolean,
+        override val nameRange: Range,
+        override val name: String,
+        override val typeParameters: List<String>,
+        override val type: Type.FunctionType,
+        override val parameters: List<AnnotatedParameter>,
+        val body: Expression
+    ) : ClassMemberInterface
+}
