042493fef6a9746a9237b3d7efac3f9b989b0c2e
Sam <sam@developersam.com>
2020-06-27 14:18:48 -0400

    [ast] Convert ClassInterface into sealed class
    Prepare for pattern matching

--- samlang-ast/src/commonMain/kotlin/samlang/ast/lang/ClassInterface.kt
+++ samlang-ast/src/commonMain/kotlin/samlang/ast/lang/ClassInterface.kt
@@ -2,10 +2,54 @@ package samlang.ast.lang
 
 import samlang.ast.common.Node
 import samlang.ast.common.Range
+import samlang.ast.common.Type
+import samlang.ast.common.TypeDefinition
 
-interface ClassInterface<M : ClassMemberInterface> : Node {
-    val nameRange: Range
-    val name: String
-    val isPublic: Boolean
-    val members: List<M>
+sealed class ClassInterface<out M : ClassMemberInterface> : Node {
+    abstract val nameRange: Range
+    abstract val name: String
+    abstract val isPublic: Boolean
+    abstract val members: List<M>
+}
+
+data class ClassDefinition(
+    override val range: Range,
+    override val nameRange: Range,
+    override val name: String,
+    override val isPublic: Boolean,
+    val typeDefinition: TypeDefinition,
+    override val members: List<MemberDefinition>
+) : ClassInterface<ClassDefinition.MemberDefinition>() {
+
+    data class MemberDefinition(
+        override val range: Range,
+        override val isPublic: Boolean,
+        override val isMethod: Boolean,
+        override val nameRange: Range,
+        override val name: String,
+        override val typeParameters: List<String>,
+        override val type: Type.FunctionType,
+        override val parameters: List<AnnotatedParameter>,
+        val body: Expression
+    ) : ClassMemberInterface
+}
+
+data class ClassDeclaration(
+    override val range: Range,
+    override val nameRange: Range,
+    override val name: String,
+    override val isPublic: Boolean,
+    override val members: List<MemberDeclaration>
+) : ClassInterface<ClassDeclaration.MemberDeclaration>() {
+
+    data class MemberDeclaration(
+        override val range: Range,
+        override val isPublic: Boolean,
+        override val isMethod: Boolean,
+        override val nameRange: Range,
+        override val name: String,
+        override val typeParameters: List<String>,
+        override val type: Type.FunctionType,
+        override val parameters: List<AnnotatedParameter>
+    ) : ClassMemberInterface
 }
