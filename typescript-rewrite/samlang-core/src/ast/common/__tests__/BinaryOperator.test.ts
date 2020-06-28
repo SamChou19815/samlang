1704a900ac35284269da823c5b3db0e6989f3fe8
Sam <sam@developersam.com>
2020-06-28 18:13:38 -0400

    [ts-rewrite][ast][test] Setup jest and add tests for BinaryOperator

---
+++ typescript-rewrite/samlang-core/src/ast/common/__tests__/BinaryOperator.test.ts
@@ -0,0 +1,11 @@
+import { binaryOperatorValues, binaryOperatorSymbolTable } from '../BinaryOperator';
+
+it(`BinaryOperator's symbol table is self consistent.`, () => {
+  binaryOperatorValues.forEach((symbol) =>
+    expect(binaryOperatorSymbolTable[symbol.symbol]).toBe(symbol)
+  );
+});
+
+it('Invalid symbol gives back undefined', () => {
+  expect(binaryOperatorSymbolTable['I am not a symbol.']).toBeUndefined();
+});
