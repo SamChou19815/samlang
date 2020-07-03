b4a678f24fefc1b94839e38bdcc939316957f32c
Sam <sam@developersam.com>
2020-07-03 18:47:16 -0400

    [ts-rewrite][checker] Implement undefined imports checker

---
+++ typescript-rewrite/samlang-core/src/checker/__tests/undefined-import-checker.test.ts
@@ -0,0 +1,109 @@
+import ModuleReference from '../../ast/common/module-reference';
+import Range from '../../ast/common/range';
+import type { Sources } from '../../ast/common/structs';
+import { SamlangModule, ClassDefinition } from '../../ast/lang/samlang-toplevel';
+import { createGlobalErrorCollector } from '../../errors/error-collector';
+import { hashMapOf } from '../../util/collections';
+import checkUndefinedImportsError from '../undefined-imports-checker';
+
+const createMockClass = (name: string, isPublic: boolean): ClassDefinition => ({
+  range: Range.DUMMY,
+  name,
+  nameRange: Range.DUMMY,
+  isPublic,
+  typeParameters: [],
+  members: [],
+  typeDefinition: { range: Range.DUMMY, type: 'object', names: [], mappings: {} },
+});
+
+const createMockModule = (
+  name: string,
+  imports: readonly (readonly [string, readonly string[]])[] = [],
+  members: readonly (readonly [string, boolean])[] = []
+): readonly [string, SamlangModule] => [
+  name,
+  {
+    imports: imports.map(([importedModuleName, importedMembers]) => ({
+      range: Range.DUMMY,
+      importedMembers: importedMembers.map((it) => [it, Range.DUMMY]),
+      importedModule: new ModuleReference([importedModuleName]),
+      importedModuleRange: Range.DUMMY,
+    })),
+    classes: members.map(([className, isPublic]) => createMockClass(className, isPublic)),
+  },
+];
+
+const createMockSources = (
+  modules: readonly (readonly [string, SamlangModule])[]
+): Sources<SamlangModule> =>
+  hashMapOf(
+    ...modules.map(([name, samlangModule]) => [new ModuleReference([name]), samlangModule] as const)
+  );
+
+const checkErrors = (
+  modules: readonly (readonly [string, SamlangModule])[],
+  errors: readonly string[]
+): void => {
+  const sources = createMockSources(modules);
+  const globalErrorCollector = createGlobalErrorCollector();
+  sources.forEach((samlangModule, moduleReference) => {
+    checkUndefinedImportsError(
+      sources,
+      samlangModule,
+      globalErrorCollector.getModuleErrorCollector(moduleReference)
+    );
+  });
+  expect(errors).toEqual(globalErrorCollector.getErrors().map((it) => it.toString()));
+};
+
+it('Empty sources have no errors.', () => checkErrors([], []));
+
+it('No import sources have no errors.', () => {
+  checkErrors(
+    [
+      createMockModule('A'),
+      createMockModule('B', [], [['Foo', true]]),
+      createMockModule('C', [], [['Bar', true]]),
+    ],
+    []
+  );
+});
+
+it('Cyclic dependency causes no errors.', () => {
+  checkErrors(
+    [
+      createMockModule('A', [['B', ['Bar']]], [['Foo', true]]),
+      createMockModule('B', [['A', ['Foo']]], [['Bar', true]]),
+    ],
+    []
+  );
+});
+
+it('Missing classes cause errors.', () => {
+  checkErrors(
+    [
+      createMockModule('A', [['B', ['Foo', 'Bar']]]),
+      createMockModule('B', [['A', ['Foo', 'Bar']]]),
+    ],
+    [
+      'A.sam:0:0-0:0: [UnresolvedName]: Name `Foo` is not resolved.',
+      'A.sam:0:0-0:0: [UnresolvedName]: Name `Bar` is not resolved.',
+      'B.sam:0:0-0:0: [UnresolvedName]: Name `Foo` is not resolved.',
+      'B.sam:0:0-0:0: [UnresolvedName]: Name `Bar` is not resolved.',
+    ]
+  );
+});
+
+it('Importing private classes causes errors.', () => {
+  checkErrors(
+    [createMockModule('A', [], [['Foo', false]]), createMockModule('B', [['A', ['Foo']]])],
+    ['B.sam:0:0-0:0: [UnresolvedName]: Name `Foo` is not resolved.']
+  );
+});
+
+it('Importing missing module causes errors.', () => {
+  checkErrors(
+    [createMockModule('A', [['B', []]])],
+    ['A.sam:0:0-0:0: [UnresolvedName]: Name `B` is not resolved.']
+  );
+});
---
+++ typescript-rewrite/samlang-core/src/checker/undefined-imports-checker.ts
@@ -0,0 +1,53 @@
+import type { Sources, ModuleMembersImport } from '../ast/common/structs';
+import type { SamlangModule } from '../ast/lang/samlang-toplevel';
+import type { ModuleErrorCollector } from '../errors/error-collector';
+import { isNotNull } from '../util/type-assertions';
+
+class UndefinedImportChecker {
+  constructor(
+    private readonly sources: Sources<SamlangModule>,
+    private readonly errorCollector: ModuleErrorCollector
+  ) {}
+
+  checkModuleImports(samlangModule: SamlangModule): SamlangModule {
+    const checkedImports = samlangModule.imports
+      .map(this.checkModuleMembersImport)
+      .filter(isNotNull);
+    return { classes: samlangModule.classes, imports: checkedImports };
+  }
+
+  private checkModuleMembersImport = (
+    oneImport: ModuleMembersImport
+  ): ModuleMembersImport | null => {
+    const availableMembers = this.sources.get(oneImport.importedModule);
+    if (availableMembers == null) {
+      this.errorCollector.reportUnresolvedNameError(
+        oneImport.range,
+        oneImport.importedModule.toString()
+      );
+      return null;
+    }
+    const availableMembersSet = new Set(
+      availableMembers.classes
+        .map((oneClass) => (oneClass.isPublic ? oneClass.name : null))
+        .filter(isNotNull)
+    );
+    const checkedMemberImports = oneImport.importedMembers.filter(([importedMember, range]) => {
+      if (!availableMembersSet.has(importedMember)) {
+        this.errorCollector.reportUnresolvedNameError(range, importedMember);
+        return false;
+      }
+      return true;
+    });
+    return { ...oneImport, importedMembers: checkedMemberImports };
+  };
+}
+
+const checkUndefinedImportsError = (
+  sources: Sources<SamlangModule>,
+  samlangModule: SamlangModule,
+  errorCollector: ModuleErrorCollector
+): SamlangModule =>
+  new UndefinedImportChecker(sources, errorCollector).checkModuleImports(samlangModule);
+
+export default checkUndefinedImportsError;
