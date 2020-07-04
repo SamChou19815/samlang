bae99dc393d5ef9a6c3a0c9637cddbfa7fd0925d
Sam <sam@developersam.com>
2020-07-03 23:26:21 -0400

    [ts-checker][errors] Unit test error collectors

---
+++ typescript-rewrite/samlang-core/src/errors/__tests__/error-collector.test.ts
@@ -0,0 +1,19 @@
+import ModuleReference from '../../ast/common/module-reference';
+import Range from '../../ast/common/range';
+import { intType, boolType } from '../../ast/common/types';
+import { ModuleErrorCollector, createGlobalErrorCollector } from '../error-collector';
+
+const assertCanAddErrors = (adder: (collector: ModuleErrorCollector) => void): void => {
+  const globalCollector = createGlobalErrorCollector();
+  adder(globalCollector.getModuleErrorCollector(ModuleReference.ROOT));
+  expect(globalCollector.getErrors().length).toBe(1);
+};
+
+it('can add errors', () => {
+  assertCanAddErrors((collector) => collector.reportSyntaxError(Range.DUMMY, ''));
+  assertCanAddErrors((collector) =>
+    collector.reportUnexpectedTypeError(Range.DUMMY, intType, boolType)
+  );
+  assertCanAddErrors((collector) => collector.reportNotWellDefinedIdentifierError(Range.DUMMY, ''));
+  assertCanAddErrors((collector) => collector.reportUnresolvedNameError(Range.DUMMY, ''));
+});
