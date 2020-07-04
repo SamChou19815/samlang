c73da050da6a0a14979bc214d09c78c4fb2e93f8
Sam <sam@developersam.com>
2020-07-03 23:57:49 -0400

    [ts-rewrite][error] Move test cases into array

--- typescript-rewrite/samlang-core/src/errors/__tests__/error-definitions.test.ts
+++ typescript-rewrite/samlang-core/src/errors/__tests__/error-definitions.test.ts
@@ -2,6 +2,7 @@ import ModuleReference from '../../ast/common/module-reference';
 import Range from '../../ast/common/range';
 import { intType, boolType } from '../../ast/common/types';
 import {
+  CompileTimeError,
   SyntaxError,
   UnexpectedTypeError,
   NotWellDefinedIdentifierError,
@@ -9,39 +10,33 @@ import {
   UnsupportedClassTypeDefinitionError,
 } from '../error-definitions';
 
-it('error toString() test', () => {
-  expect(
-    new SyntaxError(new ModuleReference(['Foo', 'Bar']), Range.DUMMY, 'bad code').toString()
-  ).toBe('Foo/Bar.sam:0:0-0:0: [SyntaxError]: bad code');
-  expect(
-    new UnexpectedTypeError(
-      new ModuleReference(['Foo', 'Bar']),
-      Range.DUMMY,
-      intType,
-      boolType
-    ).toString()
-  ).toBe('Foo/Bar.sam:0:0-0:0: [UnexpectedType]: Expected: `int`, actual: `bool`.');
-  expect(
-    new NotWellDefinedIdentifierError(
-      new ModuleReference(['Foo', 'Bar']),
-      Range.DUMMY,
-      'BadType'
-    ).toString()
-  ).toBe('Foo/Bar.sam:0:0-0:0: [NotWellDefinedIdentifier]: `BadType` is not well defined.');
-  expect(
-    new UnresolvedNameError(
-      new ModuleReference(['Foo', 'Bar']),
-      Range.DUMMY,
-      'unresolvedName'
-    ).toString()
-  ).toBe('Foo/Bar.sam:0:0-0:0: [UnresolvedName]: Name `unresolvedName` is not resolved.');
-  expect(
+const testCases: readonly (readonly [CompileTimeError, string])[] = [
+  [
+    new SyntaxError(new ModuleReference(['Foo', 'Bar']), Range.DUMMY, 'bad code'),
+    'Foo/Bar.sam:0:0-0:0: [SyntaxError]: bad code',
+  ],
+  [
+    new UnexpectedTypeError(new ModuleReference(['Foo', 'Bar']), Range.DUMMY, intType, boolType),
+    'Foo/Bar.sam:0:0-0:0: [UnexpectedType]: Expected: `int`, actual: `bool`.',
+  ],
+  [
+    new NotWellDefinedIdentifierError(new ModuleReference(['Foo', 'Bar']), Range.DUMMY, 'BadType'),
+    'Foo/Bar.sam:0:0-0:0: [NotWellDefinedIdentifier]: `BadType` is not well defined.',
+  ],
+  [
+    new UnresolvedNameError(new ModuleReference(['Foo', 'Bar']), Range.DUMMY, 'unresolvedName'),
+    'Foo/Bar.sam:0:0-0:0: [UnresolvedName]: Name `unresolvedName` is not resolved.',
+  ],
+  [
     new UnsupportedClassTypeDefinitionError(
       new ModuleReference(['Foo', 'Bar']),
       Range.DUMMY,
       'object'
-    ).toString()
-  ).toBe(
-    "Foo/Bar.sam:0:0-0:0: [UnsupportedClassTypeDefinition]: Expect the current class to have `object` type definition, but it doesn't."
-  );
-});
+    ),
+    "Foo/Bar.sam:0:0-0:0: [UnsupportedClassTypeDefinition]: Expect the current class to have `object` type definition, but it doesn't.",
+  ],
+];
+
+testCases.forEach(([error, expectedErrorString], index) =>
+  it(`error toString() test ${index + 1}`, () => expect(error.toString()).toBe(expectedErrorString))
+);
