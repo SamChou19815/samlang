e724112c95fea0b23aaabe1ed5bec41c7d081b6e
Sam <sam@developersam.com>
2020-07-04 01:19:14 -0400

    [ts-rewrite] Implement constraint aware type checker

---
+++ typescript-rewrite/samlang-core/src/checker/__tests/constraint-aware-checker.test.ts
@@ -0,0 +1,205 @@
+import {
+  unitType,
+  boolType,
+  intType,
+  stringType,
+  identifierType,
+  tupleType,
+  functionType,
+} from '../../ast/common/types';
+import { checkAndInfer, checkAndInferWithErrorRecording } from '../constraint-aware-checker';
+import TypeResolution from '../type-resolution';
+import { createGlobalErrorCollector } from '../../errors/error-collector';
+import ModuleReference from '../../ast/common/module-reference';
+import Range from '../../ast/common/range';
+
+it('t1=primitive type', () => {
+  const resolution = new TypeResolution();
+
+  expect(checkAndInfer(unitType, unitType, resolution)).toEqual(unitType);
+  expect(checkAndInfer(unitType, boolType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(unitType, intType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(unitType, stringType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(unitType, identifierType('A'), resolution).type).toBe('FAILED_MEET');
+
+  expect(checkAndInfer(unitType, { type: 'UndecidedType', index: 0 }, resolution)).toEqual(
+    unitType
+  );
+  expect(checkAndInfer(intType, { type: 'UndecidedType', index: 0 }, resolution).type).toBe(
+    'FAILED_MEET'
+  );
+});
+
+it('t1=identifier type', () => {
+  const resolution = new TypeResolution();
+
+  expect(checkAndInfer(identifierType('A'), unitType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(identifierType('A'), identifierType('B'), resolution).type).toBe(
+    'FAILED_MEET'
+  );
+  expect(checkAndInfer(identifierType('A'), identifierType('A', [intType]), resolution).type).toBe(
+    'FAILED_MEET'
+  );
+  expect(
+    checkAndInfer(
+      identifierType('A', [identifierType('B')]),
+      identifierType('A', [identifierType('B')]),
+      resolution
+    )
+  ).toEqual(identifierType('A', [identifierType('B')]));
+
+  expect(
+    checkAndInfer(
+      identifierType('A', [identifierType('B')]),
+      identifierType('A', [{ type: 'UndecidedType', index: 0 }]),
+      resolution
+    )
+  ).toEqual(identifierType('A', [identifierType('B')]));
+  expect(
+    checkAndInfer(identifierType('B'), { type: 'UndecidedType', index: 0 }, resolution)
+  ).toEqual(identifierType('B'));
+});
+
+it('t1=tuple type', () => {
+  const resolution = new TypeResolution();
+
+  expect(checkAndInfer(tupleType([]), unitType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(tupleType([]), identifierType('B'), resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(tupleType([]), tupleType([intType]), resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(tupleType([intType]), tupleType([intType]), resolution)).toEqual(
+    tupleType([intType])
+  );
+
+  expect(
+    checkAndInfer(tupleType([intType]), { type: 'UndecidedType', index: 0 }, resolution)
+  ).toEqual(tupleType([intType]));
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 0 },
+      tupleType([{ type: 'UndecidedType', index: 1 }]),
+      resolution
+    )
+  ).toEqual(tupleType([intType]));
+  expect(checkAndInfer(boolType, { type: 'UndecidedType', index: 1 }, resolution).type).toBe(
+    'FAILED_MEET'
+  );
+});
+
+it('t1=function type', () => {
+  const resolution = new TypeResolution();
+
+  expect(checkAndInfer(functionType([], intType), unitType, resolution).type).toBe('FAILED_MEET');
+  expect(checkAndInfer(functionType([], intType), identifierType('B'), resolution).type).toBe(
+    'FAILED_MEET'
+  );
+  expect(
+    checkAndInfer(functionType([], intType), functionType([intType], intType), resolution).type
+  ).toBe('FAILED_MEET');
+  expect(
+    checkAndInfer(functionType([intType], intType), functionType([intType], intType), resolution)
+  ).toEqual(functionType([intType], intType));
+
+  expect(
+    checkAndInfer(
+      functionType([intType], boolType),
+      { type: 'UndecidedType', index: 0 },
+      resolution
+    )
+  ).toEqual(functionType([intType], boolType));
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 0 },
+      functionType([{ type: 'UndecidedType', index: 1 }], { type: 'UndecidedType', index: 2 }),
+      resolution
+    )
+  ).toEqual(functionType([intType], boolType));
+  expect(checkAndInfer(boolType, { type: 'UndecidedType', index: 1 }, resolution).type).toBe(
+    'FAILED_MEET'
+  );
+  expect(checkAndInfer(intType, { type: 'UndecidedType', index: 2 }, resolution).type).toBe(
+    'FAILED_MEET'
+  );
+});
+
+it('t1=undecided type', () => {
+  const resolution = new TypeResolution();
+
+  expect(checkAndInfer({ type: 'UndecidedType', index: 10086 }, intType, resolution)).toEqual(
+    intType
+  );
+
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 0 },
+      { type: 'UndecidedType', index: 1 },
+      resolution
+    )
+  ).toEqual({ type: 'UndecidedType', index: 0 });
+
+  expect(checkAndInfer(boolType, { type: 'UndecidedType', index: 0 }, resolution)).toEqual(
+    boolType
+  );
+  expect(checkAndInfer({ type: 'UndecidedType', index: 0 }, intType, resolution).type).toBe(
+    'FAILED_MEET'
+  );
+  expect(checkAndInfer({ type: 'UndecidedType', index: 1 }, boolType, resolution)).toEqual(
+    boolType
+  );
+
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 0 },
+      { type: 'UndecidedType', index: 2 },
+      resolution
+    )
+  ).toEqual(boolType);
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 0 },
+      { type: 'UndecidedType', index: 3 },
+      resolution
+    )
+  ).toEqual(boolType);
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 2 },
+      { type: 'UndecidedType', index: 3 },
+      resolution
+    )
+  ).toEqual(boolType);
+
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 4 },
+      { type: 'UndecidedType', index: 5 },
+      resolution
+    )
+  ).toEqual({ type: 'UndecidedType', index: 4 });
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 6 },
+      { type: 'UndecidedType', index: 7 },
+      resolution
+    )
+  ).toEqual({ type: 'UndecidedType', index: 6 });
+  expect(
+    checkAndInfer(
+      { type: 'UndecidedType', index: 4 },
+      { type: 'UndecidedType', index: 7 },
+      resolution
+    )
+  ).toEqual({ type: 'UndecidedType', index: 4 });
+});
+
+it('checkAndInferWithErrorRecording type', () => {
+  const resolution = new TypeResolution();
+  const globalCollector = createGlobalErrorCollector();
+  const moduleCollector = globalCollector.getModuleErrorCollector(ModuleReference.ROOT);
+
+  expect(
+    checkAndInferWithErrorRecording(unitType, unitType, resolution, Range.DUMMY, moduleCollector)
+  ).toEqual(unitType);
+  expect(globalCollector.getErrors()).toEqual([]);
+  checkAndInferWithErrorRecording(unitType, boolType, resolution, Range.DUMMY, moduleCollector);
+  expect(globalCollector.getErrors().map((it) => it.errorType)).toEqual(['UnexpectedType']);
+});
---
+++ typescript-rewrite/samlang-core/src/checker/constraint-aware-checker.ts
@@ -0,0 +1,124 @@
+import type Range from '../ast/common/range';
+import { Type, UndecidedType, isTheSameType } from '../ast/common/types';
+import { ModuleErrorCollector } from '../errors/error-collector';
+import TypeResolution from './type-resolution';
+
+const meet = (t1: Type, t2: Type, resolution: TypeResolution): Type => {
+  const meetWithResolution = (type1: Type, type2: Type): Type => meet(type1, type2, resolution);
+
+  switch (t1.type) {
+    case 'PrimitiveType':
+      switch (t2.type) {
+        case 'UndecidedType':
+          return meetWithUndecidedType(t1, t2, resolution);
+        case 'PrimitiveType':
+          if (isTheSameType(t1, t2)) {
+            return t1;
+          }
+          throw new Error();
+        default:
+          throw new Error();
+      }
+    case 'IdentifierType':
+      switch (t2.type) {
+        case 'UndecidedType':
+          return meetWithUndecidedType(t1, t2, resolution);
+        case 'IdentifierType':
+          if (
+            t1.identifier !== t2.identifier ||
+            t1.typeArguments.length !== t2.typeArguments.length
+          ) {
+            throw new Error();
+          }
+          return {
+            type: 'IdentifierType',
+            identifier: t1.identifier,
+            typeArguments: t1.typeArguments.map((type, index) =>
+              meetWithResolution(type, t2.typeArguments[index])
+            ),
+          };
+        default:
+          throw new Error();
+      }
+    case 'TupleType':
+      switch (t2.type) {
+        case 'UndecidedType':
+          return meetWithUndecidedType(t1, t2, resolution);
+        case 'TupleType':
+          if (t1.mappings.length !== t2.mappings.length) {
+            throw new Error();
+          }
+          return {
+            type: 'TupleType',
+            mappings: t1.mappings.map((type, index) =>
+              meetWithResolution(type, t2.mappings[index])
+            ),
+          };
+        default:
+          throw new Error();
+      }
+    case 'FunctionType':
+      switch (t2.type) {
+        case 'UndecidedType':
+          return meetWithUndecidedType(t1, t2, resolution);
+        case 'FunctionType': {
+          const returnType = meet(t1.returnType, t2.returnType, resolution);
+          if (t1.argumentTypes.length !== t2.argumentTypes.length) {
+            throw new Error();
+          }
+          const argumentTypes = t1.argumentTypes.map((type, index) =>
+            meetWithResolution(type, t2.argumentTypes[index])
+          );
+          return { type: 'FunctionType', argumentTypes, returnType };
+        }
+        default:
+          throw new Error();
+      }
+    case 'UndecidedType':
+      return t2.type === 'UndecidedType'
+        ? resolution.establishAliasing(t1, t2, meetWithResolution)
+        : meetWithUndecidedType(t2, t1, resolution);
+  }
+};
+
+const meetWithUndecidedType = (
+  type: Type,
+  undecidedType: UndecidedType,
+  resolution: TypeResolution
+): Type => {
+  const resolvedType = resolution.addTypeResolution(undecidedType.index, type);
+  return resolvedType === type ? type : meet(type, resolvedType, resolution);
+};
+
+export const checkAndInfer = (
+  expectedType: Type,
+  actualType: Type,
+  resolution: TypeResolution
+): Type | { readonly type: 'FAILED_MEET'; readonly expected: Type; readonly actual: Type } => {
+  const partiallyResolvedActualType = resolution.resolveType(actualType);
+  const partiallyResolvedExpectedType = resolution.resolveType(expectedType);
+  try {
+    return meet(partiallyResolvedExpectedType, partiallyResolvedActualType, resolution);
+  } catch {
+    return {
+      type: 'FAILED_MEET',
+      expected: partiallyResolvedExpectedType,
+      actual: partiallyResolvedActualType,
+    };
+  }
+};
+
+export const checkAndInferWithErrorRecording = (
+  expectedType: Type,
+  actualType: Type,
+  resolution: TypeResolution,
+  errorRange: Range,
+  errorCollector: ModuleErrorCollector
+): Type => {
+  const result = checkAndInfer(expectedType, actualType, resolution);
+  if (result.type === 'FAILED_MEET') {
+    errorCollector.reportUnexpectedTypeError(errorRange, result.expected, result.actual);
+    return result.expected;
+  }
+  return result;
+};
