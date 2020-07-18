ff2c85383a4ea050238c03857501b75d6677cace
Sam <sam@developersam.com>
2020-07-18 17:43:51 -0400

    [ast][hir] Remove unused tuple pattern
    After this diff, we can prepare for splitting the remaining pattern into invididual statements

--- samlang-ast/src/commonMain/kotlin/samlang/ast/hir/HighIrPattern.kt
+++ samlang-ast/src/commonMain/kotlin/samlang/ast/hir/HighIrPattern.kt
@@ -2,12 +2,6 @@ package samlang.ast.hir
 
 /** All patterns that can be used in const destructing. */
 sealed class HighIrPattern {
-    /** A tuple pattern like `[t1, t2]`. */
-    data class TuplePattern(val destructedNames: List<String?>) : HighIrPattern() {
-        override fun toString(): String =
-            destructedNames.joinToString(separator = ", ", prefix = "[", postfix = "]") { it ?: "" }
-    }
-
     /** A simple variable pattern like `var1`.  */
     data class VariablePattern(val name: String) : HighIrPattern() {
         override fun toString(): String = name
