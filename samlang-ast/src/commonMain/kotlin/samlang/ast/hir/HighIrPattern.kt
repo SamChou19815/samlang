e8eac488b2caa53fb4354e58627a3982e6bec602
Sam <sam@developersam.com>
2020-07-18 17:35:40 -0400

    [ast][hir] Remove unused pretty print function

--- samlang-ast/src/commonMain/kotlin/samlang/ast/hir/HighIrPattern.kt
+++ samlang-ast/src/commonMain/kotlin/samlang/ast/hir/HighIrPattern.kt
@@ -2,23 +2,19 @@ package samlang.ast.hir
 
 /** All patterns that can be used in const destructing. */
 sealed class HighIrPattern {
-    abstract fun prettyPrint(): String
-
-    final override fun toString(): String = prettyPrint()
-
     /** A tuple pattern like `[t1, t2]`. */
     data class TuplePattern(val destructedNames: List<String?>) : HighIrPattern() {
-        override fun prettyPrint(): String =
+        override fun toString(): String =
             destructedNames.joinToString(separator = ", ", prefix = "[", postfix = "]") { it ?: "" }
     }
 
     /** A simple variable pattern like `var1`.  */
     data class VariablePattern(val name: String) : HighIrPattern() {
-        override fun prettyPrint(): String = name
+        override fun toString(): String = name
     }
 
     /** A wildcard pattern `_` that matches everything. */
     object WildCardPattern : HighIrPattern() {
-        override fun prettyPrint(): String = "_"
+        override fun toString(): String = "_"
     }
 }
