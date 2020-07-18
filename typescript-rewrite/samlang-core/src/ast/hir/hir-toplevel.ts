fffac7d1084c294ded3ac648bfab67b81f5ec7de
Sam <sam@developersam.com>
2020-07-18 19:33:37 -0400

    [ts-rewrite][hir] Setup HIR top level type definitions

---
+++ typescript-rewrite/samlang-core/src/ast/hir/hir-toplevel.ts
@@ -0,0 +1,21 @@
+import type { ModuleMembersImport } from '../common/structs';
+import type { HighIRStatement } from './hir-expressions';
+
+export interface HighIRFunction {
+  readonly isPublic: boolean;
+  readonly isMethod: boolean;
+  readonly name: string;
+  readonly parameters: readonly string[];
+  readonly hasReturn: boolean;
+  readonly body: readonly HighIRStatement[];
+}
+
+export interface HighIRClassDefinition {
+  readonly className: string;
+  readonly members: readonly HighIRFunction[];
+}
+
+export interface HighIRModule {
+  readonly imports: readonly ModuleMembersImport[];
+  readonly classDefinitions: readonly HighIRClassDefinition[];
+}
