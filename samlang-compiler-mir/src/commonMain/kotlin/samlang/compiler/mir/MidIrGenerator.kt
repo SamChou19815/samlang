39523319d2ae5f85cce4f1db4f1dca422463c351
Sam <sam@developersam.com>
2020-07-23 21:55:51 -0400

    [compiler][mir] Convert MidIrGenerator into object

--- samlang-compiler-mir/src/commonMain/kotlin/samlang/compiler/mir/MidIrGenerator.kt
+++ samlang-compiler-mir/src/commonMain/kotlin/samlang/compiler/mir/MidIrGenerator.kt
@@ -13,91 +13,73 @@ import samlang.optimization.SimpleOptimizations
 import samlang.optimization.TailRecursionOptimizer
 
 @ExperimentalStdlibApi
-class MidIrGenerator private constructor(
-    private val globalResourceAllocator: MidIrGlobalResourceAllocator,
-    private val module: HighIrModule
-) {
-    private val functions: List<MidIrFunction>
-    private val globalVariables: Set<GlobalVariable>
+object MidIrGenerator {
+    fun generate(sources: Sources<HighIrModule>, entryModuleReference: ModuleReference): MidIrCompilationUnit =
+        generateWithoutEntry(sources = sources).addMain(entryModuleReference = entryModuleReference)
 
-    init {
-        val functions: MutableList<MidIrFunction> = mutableListOf()
-        val globalVariables: MutableSet<GlobalVariable> = LinkedHashSet()
-        module.functions.forEach { function ->
-            val allocator = MidIrResourceAllocator(
-                functionName = function.name,
-                globalResourceAllocator = globalResourceAllocator
-            )
-            val allocatedArgs = function.parameters.map { allocator.allocateTemp(variableName = it) }
-            val (loweredStatements, stringGlobalVariables) = MidIrLoweringTranslator.translate(allocator, function.body)
-            val mirIrFunction = MidIrFunction(
-                functionName = function.name,
-                argumentTemps = allocatedArgs,
-                mainBodyStatements = SimpleOptimizations.optimizeIr(
-                    statements = MidIrTraceReorganizer.reorder(
-                        allocator = allocator,
-                        originalStatements = loweredStatements + Return()
-                    )
-                ),
-                numberOfArguments = allocatedArgs.size,
-                hasReturn = function.hasReturn
-            )
-            functions += mirIrFunction
-            globalVariables += stringGlobalVariables
+    fun generateWithMultipleEntries(sources: Sources<HighIrModule>): Sources<MidIrCompilationUnit> {
+        val withoutEntry = generateWithoutEntry(sources = sources)
+        val irMappings = sources.moduleMappings.mapValues { (moduleReference, _) ->
+            withoutEntry.addMain(entryModuleReference = moduleReference)
         }
-        this.functions = functions
-        this.globalVariables = globalVariables
+        return Sources(moduleMappings = irMappings)
     }
 
-    companion object {
-        fun generate(sources: Sources<HighIrModule>, entryModuleReference: ModuleReference): MidIrCompilationUnit =
-            generateWithoutEntry(sources = sources).addMain(entryModuleReference = entryModuleReference)
-
-        fun generateWithMultipleEntries(sources: Sources<HighIrModule>): Sources<MidIrCompilationUnit> {
-            val withoutEntry = generateWithoutEntry(sources = sources)
-            val irMappings = sources.moduleMappings.mapValues { (moduleReference, _) ->
-                withoutEntry.addMain(entryModuleReference = moduleReference)
-            }
-            return Sources(moduleMappings = irMappings)
-        }
-
-        private fun generateWithoutEntry(sources: Sources<HighIrModule>): MidIrCompilationUnit {
-            val globalResourceAllocator = MidIrGlobalResourceAllocator()
-            val globalVariables = LinkedHashSet<GlobalVariable>()
-            val functions = mutableListOf<MidIrFunction>()
-            sources.moduleMappings.forEach { (_, module) ->
-                val generator = MidIrGenerator(
-                    globalResourceAllocator = globalResourceAllocator,
-                    module = module
+    private fun generateWithoutEntry(sources: Sources<HighIrModule>): MidIrCompilationUnit {
+        val globalResourceAllocator = MidIrGlobalResourceAllocator()
+        val globalVariables = LinkedHashSet<GlobalVariable>()
+        val functions = mutableListOf<MidIrFunction>()
+        sources.moduleMappings.forEach { (_, module) ->
+            module.functions.forEach { function ->
+                val allocator = MidIrResourceAllocator(
+                    functionName = function.name,
+                    globalResourceAllocator = globalResourceAllocator
                 )
-                globalVariables += generator.globalVariables
-                functions += generator.functions.map { TailRecursionOptimizer.optimize(it) }
-            }
-            return MidIrCompilationUnit(globalVariables = globalVariables.toList(), functions = functions)
-        }
-
-        private fun MidIrCompilationUnit.addMain(entryModuleReference: ModuleReference): MidIrCompilationUnit =
-            SimpleOptimizations.removeUnusedNames(
-                irCompilationUnit = MidIrCompilationUnit(
-                    globalVariables = globalVariables,
-                    functions = functions + getCompiledProgramMainFunction(entryModuleReference = entryModuleReference)
+                val allocatedArgs = function.parameters.map { allocator.allocateTemp(variableName = it) }
+                val (loweredStatements, stringGlobalVariables) = MidIrLoweringTranslator.translate(
+                    allocator,
+                    function.body
                 )
-            )
-
-        private fun getCompiledProgramMainFunction(entryModuleReference: ModuleReference): MidIrFunction =
-            MidIrFunction(
-                functionName = IrNameEncoder.compiledProgramMain,
-                argumentTemps = emptyList(),
-                mainBodyStatements = listOf(
-                    CALL_FUNCTION(
-                        functionName = IrNameEncoder.encodeMainFunctionName(moduleReference = entryModuleReference),
-                        arguments = emptyList(),
-                        returnCollector = null
+                val mirIrFunction = MidIrFunction(
+                    functionName = function.name,
+                    argumentTemps = allocatedArgs,
+                    mainBodyStatements = SimpleOptimizations.optimizeIr(
+                        statements = MidIrTraceReorganizer.reorder(
+                            allocator = allocator,
+                            originalStatements = loweredStatements + Return()
+                        )
                     ),
-                    Return(returnedExpression = null)
-                ),
-                numberOfArguments = 0,
-                hasReturn = false
-            )
+                    numberOfArguments = allocatedArgs.size,
+                    hasReturn = function.hasReturn
+                )
+                functions += TailRecursionOptimizer.optimize(mirIrFunction)
+                globalVariables += stringGlobalVariables
+            }
+        }
+        return MidIrCompilationUnit(globalVariables = globalVariables.toList(), functions = functions)
     }
+
+    private fun MidIrCompilationUnit.addMain(entryModuleReference: ModuleReference): MidIrCompilationUnit =
+        SimpleOptimizations.removeUnusedNames(
+            irCompilationUnit = MidIrCompilationUnit(
+                globalVariables = globalVariables,
+                functions = functions + getCompiledProgramMainFunction(entryModuleReference = entryModuleReference)
+            )
+        )
+
+    private fun getCompiledProgramMainFunction(entryModuleReference: ModuleReference): MidIrFunction =
+        MidIrFunction(
+            functionName = IrNameEncoder.compiledProgramMain,
+            argumentTemps = emptyList(),
+            mainBodyStatements = listOf(
+                CALL_FUNCTION(
+                    functionName = IrNameEncoder.encodeMainFunctionName(moduleReference = entryModuleReference),
+                    arguments = emptyList(),
+                    returnCollector = null
+                ),
+                Return(returnedExpression = null)
+            ),
+            numberOfArguments = 0,
+            hasReturn = false
+        )
 }
