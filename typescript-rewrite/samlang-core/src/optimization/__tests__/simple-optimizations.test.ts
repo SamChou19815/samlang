8c4ce13c77f92f6375ea3cea22ce34d218777b0a
Sam <sam@developersam.com>
2020-07-25 16:05:56 -0400

    [ts-rewrite][optimization] Implement simple optimizations for IR

---
+++ typescript-rewrite/samlang-core/src/optimization/__tests__/simple-optimizations.test.ts
@@ -0,0 +1,117 @@
+import {
+  MidIRStatement,
+  midIRStatementToString,
+  MIR_MOVE_TEMP,
+  MIR_LABEL,
+  MIR_JUMP,
+  MIR_CJUMP_FALLTHROUGH,
+  MIR_RETURN,
+  MIR_TEMP,
+} from '../../ast/mir';
+import { optimizeIr } from '../simple-optimizations';
+
+const optimizeAndConvertToString = (midIRStatements: readonly MidIRStatement[]): string =>
+  optimizeIr(midIRStatements).map(midIRStatementToString).join('\n');
+
+it('optimizeIr test.', () => {
+  expect(optimizeAndConvertToString([MIR_RETURN()])).toBe('return;');
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'A'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_LABEL('A'),
+      MIR_RETURN(),
+    ])
+  ).toBe(`if (boolVar) goto A;
+a = b;
+A:
+return;`);
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'A'),
+      MIR_LABEL('A'),
+      MIR_RETURN(),
+    ])
+  ).toBe(`return;`);
+
+  expect(optimizeAndConvertToString([MIR_JUMP('A'), MIR_LABEL('A'), MIR_JUMP('A')])).toBe(
+    'A:\ngoto A;'
+  );
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_LABEL('A'),
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'A'),
+      MIR_LABEL('B'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_JUMP('B'),
+      MIR_MOVE_TEMP(MIR_TEMP('c'), MIR_TEMP('d')),
+    ])
+  ).toBe(`A:
+if (boolVar) goto A;
+B:
+a = b;
+goto B;`);
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'A'),
+      MIR_JUMP('B'),
+      MIR_LABEL('A'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_LABEL('B'),
+    ])
+  ).toBe(`if (boolVar) goto A;
+goto B;
+A:
+a = b;
+B:`);
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_JUMP('C'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_MOVE_TEMP(MIR_TEMP('c'), MIR_TEMP('d')),
+      MIR_LABEL('C'),
+    ])
+  ).toBe('');
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'A'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_LABEL('A'),
+      MIR_LABEL('B'),
+      MIR_MOVE_TEMP(MIR_TEMP('c'), MIR_TEMP('d')),
+    ])
+  ).toBe(`if (boolVar) goto B;
+a = b;
+B:
+c = d;`);
+
+  expect(
+    optimizeAndConvertToString([
+      MIR_JUMP('A'),
+      MIR_LABEL('A'),
+      MIR_LABEL('B'),
+      MIR_LABEL('C'),
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('b')),
+      MIR_MOVE_TEMP(MIR_TEMP('c'), MIR_TEMP('d')),
+      MIR_LABEL('D'),
+      MIR_LABEL('E'),
+      MIR_LABEL('F'),
+      MIR_JUMP('G'),
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('boolVar'), 'G'),
+      MIR_LABEL('G'),
+      MIR_JUMP('C'),
+      MIR_RETURN(),
+      MIR_RETURN(),
+      MIR_RETURN(),
+    ])
+  ).toBe(`C:
+a = b;
+c = d;
+goto C;`);
+});
---
+++ typescript-rewrite/samlang-core/src/optimization/simple-optimizations.ts
@@ -0,0 +1,188 @@
+import ControlFlowGraph from '../analysis/control-flow-graph';
+import { MidIRStatement, MIR_JUMP, MIR_CJUMP_FALLTHROUGH } from '../ast/mir';
+import { isNotNull } from '../util/type-assertions';
+
+const pipe = <E>(element: E, ...functions: readonly ((element: E) => E)[]): E =>
+  functions.reduce((accumulator, f) => f(accumulator), element);
+
+const withoutUnreachableCode = <I>(
+  instructions: readonly I[],
+  controlFlowGraphConstructor: (instructions: readonly I[]) => ControlFlowGraph<I>
+): readonly I[] => {
+  const reachableSet = new Set<number>();
+  controlFlowGraphConstructor(instructions).dfs((node) => reachableSet.add(node.id));
+  return instructions.filter((_, index) => reachableSet.has(index));
+};
+
+const withoutUnreachableIRCode = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] =>
+  withoutUnreachableCode(statements, ControlFlowGraph.fromMidIRStatements);
+
+const coalesceConsecutiveLabelsForIr = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] => {
+  // If label A is immediately followed by label B, then A -> B should be in the mapping.
+  const nextEquivalentLabelMap = new Map<string, string>();
+  statements.forEach((statement, index) => {
+    if (index >= statements.length - 1) return;
+    if (statement.__type__ !== 'MidIRLabelStatement') return;
+    const nextStatement = statements[index + 1];
+    if (nextStatement.__type__ !== 'MidIRLabelStatement') return;
+    nextEquivalentLabelMap.set(statement.name, nextStatement.name);
+  });
+  if (nextEquivalentLabelMap.size === 0) {
+    return statements;
+  }
+
+  // It might be the case that we find something like l1 -> l2, l2 -> l3.
+  // This pass standardized the map into l1 -> l3, l2 -> l3.
+  const optimizedNextEquivalentLabelMap = new Map<string, string>();
+  nextEquivalentLabelMap.forEach((target, source) => {
+    let finalTarget: string = target;
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const nextTarget = nextEquivalentLabelMap.get(finalTarget);
+      if (nextTarget == null) break;
+      finalTarget = nextTarget;
+    }
+    optimizedNextEquivalentLabelMap.set(source, finalTarget);
+  });
+
+  return statements
+    .map((statement) => {
+      switch (statement.__type__) {
+        case 'MidIRLabelStatement':
+          return optimizedNextEquivalentLabelMap.has(statement.name) ? null : statement;
+        case 'MidIRJumpStatement': {
+          const optimizedLabel = optimizedNextEquivalentLabelMap.get(statement.label);
+          return optimizedLabel == null ? statement : MIR_JUMP(optimizedLabel);
+        }
+        case 'MidIRConditionalJumpFallThrough': {
+          const optimizedLabel = optimizedNextEquivalentLabelMap.get(statement.label1);
+          return optimizedLabel == null
+            ? statement
+            : MIR_CJUMP_FALLTHROUGH(statement.conditionExpression, optimizedLabel);
+        }
+        default:
+          return statement;
+      }
+    })
+    .filter(isNotNull);
+};
+
+const withoutConsecutiveJumpsInIr = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] => {
+  // If a LABEL A is followed by JUMP B, then A -> B will be in the map.
+  const singleJumpLabelMap = new Map<string, string>();
+  statements.forEach((statement, index) => {
+    if (index >= statements.length - 1) return;
+    if (statement.__type__ !== 'MidIRLabelStatement') return;
+    const nextStatement = statements[index + 1];
+    if (nextStatement.__type__ !== 'MidIRJumpStatement') return;
+    singleJumpLabelMap.set(statement.name, nextStatement.label);
+  });
+
+  // It might be the case that we find something like l1 -> l2, l2 -> l3.
+  // This pass standardized the map into l1 -> l3, l2 -> l3.
+  const optimizedJumpLabelMap = new Map<string, string>();
+  singleJumpLabelMap.forEach((target, source) => {
+    let finalTarget: string = target;
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const nextTarget = singleJumpLabelMap.get(finalTarget);
+      // nextTarget === finalTarget is used to prevent infinite loop caused self jump:
+      // e.g.
+      //   label A;
+      //   jump A;
+      if (nextTarget == null || nextTarget === finalTarget) break;
+      finalTarget = nextTarget;
+    }
+    optimizedJumpLabelMap.set(source, finalTarget);
+  });
+
+  return statements.map((statement) => {
+    switch (statement.__type__) {
+      case 'MidIRJumpStatement': {
+        const optimizedLabel = optimizedJumpLabelMap.get(statement.label);
+        return optimizedLabel == null ? statement : MIR_JUMP(optimizedLabel);
+      }
+      case 'MidIRConditionalJumpFallThrough': {
+        const optimizedLabel = optimizedJumpLabelMap.get(statement.label1);
+        return optimizedLabel == null
+          ? statement
+          : MIR_CJUMP_FALLTHROUGH(statement.conditionExpression, optimizedLabel);
+      }
+      default:
+        return statement;
+    }
+  });
+};
+
+const withoutImmediateJumpInIr = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] =>
+  statements
+    .map((statement, index) => {
+      if (index >= statements.length - 1) return statement;
+      const nextStatement = statements[index + 1];
+      if (nextStatement.__type__ !== 'MidIRLabelStatement') return statement;
+      const nextLabel = nextStatement.name;
+      if (statement.__type__ === 'MidIRJumpStatement') {
+        if (statement.label === nextLabel) {
+          // This is the case where we have JUMP A, LABEL A, we can omit the jump.
+          return null;
+        }
+      }
+      if (statement.__type__ === 'MidIRConditionalJumpFallThrough') {
+        if (statement.label1 === nextLabel) {
+          // This is the case where we have CJUMP(..., labelA); LABEL labelA.
+          // It seems to the case when the true label is the same as false label.
+          // Although unlikely, we can still optimize it away in case it happens.
+          return null;
+        }
+      }
+      return statement;
+    })
+    .filter(isNotNull);
+
+const withoutUnusedLabelInIr = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] => {
+  const usedLabels = new Set<string>();
+  statements.forEach((statement) => {
+    switch (statement.__type__) {
+      case 'MidIRJumpStatement':
+        usedLabels.add(statement.label);
+        break;
+      case 'MidIRConditionalJumpFallThrough':
+        usedLabels.add(statement.label1);
+        break;
+    }
+  });
+  return statements.filter(
+    (statement) => statement.__type__ !== 'MidIRLabelStatement' || usedLabels.has(statement.name)
+  );
+};
+
+/**
+ * Perform these optimizations and return an optimized sequence of mid IR statements.
+ *
+ * - unreachable code elimination
+ * - jump to immediate label elimination
+ * - unused label elimination
+ *
+ * @returns a list of all optimized statements.
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const optimizeIr = (statements: readonly MidIRStatement[]): readonly MidIRStatement[] =>
+  pipe(
+    statements,
+    coalesceConsecutiveLabelsForIr,
+    withoutConsecutiveJumpsInIr,
+    withoutUnreachableIRCode,
+    withoutConsecutiveJumpsInIr,
+    withoutImmediateJumpInIr,
+    withoutUnusedLabelInIr
+  );
