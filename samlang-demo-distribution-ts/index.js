d9c06dd29c610cff0814494c23a5fd52a1d4f6a6
Sam <sam@developersam.com>
2020-07-25 19:22:11 -0400

    [cleanup] Nuke dead code
    It's dead after a641d72ba61aed6659a07cb0f0898d62a19bc931

--- samlang-demo-distribution-ts/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./samlang-demo').samlang.demo.runDemo;
--- samlang-parser/src/jsMain/kotlin/samlang/parser/index.module_@dev-sam_samlang-parser-generated-ts.kt
+++
@@ -1,9 +0,0 @@
-@file:JsModule("@dev-sam/samlang-parser-generated-ts")
-@file:JsNonModule
-@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
-
-import kotlin.js.*
-
-external var buildTsModuleFromText: (text: String) -> TsModule
-
-external var buildTsExpressionFromText: (text: String) -> TsExpression
