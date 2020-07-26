05533bfc7f01c5511b78f0c9c0ca4b04980c1b88
Sam <sam@developersam.com>
2020-07-26 18:35:17 -0400

    [ts-rewrite][optimization] Implement copy propagation optimization

---
+++ typescript-rewrite/samlang-core/src/optimization/__tests__/copy-propagation.test.ts
@@ -0,0 +1,77 @@
+import {
+  MIR_ZERO,
+  MIR_ONE,
+  MIR_EIGHT,
+  MIR_TEMP,
+  MIR_IMMUTABLE_MEM,
+  MIR_MOVE_TEMP,
+  MIR_MOVE_IMMUTABLE_MEM,
+  MIR_CALL_FUNCTION,
+  MIR_CJUMP_FALLTHROUGH,
+  MIR_JUMP,
+  MIR_LABEL,
+  MIR_RETURN,
+  midIRStatementToString,
+  MIR_OP,
+} from '../../ast/mir';
+import optimizeIRWithCopyPropagation from '../copy-propagation-optimization';
+
+it('optimizeIRWithCopyPropagation test 1', () => {
+  expect(
+    optimizeIRWithCopyPropagation([
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_ONE),
+      MIR_MOVE_TEMP(MIR_TEMP('b'), MIR_ZERO),
+      MIR_MOVE_TEMP(MIR_TEMP('c'), MIR_EIGHT),
+      MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_TEMP('a')),
+      MIR_MOVE_TEMP(MIR_TEMP('y'), MIR_TEMP('b')),
+      MIR_MOVE_TEMP(MIR_TEMP('z'), MIR_TEMP('c')),
+      MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_TEMP('b')),
+      MIR_CALL_FUNCTION('fff', [], 'y'),
+      MIR_MOVE_TEMP(MIR_TEMP('z'), MIR_TEMP('x')),
+      MIR_CALL_FUNCTION('fff', [MIR_TEMP('z')]),
+      MIR_RETURN(MIR_TEMP('z')),
+      MIR_RETURN(),
+    ])
+      .map(midIRStatementToString)
+      .join('\n')
+  ).toBe(`a = 1;
+b = 0;
+c = 8;
+x = a;
+y = b;
+z = c;
+x = b;
+y = fff();
+z = b;
+fff(b);
+return b;
+return;`);
+});
+
+it('optimizeIRWithCopyPropagation test 2', () => {
+  expect(
+    optimizeIRWithCopyPropagation([
+      MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_ONE),
+      MIR_MOVE_TEMP(MIR_TEMP('b'), MIR_OP('+', MIR_ZERO, MIR_ZERO)),
+      MIR_CJUMP_FALLTHROUGH(MIR_TEMP('a'), 'true'),
+      MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_TEMP('a')),
+      MIR_JUMP('end'),
+      MIR_LABEL('true'),
+      MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_TEMP('b')),
+      MIR_LABEL('end'),
+      MIR_MOVE_TEMP(MIR_TEMP('y'), MIR_TEMP('x')),
+      MIR_MOVE_IMMUTABLE_MEM(MIR_IMMUTABLE_MEM(MIR_TEMP('y')), MIR_IMMUTABLE_MEM(MIR_TEMP('x'))),
+    ])
+      .map(midIRStatementToString)
+      .join(`\n`)
+  ).toBe(`a = 1;
+b = (0 + 0);
+if (a) goto true;
+x = a;
+goto end;
+true:
+x = b;
+end:
+y = x;
+MEM[x] = MEM[x];`);
+});
---
+++ typescript-rewrite/samlang-core/src/optimization/copy-propagation-optimization.ts
@@ -0,0 +1,100 @@
+import analyzeAvailableCopies from '../analysis/available-copy-analysis';
+import {
+  MidIRStatement,
+  MidIRExpression,
+  MIR_TEMP,
+  MIR_IMMUTABLE_MEM,
+  MIR_OP,
+  MIR_RETURN,
+} from '../ast/mir';
+
+const replaceMidIRExpressionAccordingToAvailableCopies = (
+  availableCopies: Readonly<Record<string, string | undefined>>,
+  expression: MidIRExpression
+): MidIRExpression => {
+  switch (expression.__type__) {
+    case 'MidIRConstantExpression':
+    case 'MidIRNameExpression':
+      return expression;
+    case 'MidIRTemporaryExpression':
+      return MIR_TEMP(availableCopies[expression.temporaryID] ?? expression.temporaryID);
+    case 'MidIRImmutableMemoryExpression':
+      return MIR_IMMUTABLE_MEM(
+        replaceMidIRExpressionAccordingToAvailableCopies(
+          availableCopies,
+          expression.indexExpression
+        )
+      );
+    case 'MidIRBinaryExpression':
+      return MIR_OP(
+        expression.operator,
+        replaceMidIRExpressionAccordingToAvailableCopies(availableCopies, expression.e1),
+        replaceMidIRExpressionAccordingToAvailableCopies(availableCopies, expression.e2)
+      );
+  }
+};
+
+const rewriteMidIRStatementAccordingToAvailableCopies = (
+  availableCopies: Readonly<Record<string, string | undefined>>,
+  statement: MidIRStatement
+): MidIRStatement => {
+  switch (statement.__type__) {
+    case 'MidIRMoveTempStatement':
+      return {
+        ...statement,
+        source: replaceMidIRExpressionAccordingToAvailableCopies(availableCopies, statement.source),
+      };
+    case 'MidIRMoveMemStatement':
+      return {
+        ...statement,
+        memoryIndexExpression: replaceMidIRExpressionAccordingToAvailableCopies(
+          availableCopies,
+          statement.memoryIndexExpression
+        ),
+        source: replaceMidIRExpressionAccordingToAvailableCopies(availableCopies, statement.source),
+      };
+    case 'MidIRCallFunctionStatement':
+      return {
+        ...statement,
+        functionExpression: replaceMidIRExpressionAccordingToAvailableCopies(
+          availableCopies,
+          statement.functionExpression
+        ),
+        functionArguments: statement.functionArguments.map((it) =>
+          replaceMidIRExpressionAccordingToAvailableCopies(availableCopies, it)
+        ),
+      };
+    case 'MidIRJumpStatement':
+    case 'MidIRLabelStatement':
+      return statement;
+    case 'MidIRConditionalJumpFallThrough':
+      return {
+        ...statement,
+        conditionExpression: replaceMidIRExpressionAccordingToAvailableCopies(
+          availableCopies,
+          statement.conditionExpression
+        ),
+      };
+    case 'MidIRReturnStatement':
+      if (statement.returnedExpression == null) {
+        return MIR_RETURN();
+      }
+      return MIR_RETURN(
+        replaceMidIRExpressionAccordingToAvailableCopies(
+          availableCopies,
+          statement.returnedExpression
+        )
+      );
+  }
+};
+
+const optimizeIRWithCopyPropagation = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] => {
+  const availableCopies = analyzeAvailableCopies(statements);
+  return statements.map((statement, index) =>
+    rewriteMidIRStatementAccordingToAvailableCopies(availableCopies[index], statement)
+  );
+};
+
+export default optimizeIRWithCopyPropagation;
