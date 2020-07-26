a54f51c8f92bb8d59c32b19ab3ecc1169902c2c4
Sam <sam@developersam.com>
2020-07-26 16:44:09 -0400

    [ts-rewrite][optimization] Implement dead code elimination optimization

---
+++ typescript-rewrite/samlang-core/src/optimization/__tests__/dead-code-elimination-optimization.test.ts
@@ -0,0 +1,52 @@
+import {
+  MIR_ZERO,
+  MIR_ONE,
+  MIR_TEMP,
+  MIR_OP,
+  MIR_MOVE_TEMP,
+  MIR_MOVE_IMMUTABLE_MEM,
+  MIR_CALL_FUNCTION,
+  MIR_CJUMP_FALLTHROUGH,
+  MIR_JUMP,
+  MIR_LABEL,
+  MIR_RETURN,
+  MIR_IMMUTABLE_MEM,
+} from '../../ast/mir';
+import optimizeIRWithDeadCodeElimination from '../dead-code-elimination-optimization';
+
+it('optimizeIRWithDeadCodeElimination test', () => {
+  expect(
+    optimizeIRWithDeadCodeElimination([
+      /* 00 */ MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_ONE),
+      /* 01 */ MIR_CJUMP_FALLTHROUGH(MIR_OP('<', MIR_TEMP('x'), MIR_ONE), 'true'),
+      /* 02 */ MIR_CALL_FUNCTION('f', [MIR_ONE], 'z2'),
+      /* 03 */ MIR_MOVE_TEMP(MIR_TEMP('z2'), MIR_OP('/', MIR_ONE, MIR_ZERO)),
+      /* 04 */ MIR_JUMP('r'),
+      /* 05 */ MIR_LABEL('r'),
+      /* 06 */ MIR_RETURN(),
+      /* 07 */ MIR_LABEL('true'),
+      /* 08 */ MIR_MOVE_TEMP(MIR_TEMP('y'), MIR_OP('+', MIR_ONE, MIR_TEMP('x'))),
+      /* 09 */ MIR_MOVE_TEMP(MIR_TEMP('z1'), MIR_OP('*', MIR_ONE, MIR_IMMUTABLE_MEM(MIR_ONE))),
+      /* 10 */ MIR_MOVE_TEMP(MIR_TEMP('z2'), MIR_OP('/', MIR_ONE, MIR_ZERO)),
+      /* 11 */ MIR_LABEL('end'),
+      /* 12 */ MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_OP('!=', MIR_TEMP('y'), MIR_TEMP('z2'))),
+      /* 13 */ MIR_RETURN(MIR_TEMP('a')),
+      /* 14 */ MIR_RETURN(),
+    ])
+  ).toEqual([
+    /* 00 */ MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_ONE),
+    /* 01 */ MIR_CJUMP_FALLTHROUGH(MIR_OP('<', MIR_TEMP('x'), MIR_ONE), 'true'),
+    /* 02 */ MIR_CALL_FUNCTION('f', [MIR_ONE], 'z2'),
+    /* 03 */ MIR_MOVE_TEMP(MIR_TEMP('z2'), MIR_OP('/', MIR_ONE, MIR_ZERO)),
+    /* 04 */ MIR_JUMP('r'),
+    /* 05 */ MIR_LABEL('r'),
+    /* 06 */ MIR_RETURN(),
+    /* 07 */ MIR_LABEL('true'),
+    /* 08 */ MIR_MOVE_TEMP(MIR_TEMP('y'), MIR_OP('+', MIR_ONE, MIR_TEMP('x'))),
+    /* 10 */ MIR_MOVE_TEMP(MIR_TEMP('z2'), MIR_OP('/', MIR_ONE, MIR_ZERO)),
+    /* 11 */ MIR_LABEL('end'),
+    /* 12 */ MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_OP('!=', MIR_TEMP('y'), MIR_TEMP('z2'))),
+    /* 13 */ MIR_RETURN(MIR_TEMP('a')),
+    /* 14 */ MIR_RETURN(),
+  ]);
+});
---
+++ typescript-rewrite/samlang-core/src/optimization/dead-code-elimination-optimization.ts
@@ -0,0 +1,40 @@
+import analyzeLiveTemporariesAtTheEndOfEachStatement from '../analysis/live-temp-analysis';
+import type { MidIRStatement, MidIRExpression } from '../ast/mir';
+
+/** Some expressions might trigger exceptions, removing them changes the behavior of programs. */
+const isMidIRExpressionUnsafeToRemove = (expression: MidIRExpression): boolean => {
+  switch (expression.__type__) {
+    case 'MidIRConstantExpression':
+    case 'MidIRTemporaryExpression':
+    case 'MidIRNameExpression':
+      return false;
+    case 'MidIRBinaryExpression':
+      switch (expression.operator) {
+        case '/':
+        case '%':
+          return true;
+        default:
+          return (
+            isMidIRExpressionUnsafeToRemove(expression.e1) ||
+            isMidIRExpressionUnsafeToRemove(expression.e2)
+          );
+      }
+    case 'MidIRImmutableMemoryExpression':
+      return isMidIRExpressionUnsafeToRemove(expression.indexExpression);
+  }
+};
+
+const optimizeIRWithDeadCodeElimination = (
+  statements: readonly MidIRStatement[]
+): readonly MidIRStatement[] => {
+  const liveTemporariesAtTheEndOfEachStatement = analyzeLiveTemporariesAtTheEndOfEachStatement(
+    statements
+  );
+  return statements.filter((statement, index) => {
+    if (statement.__type__ !== 'MidIRMoveTempStatement') return true;
+    if (liveTemporariesAtTheEndOfEachStatement[index].has(statement.temporaryID)) return true;
+    return isMidIRExpressionUnsafeToRemove(statement.source);
+  });
+};
+
+export default optimizeIRWithDeadCodeElimination;
