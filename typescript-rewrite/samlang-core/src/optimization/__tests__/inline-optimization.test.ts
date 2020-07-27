dac69143a44640f14ab7539a2442ed5478ab3fc8
Sam <sam@developersam.com>
2020-07-26 20:23:19 -0400

    [ts-rewrite][optimization] Implement inlining optimizer

--- typescript-rewrite/samlang-core/src/optimization/__tests__/inline-optimization.test.ts
+++ typescript-rewrite/samlang-core/src/optimization/__tests__/inline-optimization.test.ts
@@ -10,9 +10,14 @@ import {
   MIR_TEMP,
   MIR_IMMUTABLE_MEM,
   MIR_OP,
+  midIRCompilationUnitToString,
+  MIR_ONE,
 } from '../../ast/mir';
-// eslint-disable-next-line camelcase
-import { estimateMidIRFunctionInlineCost_EXPOSED_FOR_TESTING } from '../inline-optimization';
+import optimizeMidIRCompilationUnitByInlining, {
+  InliningResourceAllocator,
+  // eslint-disable-next-line camelcase
+  estimateMidIRFunctionInlineCost_EXPOSED_FOR_TESTING,
+} from '../inline-optimization';
 
 it('estimateMidIRFunctionInlineCost test', () => {
   expect(
@@ -28,8 +33,184 @@ it('estimateMidIRFunctionInlineCost test', () => {
         MIR_RETURN(), // 1,
         MIR_CJUMP_FALLTHROUGH(MIR_TEMP(''), ''), // 1
         MIR_RETURN(MIR_OP('+', MIR_ZERO, MIR_ZERO)), // 2
-        MIR_CALL_FUNCTION('f', [MIR_TEMP('')]), // 5 + 1 = 6
+        MIR_CALL_FUNCTION('f', [MIR_TEMP('')]), // 10 + 1 = 11
       ],
     })
-  ).toBe(14);
+  ).toBe(19);
+});
+
+it('optimizeMidIRCompilationUnitByInlining test 1', () => {
+  expect(
+    midIRCompilationUnitToString(
+      optimizeMidIRCompilationUnitByInlining(
+        {
+          globalVariables: [],
+          functions: [
+            {
+              functionName: 'factorial',
+              argumentNames: ['n', 'acc'],
+              hasReturn: true,
+              mainBodyStatements: [
+                MIR_CJUMP_FALLTHROUGH(MIR_OP('==', MIR_TEMP('n'), MIR_ZERO), 'LABEL_RETURN_ACC'),
+                MIR_CALL_FUNCTION(
+                  'factorial',
+                  [
+                    MIR_OP('-', MIR_TEMP('n'), MIR_ONE),
+                    MIR_OP('*', MIR_TEMP('acc'), MIR_TEMP('n')),
+                  ],
+                  'dummy'
+                ),
+                MIR_RETURN(MIR_TEMP('dummy')),
+                MIR_LABEL('LABEL_RETURN_ACC'),
+                MIR_RETURN(MIR_TEMP('acc')),
+              ],
+            },
+            {
+              functionName: 'infiniteLoop',
+              argumentNames: [],
+              hasReturn: false,
+              mainBodyStatements: [MIR_CALL_FUNCTION('infiniteLoop', []), MIR_RETURN()],
+            },
+            {
+              functionName: 'insanelyBigFunction',
+              argumentNames: ['a'],
+              hasReturn: false,
+              mainBodyStatements: [
+                MIR_CALL_FUNCTION('moveMove', [MIR_TEMP('a')]),
+                MIR_CALL_FUNCTION(MIR_TEMP('a'), []),
+                ...Array.from(new Array(10).keys()).map(() =>
+                  MIR_CALL_FUNCTION('non-existing-function', [])
+                ),
+              ],
+            },
+            {
+              functionName: 'moveMove',
+              argumentNames: ['a'],
+              hasReturn: false,
+              mainBodyStatements: [
+                MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_TEMP('a')),
+                MIR_MOVE_IMMUTABLE_MEM(
+                  MIR_IMMUTABLE_MEM(MIR_TEMP('a')),
+                  MIR_IMMUTABLE_MEM(MIR_TEMP('a'))
+                ),
+                MIR_JUMP('lll'),
+                MIR_LABEL('lll'),
+                MIR_RETURN(),
+              ],
+            },
+          ],
+        },
+        new InliningResourceAllocator()
+      )
+    )
+  ).toBe(`
+function factorial {
+  let n = _ARG0;
+  let acc = _ARG1;
+
+  if ((n == 0)) goto LABEL_RETURN_ACC;
+  _INLINING_1_n = (n - 1);
+  _INLINING_1_acc = (acc * n);
+  if ((_INLINING_1_n == 0)) goto INLINING_0_LABEL_RETURN_ACC;
+  _INLINING_1_dummy = factorial((_INLINING_1_n - 1), (_INLINING_1_acc * _INLINING_1_n));
+  dummy = _INLINING_1_dummy;
+  goto INLINING_0___INLINING_END;
+  INLINING_0_LABEL_RETURN_ACC:
+  dummy = _INLINING_1_acc;
+  INLINING_0___INLINING_END:
+  return dummy;
+  LABEL_RETURN_ACC:
+  return acc;
+}
+
+function infiniteLoop {
+
+  infiniteLoop();
+  return;
+}
+
+function insanelyBigFunction {
+  let a = _ARG0;
+
+  _INLINING_5_a = a;
+  _INLINING_5_a = _INLINING_5_a;
+  MEM[_INLINING_5_a] = MEM[_INLINING_5_a];
+  a();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+  non-existing-function();
+}
+
+function moveMove {
+  let a = _ARG0;
+
+  a = a;
+  MEM[a] = MEM[a];
+  return;
+}
+`);
+});
+
+it('optimizeMidIRCompilationUnitByInlining test 2', () => {
+  expect(
+    midIRCompilationUnitToString(
+      optimizeMidIRCompilationUnitByInlining(
+        {
+          globalVariables: [],
+          functions: [],
+        },
+        new InliningResourceAllocator()
+      )
+    )
+  ).toBe('\n');
+});
+
+it('optimizeMidIRCompilationUnitByInlining test 3', () => {
+  expect(
+    midIRCompilationUnitToString(
+      optimizeMidIRCompilationUnitByInlining(
+        {
+          globalVariables: [],
+          functions: [
+            {
+              functionName: 'emptyFunction',
+              argumentNames: [],
+              hasReturn: true,
+              mainBodyStatements: [MIR_RETURN()],
+            },
+            {
+              functionName: 'insanelyBigFunction',
+              argumentNames: ['a'],
+              hasReturn: false,
+              mainBodyStatements: [
+                ...Array.from(new Array(105).keys()).map(() =>
+                  MIR_CALL_FUNCTION('non-existing-function', [])
+                ),
+                MIR_RETURN(),
+              ],
+            },
+          ],
+        },
+        new InliningResourceAllocator()
+      )
+    )
+  ).toBe(`
+function emptyFunction {
+
+  return;
+}
+
+function insanelyBigFunction {
+  let a = _ARG0;
+
+${'  non-existing-function();\n'.repeat(105)}  return;
+}
+`);
 });
--- typescript-rewrite/samlang-core/src/optimization/inline-optimization.ts
+++ typescript-rewrite/samlang-core/src/optimization/inline-optimization.ts
@@ -1,4 +1,25 @@
-import { MidIRExpression, MidIRStatement, MidIRFunction } from '../ast/mir';
+import {
+  MidIRExpression,
+  MidIRStatement,
+  MidIRFunction,
+  MidIRCompilationUnit,
+  MIR_TEMP,
+  MIR_IMMUTABLE_MEM,
+  MIR_OP,
+  MIR_MOVE_TEMP,
+  MIR_MOVE_IMMUTABLE_MEM,
+  MIR_CALL_FUNCTION,
+  MIR_JUMP,
+  MIR_LABEL,
+  MIR_CJUMP_FALLTHROUGH,
+} from '../ast/mir';
+import { assertNotNull } from '../util/type-assertions';
+import { optimizeIrWithSimpleOptimization } from './simple-optimizations';
+
+/** The threshold max tolerable cost of inlining.  */
+const INLINE_THRESHOLD = 25;
+/** The threshold max tolerable cost of performing inlining.  */
+const PERFORM_INLINE_THRESHOLD = 1000;
 
 const estimateMidIRExpressionInlineCost = (expression: MidIRExpression): number => {
   switch (expression.__type__) {
@@ -28,7 +49,7 @@ const estimateMidIRStatementInlineCost = (statement: MidIRStatement): number => 
         estimateMidIRExpressionInlineCost(statement.source)
       );
     case 'MidIRCallFunctionStatement': {
-      let sum = 5;
+      let sum = 10;
       sum += estimateMidIRExpressionInlineCost(statement.functionExpression);
       statement.functionArguments.forEach((it) => {
         sum += 1 + estimateMidIRExpressionInlineCost(it);
@@ -60,3 +81,217 @@ export const estimateMidIRFunctionInlineCost_EXPOSED_FOR_TESTING = (
   });
   return sum;
 };
+
+const getFunctionsToInline = ({
+  functions,
+}: MidIRCompilationUnit): {
+  readonly functionsThatCanPerformInlining: ReadonlySet<string>;
+  readonly functionsThatCanBeInlined: ReadonlySet<string>;
+} => {
+  const functionsThatCanBeInlined = new Set<string>();
+  const functionsThatCanPerformInlining = new Set<string>();
+
+  functions.forEach((midIRFunction) => {
+    const cost = estimateMidIRFunctionInlineCost_EXPOSED_FOR_TESTING(midIRFunction);
+    if (cost <= INLINE_THRESHOLD) {
+      functionsThatCanBeInlined.add(midIRFunction.functionName);
+    }
+    if (cost <= PERFORM_INLINE_THRESHOLD) {
+      functionsThatCanPerformInlining.add(midIRFunction.functionName);
+    }
+  });
+
+  return { functionsThatCanPerformInlining, functionsThatCanBeInlined };
+};
+
+export class InliningResourceAllocator {
+  private id = 0;
+
+  allocateInliningTemporaryPrefix(): string {
+    const prefix = `_INLINING_${this.id}_`;
+    this.id += 1;
+    return prefix;
+  }
+
+  allocateInliningLabelPrefix(): string {
+    const prefix = `INLINING_${this.id}_`;
+    this.id += 1;
+    return prefix;
+  }
+}
+
+const inlineRewriteForMidIRExpression = (
+  prefix: string,
+  expression: MidIRExpression
+): MidIRExpression => {
+  switch (expression.__type__) {
+    case 'MidIRConstantExpression':
+    case 'MidIRNameExpression':
+      return expression;
+    case 'MidIRTemporaryExpression':
+      return MIR_TEMP(`${prefix}${expression.temporaryID}`);
+    case 'MidIRImmutableMemoryExpression':
+      return MIR_IMMUTABLE_MEM(inlineRewriteForMidIRExpression(prefix, expression.indexExpression));
+    case 'MidIRBinaryExpression':
+      return MIR_OP(
+        expression.operator,
+        inlineRewriteForMidIRExpression(prefix, expression.e1),
+        inlineRewriteForMidIRExpression(prefix, expression.e2)
+      );
+  }
+};
+
+const inlineRewriteForMidIRStatement = (
+  labelPrefix: string,
+  temporaryPrefix: string,
+  returnCollectorTemporaryID: string | undefined,
+  statement: MidIRStatement
+): readonly MidIRStatement[] => {
+  switch (statement.__type__) {
+    case 'MidIRMoveTempStatement':
+      return [
+        MIR_MOVE_TEMP(
+          MIR_TEMP(`${temporaryPrefix}${statement.temporaryID}`),
+          inlineRewriteForMidIRExpression(temporaryPrefix, statement.source)
+        ),
+      ];
+    case 'MidIRMoveMemStatement':
+      return [
+        MIR_MOVE_IMMUTABLE_MEM(
+          MIR_IMMUTABLE_MEM(
+            inlineRewriteForMidIRExpression(temporaryPrefix, statement.memoryIndexExpression)
+          ),
+          inlineRewriteForMidIRExpression(temporaryPrefix, statement.source)
+        ),
+      ];
+    case 'MidIRCallFunctionStatement':
+      return [
+        MIR_CALL_FUNCTION(
+          inlineRewriteForMidIRExpression(temporaryPrefix, statement.functionExpression),
+          statement.functionArguments.map((it) =>
+            inlineRewriteForMidIRExpression(temporaryPrefix, it)
+          ),
+          statement.returnCollectorTemporaryID == null
+            ? undefined
+            : `${temporaryPrefix}${statement.returnCollectorTemporaryID}`
+        ),
+      ];
+    case 'MidIRJumpStatement':
+      return [MIR_JUMP(`${labelPrefix}${statement.label}`)];
+    case 'MidIRLabelStatement':
+      return [MIR_LABEL(`${labelPrefix}${statement.name}`)];
+    case 'MidIRConditionalJumpFallThrough':
+      return [
+        MIR_CJUMP_FALLTHROUGH(
+          inlineRewriteForMidIRExpression(temporaryPrefix, statement.conditionExpression),
+          `${labelPrefix}${statement.label1}`
+        ),
+      ];
+    case 'MidIRReturnStatement':
+      if (statement.returnedExpression == null) return [MIR_JUMP(`${labelPrefix}__INLINING_END`)];
+      assertNotNull(returnCollectorTemporaryID);
+      return [
+        MIR_MOVE_TEMP(
+          MIR_TEMP(returnCollectorTemporaryID),
+          inlineRewriteForMidIRExpression(temporaryPrefix, statement.returnedExpression)
+        ),
+        MIR_JUMP(`${labelPrefix}__INLINING_END`),
+      ];
+  }
+};
+
+const performInlineRewriteOnFunction = (
+  midIRFunction: MidIRFunction,
+  functionsThatCanBeInlined: ReadonlySet<string>,
+  allFunctions: Record<string, MidIRFunction>,
+  allocator: InliningResourceAllocator
+): MidIRFunction => {
+  const newMainBodyStatements: MidIRStatement[] = [];
+
+  midIRFunction.mainBodyStatements.forEach((oldMainBodyStatement) => {
+    if (oldMainBodyStatement.__type__ !== 'MidIRCallFunctionStatement') {
+      newMainBodyStatements.push(oldMainBodyStatement);
+      return;
+    }
+    const {
+      functionExpression,
+      functionArguments,
+      returnCollectorTemporaryID,
+    } = oldMainBodyStatement;
+    if (functionExpression.__type__ !== 'MidIRNameExpression') {
+      newMainBodyStatements.push(oldMainBodyStatement);
+      return;
+    }
+    const functionName = functionExpression.name;
+    if (!functionsThatCanBeInlined.has(functionName)) {
+      newMainBodyStatements.push(oldMainBodyStatement);
+      return;
+    }
+
+    const {
+      argumentNames: argumentNamesOfFunctionToBeInlined,
+      mainBodyStatements: mainBodyStatementsOfFunctionToBeInlined,
+    } = allFunctions[functionName];
+    const labelPrefix = allocator.allocateInliningLabelPrefix();
+    const temporaryPrefix = allocator.allocateInliningTemporaryPrefix();
+    newMainBodyStatements.push(
+      // inline step 1: move args to args temp
+      ...argumentNamesOfFunctionToBeInlined.map((parameter, index) =>
+        MIR_MOVE_TEMP(MIR_TEMP(`${temporaryPrefix}${parameter}`), functionArguments[index])
+      ),
+      // inline step 2: add in body code and change return statements and label prefix
+      ...mainBodyStatementsOfFunctionToBeInlined
+        .map((it) =>
+          inlineRewriteForMidIRStatement(
+            labelPrefix,
+            temporaryPrefix,
+            returnCollectorTemporaryID,
+            it
+          )
+        )
+        .flat(),
+      // inline step 3: mark the end of inlining for return
+      MIR_LABEL(`${labelPrefix}__INLINING_END`)
+    );
+  });
+
+  return {
+    ...midIRFunction,
+    mainBodyStatements: optimizeIrWithSimpleOptimization(newMainBodyStatements),
+  };
+};
+
+const optimizeMidIRCompilationUnitByInlining = (
+  compilationUnit: MidIRCompilationUnit,
+  allocator: InliningResourceAllocator
+): MidIRCompilationUnit => {
+  let tempCompilationUnit = compilationUnit;
+  for (let i = 0; i < 5; i += 1) {
+    const { functionsThatCanBeInlined, functionsThatCanPerformInlining } = getFunctionsToInline(
+      tempCompilationUnit
+    );
+    if (functionsThatCanBeInlined.size === 0) {
+      return tempCompilationUnit;
+    }
+    const allFunctions = Object.fromEntries(
+      tempCompilationUnit.functions.map((it) => [it.functionName, it])
+    );
+    tempCompilationUnit = {
+      ...compilationUnit,
+      functions: tempCompilationUnit.functions.map((oldFunction) => {
+        if (functionsThatCanPerformInlining.has(oldFunction.functionName)) {
+          return performInlineRewriteOnFunction(
+            oldFunction,
+            functionsThatCanBeInlined,
+            allFunctions,
+            allocator
+          );
+        }
+        return oldFunction;
+      }),
+    };
+  }
+  return tempCompilationUnit;
+};
+
+export default optimizeMidIRCompilationUnitByInlining;
