255362a7097f755854ffaeed9255a614b5804e62
Sam <sam@developersam.com>
2020-07-29 22:06:54 -0400

    [ts-rewrite][analysis] Fix available expression analysis to be compatible with kotlin code
    The old kotlin code is wrong, thus the migrated TS code is also wrong.
    This commit keeps the new TS code in sync with the kotlin code for correctness.

--- typescript-rewrite/samlang-core/src/analysis/available-expressions-analysis.ts
+++ typescript-rewrite/samlang-core/src/analysis/available-expressions-analysis.ts
@@ -1,5 +1,6 @@
 import { MidIRExpression, MidIRStatement, midIRExpressionToString } from '../ast/mir';
 import { mapEquals } from '../util/collections';
+import { isNotNull } from '../util/type-assertions';
 import ControlFlowGraph from './control-flow-graph';
 import { DataflowAnalysisGraphOperator, runForwardDataflowAnalysis } from './dataflow-analysis';
 
@@ -55,8 +56,8 @@ const collectSubExpressionsFromMidIRStatement = (
   }
 };
 
-/** String of expression => statement id where the expression first appear. */
-type AvailableExpressionToSourceMapping = ReadonlyMap<string, number>;
+/** String of expression => statement ids where the expression first appear. */
+type AvailableExpressionToSourceMapping = ReadonlyMap<string, readonly number[]>;
 
 const operator: DataflowAnalysisGraphOperator<
   MidIRStatement,
@@ -65,29 +66,33 @@ const operator: DataflowAnalysisGraphOperator<
   graphConstructor: ControlFlowGraph.fromMidIRStatements,
   edgeInitializer: () => new Map(),
   joinEdges: (parentOutEdges) => {
-    const newInEdgeMapping = new Map<string, number>();
-    const conflictingSourceExpressionSet = new Set<string>();
-    parentOutEdges.forEach((parentOutEdge) => {
-      parentOutEdge.forEach((statementId, availableExpression) => {
-        const existingStatementId = newInEdgeMapping.get(availableExpression);
-        if (existingStatementId == null) {
-          newInEdgeMapping.set(availableExpression, statementId);
-        }
-        if (existingStatementId != null && statementId !== existingStatementId) {
-          conflictingSourceExpressionSet.add(availableExpression);
-        }
-      });
+    if (parentOutEdges.length === 0) {
+      return new Map();
+    }
+    const newInEdgeMapping = new Map<string, number[]>();
+    const otherParents = parentOutEdges.slice(1);
+    parentOutEdges[0].forEach((statementIds, availableExpression) => {
+      const appearIdsFromOtherParentsWithNull = otherParents.map((map) =>
+        map.get(availableExpression)
+      );
+      const appearIdsFromOtherParents = appearIdsFromOtherParentsWithNull.filter(isNotNull);
+      if (appearIdsFromOtherParents.length === appearIdsFromOtherParentsWithNull.length) {
+        // Now we established that this `availableExpression` is indeed common.
+        newInEdgeMapping.set(
+          availableExpression,
+          Array.from(new Set([...appearIdsFromOtherParents, statementIds].flat()))
+        );
+      }
     });
-    conflictingSourceExpressionSet.forEach((expressionWithConflictingSource) =>
-      newInEdgeMapping.delete(expressionWithConflictingSource)
-    );
     return newInEdgeMapping;
   },
   computeNewEdge: (newInEdge, statement, nodeID) => {
     const newOutEdge = new Map(newInEdge);
     collectSubExpressionsFromMidIRStatement(statement).forEach((availableExpression) => {
+      // We do nothing if things are already there, because we want to keep the first appearance source.
+      // It is safe to do because all temps will only be assigned once.
       if (!newOutEdge.has(availableExpression)) {
-        newOutEdge.set(availableExpression, nodeID);
+        newOutEdge.set(availableExpression, [nodeID]);
       }
     });
     return newOutEdge;
