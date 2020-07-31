49766720e3f63a91708f983007fc9462011f6612
Sam <sam@developersam.com>
2020-07-30 23:30:05 -0400

    [ts-rewrite][optimization] Connect together all IR optimizations in optimization entry point

---
+++ typescript-rewrite/samlang-core/src/optimization/__tests__/index.test.ts
@@ -0,0 +1,90 @@
+import optimizeIRCompilationUnit from '..';
+import {
+  MidIRCompilationUnit,
+  MIR_ONE,
+  MIR_TEMP,
+  MIR_OP,
+  MIR_MOVE_TEMP,
+  MIR_MOVE_IMMUTABLE_MEM,
+  MIR_CALL_FUNCTION,
+  MIR_CJUMP_FALLTHROUGH,
+  MIR_JUMP,
+  MIR_LABEL,
+  MIR_RETURN,
+  MIR_IMMUTABLE_MEM,
+  midIRCompilationUnitToString,
+} from '../../ast/mir';
+
+const compilationUnit: MidIRCompilationUnit = {
+  globalVariables: [],
+  functions: [
+    {
+      functionName: 'fooBar',
+      argumentNames: [],
+      hasReturn: true,
+      mainBodyStatements: [
+        MIR_MOVE_TEMP(MIR_TEMP('x'), MIR_ONE),
+        MIR_CJUMP_FALLTHROUGH(MIR_OP('<', MIR_TEMP('x'), MIR_ONE), 'true'),
+        MIR_CALL_FUNCTION('f', [MIR_ONE], 'z2'),
+        MIR_MOVE_IMMUTABLE_MEM(
+          MIR_IMMUTABLE_MEM(MIR_TEMP('z2')),
+          MIR_OP('+', MIR_ONE, MIR_TEMP('x'))
+        ),
+        MIR_JUMP('r'),
+        MIR_LABEL('r'),
+        MIR_JUMP('end'),
+        MIR_LABEL('true'),
+        MIR_MOVE_TEMP(MIR_TEMP('y'), MIR_OP('+', MIR_ONE, MIR_TEMP('x'))),
+        MIR_MOVE_TEMP(
+          MIR_TEMP('z1'),
+          MIR_OP('*', MIR_OP('+', MIR_ONE, MIR_TEMP('x')), MIR_IMMUTABLE_MEM(MIR_ONE))
+        ),
+        MIR_MOVE_TEMP(
+          MIR_TEMP('z2'),
+          MIR_OP(
+            '/',
+            MIR_OP('*', MIR_OP('+', MIR_ONE, MIR_TEMP('x')), MIR_IMMUTABLE_MEM(MIR_ONE)),
+            MIR_OP('+', MIR_ONE, MIR_TEMP('x'))
+          )
+        ),
+        MIR_LABEL('end'),
+        MIR_MOVE_TEMP(MIR_TEMP('a'), MIR_OP('!=', MIR_TEMP('y'), MIR_TEMP('z2'))),
+        MIR_RETURN(MIR_TEMP('a')),
+      ],
+    },
+  ],
+};
+
+// TODO: replace this test with good integration tests once interpreters are ready.
+
+it('optimizeIRCompilationUnit all enabled test', () => {
+  expect(midIRCompilationUnitToString(optimizeIRCompilationUnit(compilationUnit))).toBe(`
+function fooBar {
+
+  z2 = f(1);
+  MEM[z2] = 2;
+  a = (2 != z2);
+  return a;
+}
+`);
+});
+
+it('optimizeIRCompilationUnit all disabled test', () => {
+  expect(midIRCompilationUnitToString(optimizeIRCompilationUnit(compilationUnit, {}))).toBe(`
+function fooBar {
+
+  x = 1;
+  if ((x < 1)) goto true;
+  z2 = f(1);
+  MEM[z2] = (1 + x);
+  goto end;
+  true:
+  y = (1 + x);
+  z1 = ((1 + x) * MEM[1]);
+  z2 = (((1 + x) * MEM[1]) / (1 + x));
+  end:
+  a = (y != z2);
+  return a;
+}
+`);
+});
---
+++ typescript-rewrite/samlang-core/src/optimization/index.ts
@@ -0,0 +1,94 @@
+import { MidIRStatement, MidIRCompilationUnit } from '../ast/mir';
+import optimizeIRWithAlgebraicSimplification from './algebraic-optimization';
+import optimizeIRWithCommonSubExpressionElimination from './common-subexpression-elimination-optimization';
+import optimizeIRWithConstantFolding from './constant-folding-optimization';
+import optimizeIRWithConstantPropagation from './constant-propagation-optimization';
+import optimizeIRWithCopyPropagation from './copy-propagation-optimization';
+import optimizeIRWithDeadCodeElimination from './dead-code-elimination-optimization';
+import optimizeMidIRCompilationUnitByInlining from './inline-optimization';
+import optimizeIRWithLocalValueNumbering from './local-value-numbering-optimization';
+import OptimizationResourceAllocator from './optimization-resource-allocator';
+import { optimizeIrWithSimpleOptimization } from './simple-optimizations';
+import optimizeIRWithTailRecursiveCallTransformation from './tail-recursion-optimization';
+
+type OptimizationConfiguration = {
+  doesPerformConstantPropagation?: boolean;
+  doesPerformCopyPropagation?: boolean;
+  doesPerformLocalValueNumbering?: boolean;
+  doesPerformCommonSubExpressionElimination?: boolean;
+  doesPerformDeadCodeElimination?: boolean;
+  doesPerformInlining?: boolean;
+};
+
+const allEnabledOptimizationConfiguration: OptimizationConfiguration = {
+  doesPerformConstantPropagation: true,
+  doesPerformCopyPropagation: true,
+  doesPerformLocalValueNumbering: true,
+  doesPerformCommonSubExpressionElimination: true,
+  doesPerformDeadCodeElimination: true,
+  doesPerformInlining: true,
+};
+
+const optimizeMidIRStatementsForOneRound = (
+  statements: readonly MidIRStatement[],
+  allocator: OptimizationResourceAllocator,
+  {
+    doesPerformConstantPropagation,
+    doesPerformCopyPropagation,
+    doesPerformLocalValueNumbering,
+    doesPerformCommonSubExpressionElimination,
+    doesPerformDeadCodeElimination,
+  }: OptimizationConfiguration
+): readonly MidIRStatement[] => {
+  let optimized = doesPerformConstantPropagation
+    ? optimizeIRWithConstantPropagation(statements)
+    : statements;
+  optimized = optimizeIRWithConstantFolding(optimizeIRWithAlgebraicSimplification(optimized));
+  if (doesPerformCopyPropagation) {
+    optimized = optimizeIRWithCopyPropagation(optimized);
+  }
+  if (doesPerformLocalValueNumbering) {
+    optimized = optimizeIrWithSimpleOptimization(optimized);
+    optimized = optimizeIRWithLocalValueNumbering(optimized);
+  }
+  if (doesPerformCommonSubExpressionElimination) {
+    optimized = optimizeIRWithCommonSubExpressionElimination(optimized, allocator);
+  }
+  if (doesPerformDeadCodeElimination) {
+    optimized = optimizeIRWithDeadCodeElimination(optimized);
+  }
+  return optimizeIrWithSimpleOptimization(optimized);
+};
+
+const optimizeIRCompilationUnit = (
+  source: MidIRCompilationUnit,
+  optimizationConfiguration: OptimizationConfiguration = allEnabledOptimizationConfiguration
+): MidIRCompilationUnit => {
+  const allocator = new OptimizationResourceAllocator();
+
+  let intermediate = source;
+  for (let i = 0; i < 4; i += 1) {
+    const newFunctions = intermediate.functions.map((midIRFunction) => {
+      let statements = midIRFunction.mainBodyStatements;
+      for (let j = 0; j < 5; j += 1) {
+        statements = optimizeMidIRStatementsForOneRound(
+          statements,
+          allocator,
+          optimizationConfiguration
+        );
+      }
+      return optimizeIRWithTailRecursiveCallTransformation({
+        ...midIRFunction,
+        mainBodyStatements: statements,
+      });
+    });
+    intermediate = { ...intermediate, functions: newFunctions };
+    if (optimizationConfiguration.doesPerformInlining) {
+      intermediate = optimizeMidIRCompilationUnitByInlining(intermediate, allocator);
+    }
+  }
+
+  return intermediate;
+};
+
+export default optimizeIRCompilationUnit;
