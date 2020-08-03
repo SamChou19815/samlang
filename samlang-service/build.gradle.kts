970a17e5f26e23204685c6e960b742828ccb0884
Sam <sam@developersam.com>
2020-08-02 21:41:01 -0400

    [service] Convert back into jvm module

--- samlang-service/build.gradle.kts
+++ samlang-service/build.gradle.kts
@@ -1,46 +1,22 @@
 plugins {
-    kotlin(module = "multiplatform")
+    kotlin(module = "jvm")
 }
 
-kotlin {
-    jvm {
-        tasks.named<Test>("jvmTest") {
-            useJUnitPlatform()
-        }
-    }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation(project(":samlang-ast"))
-                implementation(project(":samlang-utils"))
-                implementation(project(":samlang-checker"))
-                implementation(project(":samlang-errors"))
-                implementation(project(":samlang-parser"))
-                implementation(project(":samlang-analysis"))
-                implementation(project(":samlang-optimization"))
-                implementation(project(":samlang-compiler-asm"))
-                implementation(project(":samlang-compiler-ir"))
-                implementation(project(":samlang-printer"))
-            }
-        }
-        val commonTest by getting {
-            dependsOn(commonMain)
-            dependencies {
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-common")
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-annotations-common")
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-junit")
-                implementation(dependencyNotation = "io.kotlintest:kotlintest-runner-junit5:3.4.2")
-            }
-        }
-    }
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(project(":samlang-ast"))
+    implementation(project(":samlang-utils"))
+    implementation(project(":samlang-checker"))
+    implementation(project(":samlang-errors"))
+    implementation(project(":samlang-parser"))
+    implementation(project(":samlang-analysis"))
+    implementation(project(":samlang-optimization"))
+    implementation(project(":samlang-compiler-asm"))
+    implementation(project(":samlang-compiler-ir"))
+    implementation(project(":samlang-printer"))
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-common")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
