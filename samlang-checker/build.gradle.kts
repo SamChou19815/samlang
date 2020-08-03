24a53f6bb9c60ef2eb68e6481e78d0069f66447f
Sam <sam@developersam.com>
2020-08-02 22:11:27 -0400

    [checker] Convert back into jvm modules

--- samlang-checker/build.gradle.kts
+++ samlang-checker/build.gradle.kts
@@ -1,40 +1,16 @@
 plugins {
-    kotlin(module = "multiplatform")
+    kotlin(module = "jvm")
 }
 
-kotlin {
-    jvm {
-        tasks.named<Test>("jvmTest") {
-            useJUnitPlatform()
-        }
-    }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation(dependencyNotation = "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3")
-                implementation(project(":samlang-ast"))
-                implementation(project(":samlang-errors"))
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-common")
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-annotations-common")
-                implementation(project(":samlang-parser"))
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
-        val jvmTest by getting {
-            dependsOn(jvmMain)
-            dependencies {
-                implementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-junit")
-                implementation(dependencyNotation = "io.kotlintest:kotlintest-runner-junit5:3.4.2")
-            }
-        }
-    }
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(dependencyNotation = "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3")
+    implementation(project(":samlang-ast"))
+    implementation(project(":samlang-errors"))
+
+    testImplementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-common")
+    testImplementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-annotations-common")
+    testImplementation(project(":samlang-parser"))
+    testImplementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation(dependencyNotation = "io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
