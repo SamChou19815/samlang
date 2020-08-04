0c19c764c806f885710c848b16a2259b9c0bb337
Sam <sam@developersam.com>
2020-08-03 21:58:02 -0400

    [ts-rewrite][compiler][asm] Setup typedefs and factory methods for tiling results

---
+++ typescript-rewrite/samlang-core/src/compiler/asm/asm-tiling-results.ts
@@ -0,0 +1,84 @@
+import {
+  AssemblyConst,
+  AssemblyRegister,
+  AssemblyMemory,
+  AssemblyConstOrRegister,
+  AssemblyRegisterOrMemory,
+  AssemblyArgument,
+} from '../../ast/asm/asm-arguments';
+import { AssemblyInstruction } from '../../ast/asm/asm-instructions';
+
+export interface AssemblyTilingResult {
+  readonly instructions: readonly AssemblyInstruction[];
+  readonly cost: number;
+}
+
+const estimateCostFromInstructions = (instructions: readonly AssemblyInstruction[]): number => {
+  let cost = 0;
+  instructions.forEach((instruction) => {
+    switch (instruction.__type__) {
+      case 'AssemblyLabel':
+      case 'AssemblyComment':
+      case 'AssemblyNeg':
+        return;
+      case 'AssemblyIMulTwoArgs':
+      case 'AssemblyIMulThreeArgs':
+      case 'AssemblyIDiv':
+        cost += 4;
+        return;
+      case 'AssemblyLoadEffectiveAddress':
+        cost += 2;
+        return;
+      default:
+        cost += 1;
+    }
+  });
+  return cost;
+};
+
+export interface AssemblyArgumentTilingResult<E extends AssemblyArgument = AssemblyArgument>
+  extends AssemblyTilingResult {
+  readonly assemblyArgument: E;
+}
+
+export type AssemblyRegisterOrMemoryTilingResult<
+  E extends AssemblyRegisterOrMemory = AssemblyRegisterOrMemory
+> = AssemblyArgumentTilingResult<E>;
+
+export type AssemblyConstOrRegisterTilingResult<
+  E extends AssemblyConstOrRegister = AssemblyConstOrRegister
+> = AssemblyArgumentTilingResult<E>;
+
+export type AssemblyConstantTilingResult = AssemblyConstOrRegisterTilingResult<AssemblyConst>;
+
+export type AssemblyMemoryTilingResult = AssemblyRegisterOrMemoryTilingResult<AssemblyMemory>;
+
+export interface AssemblyMidIRExpressionTilingResult
+  extends AssemblyRegisterOrMemoryTilingResult<AssemblyRegister>,
+    AssemblyConstOrRegisterTilingResult<AssemblyRegister> {}
+
+export const createAssemblyTilingResult = (
+  instructions: readonly AssemblyInstruction[]
+): AssemblyTilingResult => ({ instructions, cost: estimateCostFromInstructions(instructions) });
+
+export const createAssemblyConstantTilingResult = (
+  constant: AssemblyConst
+): AssemblyConstantTilingResult => ({ instructions: [], cost: 0, assemblyArgument: constant });
+
+export const createAssemblyMemoryTilingResult = (
+  instructions: readonly AssemblyInstruction[],
+  memory: AssemblyMemory
+): AssemblyMemoryTilingResult => ({
+  instructions,
+  cost: estimateCostFromInstructions(instructions),
+  assemblyArgument: memory,
+});
+
+export const createAssemblyMidIRExpressionTilingResult = (
+  instructions: readonly AssemblyInstruction[],
+  register: AssemblyRegister
+): AssemblyMidIRExpressionTilingResult => ({
+  instructions,
+  cost: estimateCostFromInstructions(instructions),
+  assemblyArgument: register,
+});
