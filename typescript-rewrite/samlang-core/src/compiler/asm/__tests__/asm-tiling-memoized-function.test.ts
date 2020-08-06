1c68b2477f6876affc6144b3ee16010696fcab47
Sam <sam@developersam.com>
2020-08-05 21:47:53 -0400

    [ts-rewrite][compiler][asm] Make function memoization work for statement as well

--- typescript-rewrite/samlang-core/src/compiler/asm/__tests__/asm-tiling-memoized-function.test.ts
+++ typescript-rewrite/samlang-core/src/compiler/asm/__tests__/asm-tiling-memoized-function.test.ts
@@ -1,9 +1,12 @@
-import { MIR_CONST } from '../../../ast/mir';
-import getMemoizedAssemblyTilingFunction from '../asm-tiling-memoized-function';
+import { MIR_CONST, MIR_JUMP } from '../../../ast/mir';
+import {
+  getMemoizedAssemblyExpressionTilingFunction,
+  getMemoizedAssemblyStatementTilingFunction,
+} from '../asm-tiling-memoized-function';
 
-it('function returned by getMemoizedAssemblyTilingFunction is correctly memoized.', () => {
+it('function returned by getMemoizedAssemblyExpressionTilingFunction is correctly memoized.', () => {
   let called = 0;
-  const f = getMemoizedAssemblyTilingFunction((expression) => {
+  const f = getMemoizedAssemblyExpressionTilingFunction((expression) => {
     called += 1;
     return expression.__type__.length;
   });
@@ -14,4 +17,10 @@ it('function returned by getMemoizedAssemblyTilingFunction is correctly memoized
   expect(f(MIR_CONST(BigInt(2)))).toBe(23);
   expect(f(MIR_CONST(BigInt(3)))).toBe(23);
   expect(called).toBe(4);
+});
+
+it('getMemoizedAssemblyStatementTilingFunction basic test', () => {
+  expect(getMemoizedAssemblyStatementTilingFunction((s) => s.__type__.length)(MIR_JUMP(''))).toBe(
+    18
+  );
 });
--- typescript-rewrite/samlang-core/src/compiler/asm/asm-tiling-memoized-function.ts
+++ typescript-rewrite/samlang-core/src/compiler/asm/asm-tiling-memoized-function.ts
@@ -1,35 +1,54 @@
-import { MidIRExpression, midIRExpressionToString } from '../../ast/mir';
+import {
+  MidIRStatement,
+  MidIRExpression,
+  midIRStatementToString,
+  midIRExpressionToString,
+} from '../../ast/mir';
 import { Hashable, HashMap, hashMapOf } from '../../util/collections';
 
-class MidIRExpressionWrapper implements Hashable {
-  constructor(public readonly expression: MidIRExpression) {}
+class Wrapper<E> implements Hashable {
+  constructor(public readonly element: E, private readonly toStringFunction: (e: E) => string) {}
 
   uniqueHash(): string {
-    return midIRExpressionToString(this.expression);
+    return this.toStringFunction(this.element);
   }
 }
 
 /** The utility class used to memoize function application. */
-class AssemblyTilingMemoizedFunction<R> {
-  private readonly memoizedIO: HashMap<MidIRExpressionWrapper, R> = hashMapOf();
+class AssemblyTilingMemoizedFunction<T, R> {
+  private readonly memoizedIO: HashMap<Wrapper<T>, R> = hashMapOf();
 
-  constructor(private readonly computeFreshResult: (expression: MidIRExpression) => R) {}
+  constructor(
+    private readonly toStringFunction: (e: T) => string,
+    private readonly computeFreshResult: (e: T) => R
+  ) {}
 
-  invoke = (expression: MidIRExpression): R => {
-    const wrapped = new MidIRExpressionWrapper(expression);
+  invoke = (functionArgument: T): R => {
+    const wrapped = new Wrapper(functionArgument, this.toStringFunction);
     const output = this.memoizedIO.get(wrapped);
     if (output != null) return output;
-    const freshOutput = this.computeFreshResult(expression);
+    const freshOutput = this.computeFreshResult(functionArgument);
     this.memoizedIO.set(wrapped, freshOutput);
     return freshOutput;
   };
 }
 
-const getMemoizedAssemblyTilingFunction = <R>(
-  computeFreshResult: (expression: MidIRExpression) => R
-): ((expression: MidIRExpression) => R) => {
-  const memorizedFunction = new AssemblyTilingMemoizedFunction(computeFreshResult);
+export const getMemoizedAssemblyStatementTilingFunction = <R>(
+  computeFreshResult: (statement: MidIRStatement) => R
+): ((statement: MidIRStatement) => R) => {
+  const memorizedFunction = new AssemblyTilingMemoizedFunction(
+    midIRStatementToString,
+    computeFreshResult
+  );
   return memorizedFunction.invoke;
 };
 
-export default getMemoizedAssemblyTilingFunction;
+export const getMemoizedAssemblyExpressionTilingFunction = <R>(
+  computeFreshResult: (expression: MidIRExpression) => R
+): ((expression: MidIRExpression) => R) => {
+  const memorizedFunction = new AssemblyTilingMemoizedFunction(
+    midIRExpressionToString,
+    computeFreshResult
+  );
+  return memorizedFunction.invoke;
+};
