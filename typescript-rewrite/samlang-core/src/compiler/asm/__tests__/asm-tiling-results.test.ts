ecf80a6d685187dcb08264616fcaf32638f584e4
Sam <sam@developersam.com>
2020-08-05 22:26:18 -0400

    [ts-rewrite][asm][compiler] Unbreak tests

--- typescript-rewrite/samlang-core/src/compiler/asm/__tests__/asm-tiling-results.test.ts
+++ typescript-rewrite/samlang-core/src/compiler/asm/__tests__/asm-tiling-results.test.ts
@@ -9,30 +9,28 @@ import {
   ASM_COMMENT,
 } from '../../../ast/asm/asm-instructions';
 import {
-  createAssemblyTilingResult,
   createAssemblyConstantTilingResult,
   createAssemblyMemoryTilingResult,
   createAssemblyMidIRExpressionTilingResult,
 } from '../asm-tiling-results';
 
 it('tiling results have correct estimated cost', () => {
-  expect(
-    createAssemblyTilingResult([
-      ASM_COMMENT(''),
-      ASM_LABEL(''),
-      ASM_IMUL(RAX, RAX),
-      ASM_IMUL(RAX, RAX, ASM_CONST(1)),
-      ASM_IDIV(RAX),
-      ASM_LEA(RAX, ASM_MEM_CONST(ASM_CONST(1))),
-      ASM_CALL(RAX),
-    ]).cost
-  ).toBe(15);
-
   expect(createAssemblyConstantTilingResult(ASM_CONST(1)).cost).toBe(0);
   expect(
     createAssemblyMemoryTilingResult([ASM_MOVE_REG(RAX, RAX)], ASM_MEM_CONST(ASM_CONST(1))).cost
   ).toBe(1);
   expect(
-    createAssemblyMidIRExpressionTilingResult([ASM_MOVE_REG(RAX, RAX)], ASM_REG('')).cost
-  ).toBe(1);
+    createAssemblyMidIRExpressionTilingResult(
+      [
+        ASM_COMMENT(''),
+        ASM_LABEL(''),
+        ASM_IMUL(RAX, RAX),
+        ASM_IMUL(RAX, RAX, ASM_CONST(1)),
+        ASM_IDIV(RAX),
+        ASM_LEA(RAX, ASM_MEM_CONST(ASM_CONST(1))),
+        ASM_CALL(RAX),
+      ],
+      ASM_REG('')
+    ).cost
+  ).toBe(15);
 });
