77dc42d9aeb85924ffbf7abd4437da90a4399e3e
Sam <sam@developersam.com>
2020-08-06 21:14:39 -0400

    [ts-rewrite][util] Add mod 2 utils to prepare for tiling

--- typescript-rewrite/samlang-core/src/util/__tests/int-util.test.ts
+++ typescript-rewrite/samlang-core/src/util/__tests/int-util.test.ts
@@ -1,4 +1,10 @@
-import { MIN_I32_VALUE, MAX_I32_VALUE, bigIntIsWithin32BitIntegerRange } from '../int-util';
+import {
+  MIN_I32_VALUE,
+  MAX_I32_VALUE,
+  bigIntIsWithin32BitIntegerRange,
+  logTwo,
+  isPowerOfTwo,
+} from '../int-util';
 
 it('bigIntIsWithin32BitIntegerRange test', () => {
   expect(bigIntIsWithin32BitIntegerRange(BigInt(-21474836480))).toBeFalsy();
@@ -6,4 +12,23 @@ it('bigIntIsWithin32BitIntegerRange test', () => {
   expect(bigIntIsWithin32BitIntegerRange(BigInt(0))).toBeTruthy();
   expect(bigIntIsWithin32BitIntegerRange(MAX_I32_VALUE)).toBeTruthy();
   expect(bigIntIsWithin32BitIntegerRange(BigInt(21474836470))).toBeFalsy();
+});
+
+it('logTwo, isPowerOfTwo test', () => {
+  expect(isPowerOfTwo(BigInt(0))).toBeFalsy();
+  expect(isPowerOfTwo(BigInt(1))).toBeTruthy();
+  expect(isPowerOfTwo(BigInt(2))).toBeTruthy();
+  expect(isPowerOfTwo(BigInt(3))).toBeFalsy();
+  expect(isPowerOfTwo(BigInt(4))).toBeTruthy();
+  expect(isPowerOfTwo(BigInt(5))).toBeFalsy();
+  expect(isPowerOfTwo(BigInt(6))).toBeFalsy();
+  expect(isPowerOfTwo(BigInt(7))).toBeFalsy();
+  expect(isPowerOfTwo(BigInt(8))).toBeTruthy();
+
+  expect(logTwo(BigInt(1))).toBe(0);
+  expect(logTwo(BigInt(2))).toBe(1);
+  expect(logTwo(BigInt(4))).toBe(2);
+  expect(logTwo(BigInt(8))).toBe(3);
+  expect(logTwo(BigInt(16))).toBe(4);
+  expect(logTwo(BigInt(65536))).toBe(16);
 });
--- typescript-rewrite/samlang-core/src/util/int-util.ts
+++ typescript-rewrite/samlang-core/src/util/int-util.ts
@@ -1,5 +1,13 @@
 export const MIN_I32_VALUE = BigInt(-2147483648);
 export const MAX_I32_VALUE = BigInt(2147483647);
+export const BIGINT_ONE = BigInt(1);
 
 export const bigIntIsWithin32BitIntegerRange = (value: bigint): boolean =>
   value >= MIN_I32_VALUE && value <= MAX_I32_VALUE;
+
+export const logTwo = (number: bigint): number =>
+  number === BIGINT_ONE ? 0 : 1 + logTwo(number / BigInt(2));
+
+export const isPowerOfTwo = (number: bigint): boolean =>
+  // eslint-disable-next-line no-bitwise
+  number > 0 && (number & (number - BIGINT_ONE)) === BigInt(0);
