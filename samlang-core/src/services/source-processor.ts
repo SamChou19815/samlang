98c18ace5c58f0fcad9e13e12f93b91f9f94375b
Megan Yin <my474@cornell.edu>
2020-08-14 20:51:51 -0400

    highIR toString methods

--- samlang-core/src/services/source-processor.ts
+++ samlang-core/src/services/source-processor.ts
@@ -1,6 +1,18 @@
 import type { AssemblyProgram } from '../ast/asm/asm-program';
 import type ModuleReference from '../ast/common/module-reference';
 import type { Sources } from '../ast/common/structs';
+import {
+  HighIRStatement,
+  HighIRExpression,
+  HighIRIndexAccessExpression,
+  HighIRVariableExpression,
+  HighIRNameExpression,
+  HighIRBinaryExpression,
+  HighIRIfElseStatement,
+  HighIRFunctionCallStatement,
+  HighIRLetDefinitionStatement,
+  HighIRStructInitializationStatement,
+} from '../ast/hir/hir-expressions';
 import { HighIRModule, HighIRFunction } from '../ast/hir/hir-toplevel';
 import type { SamlangModule } from '../ast/lang/samlang-toplevel';
 import { typeCheckSources, GlobalTypingContext } from '../checker';
@@ -53,6 +65,73 @@ export const lowerSourcesToAssemblyPrograms = (
           ] as const
       )
   );
+
+const highIRStatementToString = (highIRStatement: HighIRStatement): string => {
+  switch (highIRStatement.__type__) {
+    case 'HighIRIfElseStatement': {
+      const { booleanExpression, s1, s2 } = highIRStatement as HighIRIfElseStatement;
+      return `if (${highIRExpressionToString(booleanExpression)}) {
+        ${s1.map((s) => highIRStatementToString(s)).join(';\n')}  
+      } else {
+        ${s2.map((s) => highIRStatementToString(s)).join(';\n')}
+      }`;
+    }
+    case 'HighIRFunctionCallStatement': {
+      const {
+        functionArguments,
+        functionExpression,
+        returnCollector,
+      } = highIRStatement as HighIRFunctionCallStatement;
+      return `let ${returnCollector} = ${highIRExpressionToString(
+        functionExpression
+      )}(${functionArguments.map((arg) => highIRExpressionToString(arg)).join(', ')})`;
+    }
+    case 'HighIRLetDefinitionStatement': {
+      const { name, assignedExpression } = highIRStatement as HighIRLetDefinitionStatement;
+      return `let ${name} = ${highIRExpressionToString(assignedExpression)}`;
+    }
+    case 'HighIRReturnStatement':
+      return `return ${
+        highIRStatement.expression && highIRExpressionToString(highIRStatement.expression)
+      };`;
+    case 'HighIRStructInitializationStatement': {
+      const {
+        structVariableName,
+        expressionList,
+      } = highIRStatement as HighIRStructInitializationStatement;
+      return `${structVariableName} = [${expressionList
+        .map((e) => highIRExpressionToString(e))
+        .join(', ')}]`;
+    }
+  }
+};
+
+const highIRFunctionToString = (highIRFunction: HighIRFunction): string =>
+  `const ${highIRFunction.name} = (${highIRFunction.parameters.join(', ')}) => {
+    ${highIRFunction.body.map((statement) => highIRStatementToString(statement)).join(';\n')}
+    ${highIRFunction.hasReturn && 'return;'}
+  }`;
+
+const highIRExpressionToString = (highIRExpression: HighIRExpression): string => {
+  switch (highIRExpression.__type__) {
+    case 'HighIRIntLiteralExpression':
+      return `${highIRExpression.value}`;
+    case 'HighIRStringLiteralExpression':
+      return highIRExpression.value;
+    case 'HighIRIndexAccessExpression': {
+      const { expression, index } = highIRExpression as HighIRIndexAccessExpression;
+      return `${highIRExpressionToString(expression)}[${index}]`;
+    }
+    case 'HighIRVariableExpression':
+      return (highIRExpression as HighIRVariableExpression).name;
+    case 'HighIRNameExpression':
+      return (highIRExpression as HighIRNameExpression).name;
+    case 'HighIRBinaryExpression': {
+      const { e1, e2, operator } = highIRExpression as HighIRBinaryExpression;
+      return `${highIRExpressionToString(e1)} ${operator} ${highIRExpressionToString(e2)}`;
+    }
+  }
+};
 
 export const highIRSourcesToJSString = (sources: Sources<HighIRModule>): Sources<string> => {
   const jsSources: HashMap<ModuleReference, string> = hashMapOf();
