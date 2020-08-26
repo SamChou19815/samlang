0837e767496bcd0592308e93edd69c414d896ce9
Sam <sam@developersam.com>
2020-08-26 00:21:05 -0400

    [core] No emit for core

--- samlang-core/suppress-type-errors.js
+++ samlang-core/suppress-type-errors.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const suppress = (filename) =>
   fs.writeFileSync(filename, `// @ts-nocheck\n${fs.readFileSync(filename).toString()}`);
 
-suppress('src/parser/generated/PLLexer.ts');
-suppress('src/parser/generated/PLParser.ts');
+suppress('parser/generated/PLLexer.ts');
+suppress('parser/generated/PLParser.ts');
