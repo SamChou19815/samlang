c1699a7e77e608143ad38ce2f24e2ee79c09d641
Sam <sam@developersam.com>
2020-09-20 20:04:37 -0400

    [interpreter] Factor interpreter into its own package

--- samlang-core/index.ts
+++ samlang-core/index.ts
@@ -1,5 +1,4 @@
 export { compileSamlangSourcesToHighIRSources } from './compiler';
-export { default as interpretSamlangModule } from './interpreter/source-level-interpreter';
 export { prettyPrintSamlangModule, prettyPrintHighIRModuleAsJS } from './printer';
 export { checkSources, lowerSourcesToAssemblyPrograms } from './services/source-processor';
 export { LanguageServiceState, LanguageServices } from './services/language-service';
