de33561e05efe4bdb315df6c069e0d64f2729ca3
Sam <sam@developersam.com>
2021-01-02 12:01:23 -0500

    [compiler][hir] Setup HIR type lowering

---
+++ samlang-core-compiler/__tests__/hir-type-lowering.test.ts
@@ -0,0 +1,44 @@
+import lowerSamlangType from '../hir-types-lowering';
+
+import {
+  boolType,
+  functionType,
+  identifierType,
+  intType,
+  ModuleReference,
+  stringType,
+  tupleType,
+  unitType,
+} from 'samlang-core-ast/common-nodes';
+import {
+  HIR_INT_TYPE,
+  HIR_VOID_TYPE,
+  HIR_ANY_TYPE,
+  HIR_IDENTIFIER_TYPE,
+  HIR_POINTER_TYPE,
+  HIR_STRUCT_TYPE,
+  HIR_FUNCTION_TYPE,
+} from 'samlang-core-ast/hir-types';
+
+it('lowerSamlangType works', () => {
+  expect(lowerSamlangType(intType, new Set())).toEqual(HIR_INT_TYPE);
+  expect(lowerSamlangType(boolType, new Set())).toEqual(HIR_INT_TYPE);
+  expect(lowerSamlangType(unitType, new Set())).toEqual(HIR_VOID_TYPE);
+  expect(lowerSamlangType(stringType, new Set([]))).toEqual(HIR_POINTER_TYPE(HIR_INT_TYPE));
+
+  expect(lowerSamlangType(identifierType(ModuleReference.ROOT, 'A'), new Set())).toEqual(
+    HIR_IDENTIFIER_TYPE('_A')
+  );
+  expect(lowerSamlangType(identifierType(ModuleReference.ROOT, 'A'), new Set(['A']))).toEqual(
+    HIR_ANY_TYPE
+  );
+
+  expect(lowerSamlangType(tupleType([intType, boolType]), new Set())).toEqual(
+    HIR_POINTER_TYPE(HIR_STRUCT_TYPE([HIR_INT_TYPE, HIR_INT_TYPE]))
+  );
+  expect(lowerSamlangType(functionType([intType, boolType], unitType), new Set())).toEqual(
+    HIR_POINTER_TYPE(HIR_FUNCTION_TYPE([HIR_INT_TYPE, HIR_INT_TYPE], HIR_VOID_TYPE))
+  );
+
+  expect(() => lowerSamlangType({ type: 'UndecidedType', index: 0 }, new Set())).toThrow();
+});
---
+++ samlang-core-compiler/hir-types-lowering.ts
@@ -0,0 +1,46 @@
+import type { Type } from 'samlang-core-ast/common-nodes';
+import {
+  HighIRType,
+  HIR_INT_TYPE,
+  HIR_VOID_TYPE,
+  HIR_ANY_TYPE,
+  HIR_IDENTIFIER_TYPE,
+  HIR_POINTER_TYPE,
+  HIR_STRUCT_TYPE,
+  HIR_FUNCTION_TYPE,
+} from 'samlang-core-ast/hir-types';
+
+const lowerSamlangType = (type: Type, genericTypes: ReadonlySet<string>): HighIRType => {
+  // istanbul ignore next
+  if (type.type === 'UndecidedType') throw new Error('Unreachable!');
+  switch (type.type) {
+    case 'PrimitiveType':
+      switch (type.name) {
+        case 'bool':
+        case 'int':
+          return HIR_INT_TYPE;
+        case 'unit':
+          return HIR_VOID_TYPE;
+        case 'string':
+          return HIR_POINTER_TYPE(HIR_INT_TYPE);
+      }
+    // eslint-disable-next-line no-fallthrough
+    case 'IdentifierType': {
+      if (genericTypes.has(type.identifier)) return HIR_ANY_TYPE;
+      return HIR_IDENTIFIER_TYPE(`${type.moduleReference.parts.join('_')}_${type.identifier}`);
+    }
+    case 'TupleType':
+      return HIR_POINTER_TYPE(
+        HIR_STRUCT_TYPE(type.mappings.map((it) => lowerSamlangType(it, genericTypes)))
+      );
+    case 'FunctionType':
+      return HIR_POINTER_TYPE(
+        HIR_FUNCTION_TYPE(
+          type.argumentTypes.map((it) => lowerSamlangType(it, genericTypes)),
+          lowerSamlangType(type.returnType, genericTypes)
+        )
+      );
+  }
+};
+
+export default lowerSamlangType;
