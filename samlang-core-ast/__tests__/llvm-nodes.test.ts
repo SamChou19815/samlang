482184b05da4a1036dae667bc9f39b474e0dac11
Sam <sam@developersam.com>
2021-01-05 23:13:35 -0500

    [ast][llvm] Setup LLVM instructions constructors and pretty printers

--- samlang-core-ast/__tests__/llvm-nodes.test.ts
+++ samlang-core-ast/__tests__/llvm-nodes.test.ts
@@ -1,6 +1,7 @@
 import {
   prettyPrintLLVMType,
   prettyPrintLLVMValue,
+  prettyPrintLLVMInstruction,
   LLVM_BOOL_TYPE,
   LLVM_INT_TYPE,
   LLVM_VOID_TYPE,
@@ -11,6 +12,15 @@ import {
   LLVM_INT,
   LLVM_VARIABLE,
   LLVM_NAME,
+  LLVM_GET_ELEMENT_PTR,
+  LLVM_BINARY,
+  LLVM_LOAD,
+  LLVM_STORE,
+  LLVM_PHI,
+  LLVM_CALL,
+  LLVM_CJUMP,
+  LLVM_RETURN,
+  LLVM_RETURN_VOID,
 } from '../llvm-nodes';
 
 import { Long } from 'samlang-core-utils';
@@ -34,4 +44,240 @@ it('prettyPrintLLVMValue works.', () => {
   expect(prettyPrintLLVMValue(LLVM_INT(Long.fromInt(3)))).toBe('3');
   expect(prettyPrintLLVMValue(LLVM_VARIABLE('foo'))).toBe('%foo');
   expect(prettyPrintLLVMValue(LLVM_NAME('foo'))).toBe('@foo');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_GET_ELEMENT_PTR.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_GET_ELEMENT_PTR({
+        resultVariable: 'foo',
+        pointerType: LLVM_IDENTIFIER_TYPE('Foo'),
+        sourceVariable: 'bar',
+        offset: 3,
+      })
+    )
+  ).toBe('%foo = getelementptr %Foo, %Foo %bar, i64 3');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_BINARY.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '+',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(3),
+      })
+    )
+  ).toBe('%foo = add i64 %bar, 3');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '-',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(3),
+      })
+    )
+  ).toBe('%foo = sub i64 %bar, 3');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '*',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(3),
+      })
+    )
+  ).toBe('%foo = mul i64 %bar, 3');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '/',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(3),
+      })
+    )
+  ).toBe('%foo = sdiv i64 %bar, 3');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '%',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(3),
+      })
+    )
+  ).toBe('%foo = srem i64 %bar, 3');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '^',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = xor i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '<',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp slt i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '<=',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp sle i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '>',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp sgt i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '>=',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp sge i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '==',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp eq i1 %bar, 1');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_BINARY({
+        resultVariable: 'foo',
+        operator: '!=',
+        v1: LLVM_VARIABLE('bar'),
+        v2: LLVM_INT(1),
+      })
+    )
+  ).toBe('%foo = icmp ne i1 %bar, 1');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_LOAD.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_LOAD({
+        resultVariable: 'foo',
+        resultType: LLVM_IDENTIFIER_TYPE('Foo'),
+        sourceVariable: 'bar',
+        sourceType: LLVM_IDENTIFIER_TYPE('Foo'),
+      })
+    )
+  ).toBe('%foo = load %Foo, %Foo %bar');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_STORE.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_STORE({
+        targetVariable: 'foo',
+        targetType: LLVM_IDENTIFIER_TYPE('Foo'),
+        sourceValue: LLVM_NAME('bar'),
+        sourceType: LLVM_IDENTIFIER_TYPE('Foo'),
+      })
+    )
+  ).toBe('store %Foo %@bar, %Foo %foo');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_PHI.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_PHI({
+        variableType: LLVM_INT_TYPE,
+        v1: LLVM_VARIABLE('bar'),
+        b1: 'b1',
+        v2: LLVM_INT(1),
+        b2: 'b2',
+      })
+    )
+  ).toBe('phi i64 [ %%bar, %b1 ], [ %1, %b2 ]');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_CALL.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_CALL({
+        resultType: LLVM_INT_TYPE,
+        resultVariable: 'c',
+        functionName: LLVM_NAME('plusPlus'),
+        functionArguments: [
+          { value: LLVM_VARIABLE('bar'), type: LLVM_INT_TYPE },
+          { value: LLVM_INT(1), type: LLVM_INT_TYPE },
+        ],
+      })
+    )
+  ).toBe('%c = call i64 @plusPlus(i64 %bar, i64 1) nounwind');
+
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_CALL({
+        resultType: LLVM_INT_TYPE,
+        functionName: LLVM_NAME('plusPlus'),
+        functionArguments: [
+          { value: LLVM_VARIABLE('bar'), type: LLVM_INT_TYPE },
+          { value: LLVM_INT(1), type: LLVM_INT_TYPE },
+        ],
+      })
+    )
+  ).toBe('call i64 @plusPlus(i64 %bar, i64 1) nounwind');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_PHI.', () => {
+  expect(
+    prettyPrintLLVMInstruction(
+      LLVM_CJUMP({
+        condition: LLVM_VARIABLE('c'),
+        b1: 'b1',
+        b2: 'b2',
+      })
+    )
+  ).toBe('br i1 %c, label %b1, label %b2');
+});
+
+it('prettyPrintLLVMInstruction works for LLVM_RETURN.', () => {
+  expect(prettyPrintLLVMInstruction(LLVM_RETURN_VOID)).toBe('ret void');
+
+  expect(prettyPrintLLVMInstruction(LLVM_RETURN(LLVM_VARIABLE('bar'), LLVM_BOOL_TYPE))).toBe(
+    'ret i1 %bar'
+  );
 });
--- samlang-core-ast/llvm-nodes.ts
+++ samlang-core-ast/llvm-nodes.ts
@@ -99,10 +99,11 @@ export const prettyPrintLLVMValue = (value: LLVMValue): string => {
 
 export type LLVMAnnotatedValue = { readonly value: LLVMValue; readonly type: LLVMType };
 
-export type LLVMGetElementAddressInstruction = {
-  readonly __type__: 'LLVMGetElementAddressInstruction';
+export type LLVMGetElementPointerInstruction = {
+  readonly __type__: 'LLVMGetElementPointerInstruction';
+  readonly resultVariable: string;
   readonly pointerType: LLVMType;
-  readonly value: LLVMValue;
+  readonly sourceVariable: string;
   readonly offset: number;
 };
 
@@ -110,12 +111,12 @@ export type LLVMBinaryInstruction = {
   readonly __type__: 'LLVMBinaryInstruction';
   readonly resultVariable: string;
   readonly operator: IROperator;
-  readonly e1: LLVMValue;
-  readonly e2: LLVMValue;
+  readonly v1: LLVMValue;
+  readonly v2: LLVMValue;
 };
 
 export type LLVMLoadInstruction = {
-  readonly __type__: 'LLVMBinaryInstruction';
+  readonly __type__: 'LLVMLoadInstruction';
   readonly resultVariable: string;
   readonly resultType: LLVMType;
   readonly sourceVariable: string;
@@ -126,16 +127,16 @@ export type LLVMStoreInstruction = {
   readonly __type__: 'LLVMStoreInstruction';
   readonly targetVariable: string;
   readonly targetType: LLVMType;
-  readonly sourceVariable: string;
+  readonly sourceValue: LLVMValue;
   readonly sourceType: LLVMType;
 };
 
 export type LLVMPhiInstruction = {
   readonly __type__: 'LLVMPhiInstruction';
   readonly variableType: LLVMType;
-  readonly v1: string;
+  readonly v1: LLVMValue;
   readonly b1: string;
-  readonly v2: string;
+  readonly v2: LLVMValue;
   readonly b2: string;
 };
 
@@ -160,7 +161,7 @@ export type LLVMReturnInstruction = {
 };
 
 export type LLVMInstruction =
-  | LLVMGetElementAddressInstruction
+  | LLVMGetElementPointerInstruction
   | LLVMBinaryInstruction
   | LLVMLoadInstruction
   | LLVMStoreInstruction
@@ -168,3 +169,195 @@ export type LLVMInstruction =
   | LLVMFunctionCallInstruction
   | LLVMConditionalJumpInstruction
   | LLVMReturnInstruction;
+
+type ConstructorArgumentObject<E extends LLVMInstruction> = Omit<E, '__type__'>;
+
+export const LLVM_GET_ELEMENT_PTR = ({
+  resultVariable,
+  pointerType,
+  sourceVariable,
+  offset,
+}: ConstructorArgumentObject<LLVMGetElementPointerInstruction>): LLVMGetElementPointerInstruction => ({
+  __type__: 'LLVMGetElementPointerInstruction',
+  resultVariable,
+  pointerType,
+  sourceVariable,
+  offset,
+});
+
+export const LLVM_BINARY = ({
+  resultVariable,
+  operator,
+  v1,
+  v2,
+}: ConstructorArgumentObject<LLVMBinaryInstruction>): LLVMBinaryInstruction => ({
+  __type__: 'LLVMBinaryInstruction',
+  resultVariable,
+  operator,
+  v1,
+  v2,
+});
+
+export const LLVM_LOAD = ({
+  resultVariable,
+  resultType,
+  sourceVariable,
+  sourceType,
+}: ConstructorArgumentObject<LLVMLoadInstruction>): LLVMLoadInstruction => ({
+  __type__: 'LLVMLoadInstruction',
+  resultVariable,
+  resultType,
+  sourceVariable,
+  sourceType,
+});
+
+export const LLVM_STORE = ({
+  targetVariable,
+  targetType,
+  sourceValue,
+  sourceType,
+}: ConstructorArgumentObject<LLVMStoreInstruction>): LLVMStoreInstruction => ({
+  __type__: 'LLVMStoreInstruction',
+  targetVariable,
+  targetType,
+  sourceValue,
+  sourceType,
+});
+
+export const LLVM_PHI = ({
+  variableType,
+  v1,
+  b1,
+  v2,
+  b2,
+}: ConstructorArgumentObject<LLVMPhiInstruction>): LLVMPhiInstruction => ({
+  __type__: 'LLVMPhiInstruction',
+  variableType,
+  v1,
+  b1,
+  v2,
+  b2,
+});
+
+export const LLVM_CALL = ({
+  resultType,
+  resultVariable,
+  functionName,
+  functionArguments,
+}: ConstructorArgumentObject<LLVMFunctionCallInstruction>): LLVMFunctionCallInstruction => ({
+  __type__: 'LLVMFunctionCallInstruction',
+  resultType,
+  resultVariable,
+  functionName,
+  functionArguments,
+});
+
+export const LLVM_CJUMP = ({
+  condition,
+  b1,
+  b2,
+}: ConstructorArgumentObject<LLVMConditionalJumpInstruction>): LLVMConditionalJumpInstruction => ({
+  __type__: 'LLVMBranchInstruction',
+  condition,
+  b1,
+  b2,
+});
+
+export const LLVM_RETURN_VOID: LLVMReturnInstruction = { __type__: 'LLVMReturnInstruction' };
+
+export const LLVM_RETURN = (value: LLVMValue, type: LLVMType): LLVMReturnInstruction => ({
+  __type__: 'LLVMReturnInstruction',
+  value: { value, type },
+});
+
+export const prettyPrintLLVMInstruction = (instruction: LLVMInstruction): string => {
+  switch (instruction.__type__) {
+    case 'LLVMGetElementPointerInstruction': {
+      const { resultVariable, pointerType, sourceVariable, offset } = instruction;
+      const type = prettyPrintLLVMType(pointerType);
+      return `%${resultVariable} = getelementptr ${type}, ${type} %${sourceVariable}, i64 ${offset}`;
+    }
+    case 'LLVMBinaryInstruction': {
+      const result = `%${instruction.resultVariable}`;
+      const v1 = prettyPrintLLVMValue(instruction.v1);
+      const v2 = prettyPrintLLVMValue(instruction.v2);
+      let stringOperatorAndType: string;
+      switch (instruction.operator) {
+        case '+':
+          stringOperatorAndType = 'add i64';
+          break;
+        case '-':
+          stringOperatorAndType = 'sub i64';
+          break;
+        case '*':
+          stringOperatorAndType = 'mul i64';
+          break;
+        case '/':
+          stringOperatorAndType = 'sdiv i64';
+          break;
+        case '%':
+          stringOperatorAndType = 'srem i64';
+          break;
+        case '^':
+          stringOperatorAndType = 'xor i1';
+          break;
+        case '<':
+          stringOperatorAndType = 'icmp slt i1';
+          break;
+        case '<=':
+          stringOperatorAndType = 'icmp sle i1';
+          break;
+        case '>':
+          stringOperatorAndType = 'icmp sgt i1';
+          break;
+        case '>=':
+          stringOperatorAndType = 'icmp sge i1';
+          break;
+        case '==':
+          stringOperatorAndType = 'icmp eq i1';
+          break;
+        case '!=':
+          stringOperatorAndType = 'icmp ne i1';
+          break;
+      }
+      return `${result} = ${stringOperatorAndType} ${v1}, ${v2}`;
+    }
+    case 'LLVMLoadInstruction': {
+      const { resultVariable, sourceVariable } = instruction;
+      const resultType = prettyPrintLLVMType(instruction.resultType);
+      const sourceType = prettyPrintLLVMType(instruction.sourceType);
+      return `%${resultVariable} = load ${resultType}, ${sourceType} %${sourceVariable}`;
+    }
+    case 'LLVMStoreInstruction': {
+      const sourceValue = prettyPrintLLVMValue(instruction.sourceValue);
+      const targetType = prettyPrintLLVMType(instruction.targetType);
+      const sourceType = prettyPrintLLVMType(instruction.sourceType);
+      return `store ${sourceType} %${sourceValue}, ${targetType} %${instruction.targetVariable}`;
+    }
+    case 'LLVMPhiInstruction': {
+      const v1 = prettyPrintLLVMValue(instruction.v1);
+      const v2 = prettyPrintLLVMValue(instruction.v2);
+      const type = prettyPrintLLVMType(instruction.variableType);
+      return `phi ${type} [ %${v1}, %${instruction.b1} ], [ %${v2}, %${instruction.b2} ]`;
+    }
+    case 'LLVMFunctionCallInstruction': {
+      const assignedTo =
+        instruction.resultVariable != null ? `%${instruction.resultVariable} = ` : '';
+      const resultType = prettyPrintLLVMType(instruction.resultType);
+      const functionName = prettyPrintLLVMValue(instruction.functionName);
+      const functionArguments = instruction.functionArguments
+        .map(({ value, type }) => `${prettyPrintLLVMType(type)} ${prettyPrintLLVMValue(value)}`)
+        .join(', ');
+      return `${assignedTo}call ${resultType} ${functionName}(${functionArguments}) nounwind`;
+    }
+    case 'LLVMBranchInstruction': {
+      const { condition, b1, b2 } = instruction;
+      return `br i1 ${prettyPrintLLVMValue(condition)}, label %${b1}, label %${b2}`;
+    }
+    case 'LLVMReturnInstruction': {
+      if (instruction.value == null) return 'ret void';
+      const { value, type } = instruction.value;
+      return `ret ${prettyPrintLLVMType(type)} ${prettyPrintLLVMValue(value)}`;
+    }
+  }
+};
