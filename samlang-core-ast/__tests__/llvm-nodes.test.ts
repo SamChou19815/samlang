7b686f28918a463fec8cb419342644eb003e5a23
Sam Zhou <sam@developersam.com>
2021-01-05 22:20:00 -0500

    [ast][llvm] Setup LLVM value constructors and pretty printers (#215)

--- samlang-core-ast/__tests__/llvm-nodes.test.ts
+++ samlang-core-ast/__tests__/llvm-nodes.test.ts
@@ -1,5 +1,6 @@
 import {
   prettyPrintLLVMType,
+  prettyPrintLLVMValue,
   LLVM_BOOL_TYPE,
   LLVM_INT_TYPE,
   LLVM_VOID_TYPE,
@@ -7,7 +8,12 @@ import {
   LLVM_POINTER_TYPE,
   LLVM_STRUCT_TYPE,
   LLVM_FUNCTION_TYPE,
+  LLVM_INT,
+  LLVM_VARIABLE,
+  LLVM_NAME,
 } from '../llvm-nodes';
+
+import { Long } from 'samlang-core-utils';
 
 it('prettyPrintLLVMType works.', () => {
   expect(prettyPrintLLVMType(LLVM_BOOL_TYPE)).toBe('i1');
@@ -21,4 +27,11 @@ it('prettyPrintLLVMType works.', () => {
   expect(
     prettyPrintLLVMType(LLVM_FUNCTION_TYPE([LLVM_INT_TYPE, LLVM_BOOL_TYPE], LLVM_VOID_TYPE))
   ).toBe('void (i64, i1)');
+});
+
+it('prettyPrintLLVMValue works.', () => {
+  expect(prettyPrintLLVMValue(LLVM_INT(3))).toBe('3');
+  expect(prettyPrintLLVMValue(LLVM_INT(Long.fromInt(3)))).toBe('3');
+  expect(prettyPrintLLVMValue(LLVM_VARIABLE('foo'))).toBe('%foo');
+  expect(prettyPrintLLVMValue(LLVM_NAME('foo'))).toBe('@foo');
 });
