c1a10aae572cac24b368c53e604caaa507a64fa7
Sam Zhou <sam@developersam.com>
2021-01-07 15:44:15 -0500

    [compiler] Support allocating temporaries for resource allocator (#233)
    Prepare for LLVM

--- samlang-core-compiler/mir-resource-allocator.ts
+++ samlang-core-compiler/mir-resource-allocator.ts
@@ -1,5 +1,12 @@
 export default class MidIRResourceAllocator {
+  private nextTempId = 0;
   private nextLabelId = 0;
+
+  allocateTemp(purpose: string): string {
+    const tempID = this.nextTempId;
+    this.nextTempId += 1;
+    return `_temp_${tempID}_${purpose}`;
+  }
 
   allocateLabel(functionName: string): string {
     const temp = this.nextLabelId;
