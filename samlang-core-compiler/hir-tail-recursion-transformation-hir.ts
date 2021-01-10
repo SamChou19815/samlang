ddf14885efa69f997b4442fd262e380d2d7eb184
Sam <sam@developersam.com>
2021-01-10 10:32:39 -0500

    [compiler][hir] Condense recursivelyPerformTailRecursiveCallTransformationOnStatements

--- samlang-core-compiler/hir-tail-recursion-transformation-hir.ts
+++ samlang-core-compiler/hir-tail-recursion-transformation-hir.ts
@@ -9,14 +9,36 @@ import { checkNotNull } from 'samlang-core-utils';
 
 const getFunctionParameterCollector = (name: string): string => `_param_${name}_temp_collector`;
 
-const performTailRecursiveCallTransformationOnLinearStatements = (
+const recursivelyPerformTailRecursiveCallTransformationOnStatements = (
   highIRFunction: HighIRFunction,
   statements: readonly HighIRStatement[]
 ): readonly HighIRStatement[] | null => {
-  const returnStatement = statements[statements.length - 1];
+  const lastStatement = statements[statements.length - 1];
+  if (lastStatement == null) return null;
+  if (lastStatement.__type__ === 'HighIRIfElseStatement') {
+    const s1 = recursivelyPerformTailRecursiveCallTransformationOnStatements(
+      highIRFunction,
+      lastStatement.s1
+    );
+    const s2 = recursivelyPerformTailRecursiveCallTransformationOnStatements(
+      highIRFunction,
+      lastStatement.s2
+    );
+    if (s1 == null && s2 == null) return null;
+    return [
+      ...statements.slice(0, statements.length - 1),
+      HIR_IF_ELSE({
+        multiAssignedVariable: lastStatement.multiAssignedVariable,
+        booleanExpression: lastStatement.booleanExpression,
+        s1: s1 ?? lastStatement.s1,
+        s2: s2 ?? lastStatement.s2,
+      }),
+    ];
+  }
+
+  const returnStatement = lastStatement;
   const functionCallStatement = statements[statements.length - 2];
   if (
-    returnStatement == null ||
     functionCallStatement == null ||
     returnStatement.__type__ !== 'HighIRReturnStatement' ||
     functionCallStatement.__type__ !== 'HighIRFunctionCallStatement'
@@ -59,39 +81,5 @@ const performTailRecursiveCallTransformationOnLinearStatements = (
   }
   return null;
 };
-
-const performTailRecursiveCallTransformationOnIfElseEndedStatements = (
-  highIRFunction: HighIRFunction,
-  statements: readonly HighIRStatement[]
-): readonly HighIRStatement[] | null => {
-  const lastStatement = statements[statements.length - 1];
-  if (lastStatement == null || lastStatement.__type__ !== 'HighIRIfElseStatement') return null;
-
-  const s1 = recursivelyPerformTailRecursiveCallTransformationOnStatements(
-    highIRFunction,
-    lastStatement.s1
-  );
-  const s2 = recursivelyPerformTailRecursiveCallTransformationOnStatements(
-    highIRFunction,
-    lastStatement.s2
-  );
-  if (s1 == null && s2 == null) return null;
-  return [
-    ...statements.slice(0, statements.length - 1),
-    HIR_IF_ELSE({
-      multiAssignedVariable: lastStatement.multiAssignedVariable,
-      booleanExpression: lastStatement.booleanExpression,
-      s1: s1 ?? lastStatement.s1,
-      s2: s2 ?? lastStatement.s2,
-    }),
-  ];
-};
-
-const recursivelyPerformTailRecursiveCallTransformationOnStatements = (
-  highIRFunction: HighIRFunction,
-  statements: readonly HighIRStatement[]
-): readonly HighIRStatement[] | null =>
-  performTailRecursiveCallTransformationOnLinearStatements(highIRFunction, statements) ??
-  performTailRecursiveCallTransformationOnIfElseEndedStatements(highIRFunction, statements);
 
 export default recursivelyPerformTailRecursiveCallTransformationOnStatements;
