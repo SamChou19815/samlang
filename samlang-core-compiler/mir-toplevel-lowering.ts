35b5cf1599195d473f610ebf3b69c824c324a004
Sam <sam@developersam.com>
2021-01-10 20:51:27 -0500

    [compiler][hir] Purge tail recursion optimization
    I plan to reimplement it once LLVM code is stablized. Right now the code here just increases migration burden.

--- samlang-core-compiler/mir-toplevel-lowering.ts
+++ samlang-core-compiler/mir-toplevel-lowering.ts
@@ -1,5 +1,3 @@
-import coalesceMoveAndReturnForHighIRStatements from './hir-move-return-coalescing';
-import recursivelyPerformTailRecursiveCallTransformationOnStatements from './hir-tail-recursion-transformation-hir';
 import createMidIRBasicBlocks from './mir-basic-block';
 import emitCanonicalMidIRStatementsFromReorderedBasicBlocks from './mir-basic-block-optimized-emitter';
 import reorderMidIRBasicBlocksToMaximizeLongestNoJumpPath from './mir-basic-block-reorder';
@@ -23,39 +21,21 @@ const compileHighIrModuleToMidIRCompilationUnit = (
   const allocator = new MidIRResourceAllocator();
   const functions: MidIRFunction[] = [];
   highIRModule.functions.forEach((highIRFunction) => {
-    const slightlyOptimizedBody =
-      coalesceMoveAndReturnForHighIRStatements(highIRFunction.body) ?? highIRFunction.body;
-    const tailRecursionRewrittenStatements = recursivelyPerformTailRecursiveCallTransformationOnStatements(
-      highIRFunction,
-      slightlyOptimizedBody
-    );
     const loweredStatements = midIRTranslateStatementsAndCollectGlobalStrings(
       allocator,
       highIRFunction.name,
-      tailRecursionRewrittenStatements ?? slightlyOptimizedBody
+      highIRFunction.body
     );
-    let finalStatements: typeof loweredStatements;
-    if (tailRecursionRewrittenStatements == null) {
-      finalStatements = [...loweredStatements, MIR_RETURN(MIR_ZERO)];
-    } else {
-      const whileTrueStartLabel = allocator.allocateLabelWithAnnotation(
-        highIRFunction.name,
-        'WHILE_TRUE_START'
-      );
-      finalStatements = [
-        MIR_LABEL(whileTrueStartLabel),
-        ...loweredStatements,
-        MIR_JUMP(whileTrueStartLabel),
-        MIR_RETURN(MIR_ZERO),
-      ];
-    }
     functions.push({
       functionName: highIRFunction.name,
       argumentNames: highIRFunction.parameters.map((it) => `_${it}`),
       mainBodyStatements: optimizeIrWithSimpleOptimization(
         emitCanonicalMidIRStatementsFromReorderedBasicBlocks(
           reorderMidIRBasicBlocksToMaximizeLongestNoJumpPath(
-            createMidIRBasicBlocks(allocator, highIRFunction.name, finalStatements)
+            createMidIRBasicBlocks(allocator, highIRFunction.name, [
+              ...loweredStatements,
+              MIR_RETURN(MIR_ZERO),
+            ])
           )
         )
       ),
