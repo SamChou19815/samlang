a232ab3df799494758cdba842762aed1ac0c395a
Sam Zhou <sam@developersam.com>
2021-01-12 14:24:47 -0500

    [analysis] Construct CFG for LLVM instructions (#296)

--- samlang-core-analysis/__tests__/control-flow-graph.test.ts
+++ samlang-core-analysis/__tests__/control-flow-graph.test.ts
@@ -1,6 +1,19 @@
 import ControlFlowGraph from '../control-flow-graph';
 
 import {
+  LLVMInstruction,
+  LLVM_INT,
+  LLVM_INT_TYPE,
+  LLVM_VARIABLE,
+  LLVM_CAST,
+  LLVM_LABEL,
+  LLVM_JUMP,
+  LLVM_CJUMP,
+  LLVM_SWITCH,
+  LLVM_RETURN,
+} from 'samlang-core-ast/llvm-nodes';
+import {
+  MidIRStatement,
   MIR_ONE,
   MIR_TEMP,
   MIR_MOVE_TEMP,
@@ -10,7 +23,7 @@ import {
   MIR_RETURN,
 } from 'samlang-core-ast/mir-nodes';
 
-const statements = [
+const midIRStatements: readonly MidIRStatement[] = [
   MIR_LABEL('foo'),
   MIR_LABEL('bar'),
   MIR_MOVE_TEMP('a', MIR_TEMP('b')),
@@ -20,31 +33,64 @@ const statements = [
   MIR_RETURN(MIR_ONE),
   MIR_RETURN(MIR_ONE),
 ];
-const graph = ControlFlowGraph.fromMidIRStatements(statements);
+const midIRGraph = ControlFlowGraph.fromMidIRStatements(midIRStatements);
+
+const llvmStatements: readonly LLVMInstruction[] = [
+  LLVM_LABEL('foo'),
+  LLVM_LABEL('bar'),
+  LLVM_CAST({
+    resultVariable: 'a',
+    resultType: LLVM_INT_TYPE,
+    sourceValue: LLVM_VARIABLE('b'),
+    sourceType: LLVM_INT_TYPE,
+  }),
+  LLVM_CJUMP(LLVM_VARIABLE(''), 'baz', 'fall'),
+  LLVM_LABEL('fall'),
+  LLVM_JUMP('baz'),
+  LLVM_LABEL('baz'),
+  LLVM_RETURN(LLVM_INT(0), LLVM_INT_TYPE),
+  LLVM_SWITCH(LLVM_VARIABLE(''), 'baz', [{ value: 0, branch: 'baz' }]),
+  LLVM_RETURN(LLVM_INT(0), LLVM_INT_TYPE),
+];
+const llvmGraph = ControlFlowGraph.fromLLVMInstructions(llvmStatements);
 
 it('Control flow graph should preserve order.', () => {
-  expect(graph.nodes.map((it) => it.instruction)).toEqual(statements);
+  expect(midIRGraph.nodes.map((it) => it.instruction)).toEqual(midIRStatements);
+  expect(llvmGraph.nodes.map((it) => it.instruction)).toEqual(llvmStatements);
 });
 
-it('DFS should hit all the reachable statements.', () => {
+it('DFS should hit all the reachable statements for midIRGraph.', () => {
   const visited = new Set<number>();
-  graph.dfs((node) => {
+  midIRGraph.dfs((node) => {
     visited.add(node.id);
   });
   expect(Array.from(visited.values()).sort((a, b) => a - b)).toEqual([0, 1, 2, 3, 4, 5, 6]);
 });
 
+it('DFS should hit all the reachable statements for llvmGraph.', () => {
+  const visited = new Set<number>();
+  llvmGraph.dfs((node) => {
+    visited.add(node.id);
+  });
+  expect(Array.from(visited.values()).sort((a, b) => a - b)).toEqual([0, 1, 2, 3, 4, 5, 6, 7]);
+});
+
 it('DFS should not be able to get to unreachable statements.', () => {
-  graph.dfs((node) => {
+  midIRGraph.dfs((node) => {
     if (node.id === 7) {
+      fail('We should not hit the final unreachable return.');
+    }
+  });
+  llvmGraph.dfs((node) => {
+    if (node.id > 7) {
       fail('We should not hit the final unreachable return.');
     }
   });
 });
 
-it('getParentIds is correct.', () => {
+it('getParentIds is correct for midIRGraph.', () => {
   const parentOf = (id: number) =>
-    Array.from(graph.getParentIds(id).values()).sort((a, b) => a - b);
+    Array.from(midIRGraph.getParentIds(id).values()).sort((a, b) => a - b);
   expect(parentOf(0)).toEqual([]);
   expect(parentOf(1)).toEqual([0]);
   expect(parentOf(2)).toEqual([1]);
@@ -52,4 +98,16 @@ it('getParentIds is correct.', () => {
   expect(parentOf(4)).toEqual([3]);
   expect(parentOf(5)).toEqual([3, 4]);
   expect(parentOf(6)).toEqual([5]);
+});
+
+it('getParentIds is correct for llvmGraph.', () => {
+  const parentOf = (id: number) =>
+    Array.from(llvmGraph.getParentIds(id).values()).sort((a, b) => a - b);
+  expect(parentOf(0)).toEqual([]);
+  expect(parentOf(1)).toEqual([0]);
+  expect(parentOf(2)).toEqual([1]);
+  expect(parentOf(3)).toEqual([2]);
+  expect(parentOf(4)).toEqual([3]);
+  expect(parentOf(5)).toEqual([4]);
+  expect(parentOf(6)).toEqual([3, 5, 8]);
 });
