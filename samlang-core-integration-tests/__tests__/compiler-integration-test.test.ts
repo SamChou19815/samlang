9fce959940ea325560fa9cfa3b8cb111a753b605
Sam <sam@developersam.com>
2021-01-22 15:34:15 -0500

    [optimization] Ensure all optimizations are tested in isolation

--- samlang-core-integration-tests/__tests__/compiler-integration-test.test.ts
+++ samlang-core-integration-tests/__tests__/compiler-integration-test.test.ts
@@ -9,6 +9,10 @@ import {
 } from 'samlang-core-compiler';
 import interpretLLVMModule from 'samlang-core-interpreter/llvm-ir-interpreter';
 import interpretSamlangModule from 'samlang-core-interpreter/source-level-interpreter';
+import {
+  OptimizationConfiguration,
+  optimizeHighIRModuleAccordingToConfiguration,
+} from 'samlang-core-optimization';
 // eslint-disable-next-line import/no-internal-modules
 import { createPrettierDocumentFromHighIRModule } from 'samlang-core-printer/printer-js';
 // eslint-disable-next-line import/no-internal-modules
@@ -33,7 +37,7 @@ if (process.env.CI) {
   });
 }
 
-const hirSources = compileSamlangSourcesToHighIRSources(checkedSources);
+const hirSources = compileSamlangSourcesToHighIRSources(checkedSources, {});
 
 const highIRModuleToJSCode = (highIRModule: HighIRModule): string =>
   prettyPrintAccordingToPrettierAlgorithm(
@@ -41,29 +45,56 @@ const highIRModuleToJSCode = (highIRModule: HighIRModule): string =>
     createPrettierDocumentFromHighIRModule(highIRModule, true)
   );
 
+const testHIR = (
+  program: HighIRModule,
+  expectedStandardOut: string,
+  optimizationConfig?: OptimizationConfiguration
+): void => {
+  const jsCode = highIRModuleToJSCode(
+    optimizeHighIRModuleAccordingToConfiguration(program, optimizationConfig)
+  );
+  let interpretationResult: string;
+  try {
+    // eslint-disable-next-line no-eval
+    interpretationResult = eval(jsCode);
+  } catch (e) {
+    fail(`${e.message}\n\n${jsCode}`);
+    return;
+  }
+  if (interpretationResult !== expectedStandardOut) {
+    const unoptimizedJSCode = highIRModuleToJSCode(program);
+    fail(
+      `Expected:\n${expectedStandardOut}\nActual:\n${interpretationResult}\nCode:\n${jsCode}\n\nUnoptimized Code:\n${unoptimizedJSCode}`
+    );
+  }
+};
+
 runnableSamlangProgramTestCases.forEach(({ testCaseName, expectedStandardOut }) => {
   const program = hirSources.forceGet(new ModuleReference([testCaseName]));
 
-  it(`HIR: ${testCaseName}`, () => {
-    const jsCode = highIRModuleToJSCode(program);
-    let interpretationResult: string;
-    try {
-      // eslint-disable-next-line no-eval
-      interpretationResult = eval(jsCode);
-    } catch (e) {
-      fail(`${e.message}\n\n${jsCode}`);
-      return;
-    }
-    if (interpretationResult !== expectedStandardOut) {
-      fail(`Expected:\n${expectedStandardOut}\nActual:\n${interpretationResult}\nCode: ${jsCode}`);
-    }
+  it(`HIR[no-opt]: ${testCaseName}`, () => {
+    testHIR(program, expectedStandardOut, {});
+  });
+  it(`HIR[lvn]: ${testCaseName}`, () => {
+    testHIR(program, expectedStandardOut, { doesPerformLocalValueNumbering: true });
+  });
+  it(`HIR[cse]: ${testCaseName}`, () => {
+    testHIR(program, expectedStandardOut, { doesPerformCommonSubExpressionElimination: true });
+  });
+  it(`HIR[inl]: ${testCaseName}`, () => {
+    testHIR(program, expectedStandardOut, { doesPerformInlining: true });
+  });
+  it(`HIR[all]: ${testCaseName}`, () => {
+    testHIR(program, expectedStandardOut);
   });
 });
 
 runnableSamlangProgramTestCases.forEach(({ testCaseName, expectedStandardOut }) => {
   it(`LLVM: ${testCaseName}`, () => {
     const compilationUnit = lowerHighIRModuleToLLVMModule(
-      hirSources.forceGet(new ModuleReference([testCaseName]))
+      optimizeHighIRModuleAccordingToConfiguration(
+        hirSources.forceGet(new ModuleReference([testCaseName]))
+      )
     );
 
     let result: string;
