9cb63e33815ec1131e2fbad2aa2de672da7dd231
Sam Zhou <sam@developersam.com>
2021-01-24 12:08:48 -0500

    [optimization] Perform more DCE on loops (#378)

--- samlang-core-optimization/hir-dead-code-elimination-optimization.ts
+++ samlang-core-optimization/hir-dead-code-elimination-optimization.ts
@@ -3,6 +3,63 @@ import { ifElseOrNull } from './hir-optimization-common';
 import type { HighIRExpression, HighIRStatement } from 'samlang-core-ast/hir-expressions';
 import { isNotNull } from 'samlang-core-utils';
 
+const collectUseFromHighIRStatement = (statement: HighIRStatement, set: Set<string>): void => {
+  const collectUseFromExpression = (expression: HighIRExpression): void => {
+    if (expression.__type__ === 'HighIRVariableExpression') set.add(expression.name);
+  };
+
+  switch (statement.__type__) {
+    case 'HighIRIndexAccessStatement':
+      collectUseFromExpression(statement.pointerExpression);
+      return;
+    case 'HighIRBinaryStatement':
+      collectUseFromExpression(statement.e1);
+      collectUseFromExpression(statement.e2);
+      return;
+    case 'HighIRFunctionCallStatement':
+      collectUseFromExpression(statement.functionExpression);
+      statement.functionArguments.forEach(collectUseFromExpression);
+      return;
+    case 'HighIRIfElseStatement':
+      statement.finalAssignments.forEach((finalAssignment) => {
+        collectUseFromExpression(finalAssignment.branch1Value);
+        collectUseFromExpression(finalAssignment.branch2Value);
+      });
+      statement.s1.forEach((it) => collectUseFromHighIRStatement(it, set));
+      statement.s2.forEach((it) => collectUseFromHighIRStatement(it, set));
+      collectUseFromExpression(statement.booleanExpression);
+      return;
+    case 'HighIRSingleIfStatement':
+      statement.statements.forEach((it) => collectUseFromHighIRStatement(it, set));
+      collectUseFromExpression(statement.booleanExpression);
+      return;
+    case 'HighIRBreakStatement':
+      collectUseFromExpression(statement.breakValue);
+      return;
+    case 'HighIRWhileStatement': {
+      statement.loopVariables.forEach((it) => {
+        collectUseFromExpression(it.initialValue);
+        collectUseFromExpression(it.loopValue);
+      });
+      statement.statements.forEach((it) => collectUseFromHighIRStatement(it, set));
+      statement.loopVariables.forEach((variable) => {
+        collectUseFromExpression(variable.initialValue);
+      });
+      return;
+    }
+    case 'HighIRCastStatement':
+      collectUseFromExpression(statement.assignedExpression);
+      return;
+    case 'HighIRStructInitializationStatement':
+      statement.expressionList.forEach(collectUseFromExpression);
+      return;
+    // istanbul ignore next
+    case 'HighIRReturnStatement':
+      // istanbul ignore next
+      collectUseFromExpression(statement.expression);
+  }
+};
+
 const optimizeHighIRStatement = (
   statement: HighIRStatement,
   set: Set<string>
@@ -66,15 +123,19 @@ const optimizeHighIRStatement = (
           breakCollector = undefined;
         }
       }
-      statement.loopVariables.forEach((it) => collectUseFromExpression(it.loopValue));
+      const usedSetInsideLoop = new Set<string>();
+      collectUseFromHighIRStatement(statement, usedSetInsideLoop);
+      const usedLoopVariablesInsideLoop = statement.loopVariables.filter((it) =>
+        usedSetInsideLoop.has(it.name)
+      );
+      usedLoopVariablesInsideLoop.forEach((it) => collectUseFromExpression(it.loopValue));
       const statements = internalOptimizeHighIRStatementsByDCE(statement.statements, set);
-      const loopVariables = statement.loopVariables
+      const loopVariables = usedLoopVariablesInsideLoop
         .map((variable) => {
-          if (set.has(variable.name)) {
-            collectUseFromExpression(variable.initialValue);
-            return variable;
-          }
-          return null;
+          // istanbul ignore next
+          if (!set.has(variable.name)) return null;
+          collectUseFromExpression(variable.initialValue);
+          return variable;
         })
         .filter(isNotNull);
       return [{ ...statement, loopVariables, statements, breakCollector }];
