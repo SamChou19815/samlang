aecbe81439a8a9c5669051ae35cfd4b379a999fc
Sam <sam@developersam.com>
2021-05-11 12:45:40 -0400

    [test] Add a benchmark test

--- scripts/compile-repository.js
+++ scripts/compile-repository.js
@@ -9,12 +9,15 @@ const path = require('path');
 const read = (/** @type {string} */ filename) => fs.readFileSync(filename).toString();
 
 const runWithErrorCheck = (/** @type {string} */ command, /** @type {string[]} */ args = []) => {
+  const startTime = new Date().getTime();
   const result = spawnSync(command, args, {
     shell: true,
     stdio: ['pipe', 'pipe', 'inherit'],
   });
   if (result.status !== 0) throw new Error(`Command \`${command}\` failed with ${result.status}.`);
-  return result.stdout.toString();
+  const resultString = result.stdout.toString();
+  const time = new Date().getTime() - startTime;
+  return { resultString, time };
 };
 
 const basePath = './out';
@@ -47,16 +50,29 @@ const getJSPrograms = () => {
   return programs;
 };
 
-const interpretPrograms = (/** @type {string[]} */ programs) =>
-  programs.map((program) => `#${program}\n${runWithErrorCheck(program)}`).join('\n');
-
-const interpretJSPrograms = (/** @type {string[]} */ programs) =>
-  programs
-    .map(
-      (program) =>
-        `#${program.substring(0, program.length - 3)}\n${runWithErrorCheck('node', [program])}`
-    )
+const interpretPrograms = (/** @type {string[]} */ programs) => {
+  let totalTime = 0;
+  const result = programs
+    .map((program) => {
+      const { resultString, time } = runWithErrorCheck(program);
+      totalTime += time;
+      return `#${program}\n${resultString}`;
+    })
     .join('\n');
+  return { result, totalTime };
+};
+
+const interpretJSPrograms = (/** @type {string[]} */ programs) => {
+  let totalTime = 0;
+  const result = programs
+    .map((program) => {
+      const { resultString, time } = runWithErrorCheck('node', [program]);
+      totalTime += time;
+      return `#${program.substring(0, program.length - 3)}\n${resultString}`;
+    })
+    .join('\n');
+  return { result, totalTime };
+};
 
 const compare = (/** @type {string} */ expected, /** @type {string} */ actual) => {
   if (expected === actual) {
@@ -74,16 +90,22 @@ console.error('Compiling...');
 runWithErrorCheck('./samlang-cli/bin/index.js', ['compile']);
 console.error('Compiled!');
 console.error('Checking generated JS code...');
-if (!compare(read('./scripts/snapshot.txt'), interpretJSPrograms(getJSPrograms()))) {
+const { result: jsProgramResult, totalTime: jsTotalTime } = interpretJSPrograms(getJSPrograms());
+if (!compare(read('./scripts/snapshot.txt'), jsProgramResult)) {
   process.exit(1);
 }
 console.error('Generated JS code is good.');
+console.error(`Generated JS code takes ${jsTotalTime}ms to run.`);
 if (spawnSync('llc', ['--help'], { shell: true, stdio: 'pipe' }).status === 0) {
+  const { result: nativeProgramResult, totalTime: nativeTotalTime } = interpretPrograms(
+    getX86Programs()
+  );
   console.error('Checking generated machine code...');
-  if (!compare(read('./scripts/snapshot.txt'), interpretPrograms(getX86Programs()))) {
+  if (!compare(read('./scripts/snapshot.txt'), nativeProgramResult)) {
     process.exit(1);
   }
   console.error('Generated machine code is good.');
+  console.error(`Generated machine code takes ${nativeTotalTime}ms to run.`);
 } else {
   console.log('No LLVM toolchain installation. Skipping LLVM IR verification.');
 }
