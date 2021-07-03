7375b0de50192d99e4c962a6901f4528f0efccd3
Sam Zhou <sam@developersam.com>
2021-07-03 16:17:23 -0700

    [repo] Adopt yarn v3 infra (#575)

--- .yarn/plugins/@yarnpkg/plugin-monorail.cjs
+++ .yarn/plugins/@yarnpkg/plugin-monorail.cjs
@@ -1,8 +1,69 @@
 /* eslint-disable */
+//prettier-ignore
 module.exports = {
 name: "@yarnpkg/plugin-monorail",
 factory: function (require) {
-var plugin;plugin=(()=>{"use strict";var n={711:(n,e,r)=>{r.r(e),r.d(e,{default:()=>k});const o=require("fs"),t=require("clipanion"),a=require("path"),i='\n    runs-on: ubuntu-latest\n    steps:\n      - uses: actions/checkout@v2\n        with:\n          fetch-depth: "2"\n      - uses: actions/setup-node@v2\n      - uses: actions/cache@v2\n        with:\n          path: ".yarn/cache\\n.pnp.js"\n          key: "yarn-berry-${{ hashFiles(\'**/yarn.lock\') }}"\n          restore-keys: "yarn-berry-"\n      - name: Yarn Install\n        run: yarn install --immutable',s=n=>[[".github/workflows/generated-general.yml",`# @generated\n\nname: General\non:\n  push:\n    branches:\n      - main\n  pull_request:\n\njobs:\n  lint:${i}\n      - name: Format Check\n        run: yarn format:check\n      - name: Lint\n        run: yarn lint\n  build:${i}\n      - name: Compile\n        run: yarn c\n  validate:${i}\n      - name: Check changed\n        run: if [[ \`git status --porcelain\` ]]; then exit 1; fi\n  test:${i}\n      - name: Test\n        run: yarn test\n`],...n.topologicallyOrdered.filter(e=>((n,e,r)=>{var t,i;const s=n.information[e];if(null==s)throw new Error;return null!=(null===(i=null===(t=JSON.parse((0,o.readFileSync)((0,a.join)(s.workspaceLocation,"package.json")).toString()))||void 0===t?void 0:t.scripts)||void 0===i?void 0:i[r])})(n,e,"deploy")).map(e=>{var r,o;return[`.github/workflows/generated-${"cd-"+e}.yml`,`# @generated\n\nname: CD ${e}\non:\n  push:\n    paths:${(null!==(o=null===(r=n.information[e])||void 0===r?void 0:r.dependencyChain)&&void 0!==o?o:[]).map(e=>{var r;return`\n      - '${null===(r=n.information[e])||void 0===r?void 0:r.workspaceLocation}/**'`}).join("")}\n      - 'configuration/**'\n      - '.github/workflows/generated-*-${e}.yml'\n    branches:\n      - main\n\njobs:\n  deploy:${i}\n      - name: Build\n        run: yarn workspace ${e} build\n      - name: Install Firebase\n        run: sudo npm install -g firebase-tools\n      - name: Deploy\n        run: FIREBASE_TOKEN=\${{ secrets.FIREBASE_TOKEN }} yarn workspace ${e} deploy\n`]})],l=(0,a.join)(".github","workflows"),c=async n=>{(0,o.existsSync)(l)&&(0,o.readdirSync)(l).forEach(n=>{n.startsWith("generated-")&&(0,o.unlinkSync)((0,a.join)(l,n))}),(0,o.mkdirSync)(l,{recursive:!0}),s(n).forEach(([n,e])=>{(0,o.writeFileSync)(n,e)})};var d=r(129);const u=n=>e=>process.stderr.isTTY?`[${n}m${e}[0m`:e,p=u(31),m=u(32),y=u(33),h=u(34),f=u(35),w=(u(36),(n,e)=>{var r,o;const t=r=>{var o,t;return(0,a.dirname)(r)!==(0,a.join)(null!==(t=null===(o=n.information[e])||void 0===o?void 0:o.workspaceLocation)&&void 0!==t?t:".","bin")};return(null!==(o=null===(r=n.information[e])||void 0===r?void 0:r.dependencyChain)&&void 0!==o?o:[]).some(e=>{var r,o;return(n=>{const e=(e,r)=>{const o=(0,d.spawnSync)("git",["diff",e,...r?[r]:[],"--name-only","--",n]).stdout.toString().trim();return""===o?[]:o.split("\n")};return process.env.CI?e("HEAD^","HEAD"):e("origin/main")})(null!==(o=null===(r=n.information[e])||void 0===r?void 0:r.workspaceLocation)&&void 0!==o?o:".").some(t)})}),v=async()=>{var n;const e=(n=>n.topologicallyOrdered.map(e=>[e,w(n,e)]).filter(([,n])=>n).map(([n])=>n))((n="workspaces.json",JSON.parse((0,o.readFileSync)(n).toString())));e.forEach(n=>{console.error(h(`[i] \`${n}\` needs to be recompiled.`))});const r=(()=>{let n=0;const e=(new Date).getTime(),r=["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"];return setInterval(()=>{const o=(((new Date).getTime()-e)/1e3).toFixed(1),t=r[n%10];process.stderr.write(y(`[?] Compiling (${o}s) ${t}\r`)),n+=1},process.stderr.isTTY?50:1e3)})(),t=Promise.all(e.map(n=>{const e=(0,d.spawn)("yarn",["workspace",n,"compile"],{shell:!0,stdio:["ignore","pipe","ignore"]});let r="";return e.stdout.on("data",n=>{r+=n.toString()}),new Promise(o=>{e.on("exit",e=>o([n,0===e,r]))})})),a=await t;clearInterval(r);const i=a.map(n=>n[2]).join(""),s=a.filter(n=>!n[1]).map(n=>n[0]);return 0===s.length?(console.error(m("[✓] All workspaces have been successfully compiled!")),!0):(console.error(f("[!] Compilation finished with some errors.")),console.error(i.trim()),s.forEach(n=>{console.error(p(`[x] \`${n}\` failed to exit with 0.`))}),!1)};class g extends t.Command{async execute(){return await v()?0:1}}g.addPath("c");const k={hooks:{afterAllInstalled:()=>{const n=r(904).Z;(0,o.writeFileSync)("workspaces.json",JSON.stringify(n,void 0,2)+"\n"),c(n)}},commands:[g]}},904:(n,e,r)=>{r.d(e,{Z:()=>i});var o=r(129);const t=(()=>{const n=new Map,e=`[${(0,o.spawnSync)("yarn",["workspaces","list","-v","--json"]).stdout.toString().trim().split("\n").join(",")}]`,r=JSON.parse(e),t={};return r.forEach(({name:n,location:e})=>{null!=n&&(t[e]=n)}),r.forEach(({name:e,location:r,workspaceDependencies:o})=>{null!=e&&n.set(e,{workspaceLocation:r,dependencies:o.map(n=>{const e=t[n];if(null==e)throw new Error(`Bad dependency of ${e}: ${n}`);return e})})}),n})(),a=n=>{const e=[],r=[],o=new Set,a=new Set,i=s=>{var l;if(a.has(s)){if(!o.has(s))return;r.push(s);const n=r.indexOf(s),e=r.slice(n,r.length).join(" -> ");throw new Error("Cyclic dependency detected: "+e)}const c=null===(l=t.get(s))||void 0===l?void 0:l.dependencies;if(null==c)throw new Error(`Workspace ${n} is not found!`);a.add(s),r.push(s),o.add(s),c.forEach(i),o.delete(s),r.pop(),e.push(s)};return i(n),e},i={__type__:"@generated",information:Object.fromEntries(Array.from(t.entries()).map(([n,{dependencies:e,...r}])=>[n,{...r,dependencyChain:a(n)}]).sort(([n],[e])=>n.localeCompare(e))),topologicallyOrdered:(()=>{const n=[],e=new Set;return Array.from(t.keys()).forEach(r=>{a(r).forEach(r=>{e.has(r)||(n.push(r),e.add(r))})}),n})()}},129:n=>{n.exports=require("child_process")}},e={};function r(o){if(e[o])return e[o].exports;var t=e[o]={exports:{}};return n[o](t,t.exports,r),t.exports}return r.n=n=>{var e=n&&n.__esModule?()=>n.default:()=>n;return r.d(e,{a:e}),e},r.d=(n,e)=>{for(var o in e)r.o(e,o)&&!r.o(n,o)&&Object.defineProperty(n,o,{enumerable:!0,get:e[o]})},r.o=(n,e)=>Object.prototype.hasOwnProperty.call(n,e),r.r=n=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},r(711)})();
+var plugin=(()=>{var U=Object.create,f=Object.defineProperty,K=Object.defineProperties,V=Object.getOwnPropertyDescriptor,z=Object.getOwnPropertyDescriptors,Q=Object.getOwnPropertyNames,h=Object.getOwnPropertySymbols,X=Object.getPrototypeOf,C=Object.prototype.hasOwnProperty,F=Object.prototype.propertyIsEnumerable;var W=(n,e,r)=>e in n?f(n,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):n[e]=r,T=(n,e)=>{for(var r in e||(e={}))C.call(e,r)&&W(n,r,e[r]);if(h)for(var r of h(e))F.call(e,r)&&W(n,r,e[r]);return n},Y=(n,e)=>K(n,z(e)),Z=n=>f(n,"__esModule",{value:!0});var m=n=>{if(typeof require!="undefined")return require(n);throw new Error('Dynamic require of "'+n+'" is not supported')};var x=(n,e)=>{var r={};for(var o in n)C.call(n,o)&&e.indexOf(o)<0&&(r[o]=n[o]);if(n!=null&&h)for(var o of h(n))e.indexOf(o)<0&&F.call(n,o)&&(r[o]=n[o]);return r},nn=(n,e)=>()=>(n&&(e=n(n=0)),e);var A=(n,e)=>{for(var r in e)f(n,r,{get:e[r],enumerable:!0})},en=(n,e,r)=>{if(e&&typeof e=="object"||typeof e=="function")for(let o of Q(e))!C.call(n,o)&&o!=="default"&&f(n,o,{get:()=>e[o],enumerable:!(r=V(e,o))||r.enumerable});return n},u=n=>en(Z(f(n!=null?U(X(n)):{},"default",n&&n.__esModule&&"default"in n?{get:()=>n.default,enumerable:!0}:{value:n,enumerable:!0})),n);var B={};A(B,{default:()=>gn});var R,b,J,un,gn,G=nn(()=>{R=u(m("child_process")),b=(()=>{let n=new Map,o=`[${(0,R.spawnSync)("yarn",["workspaces","list","-v","--json"]).stdout.toString().trim().split(`
+`).join(",")}]`,t=JSON.parse(o),a={};return t.forEach(({name:s,location:i})=>{s!=null&&(a[i]=s)}),t.forEach(({name:s,location:i,workspaceDependencies:p})=>{s!=null&&n.set(s,{workspaceLocation:i,dependencies:p.map(d=>{let c=a[d];if(c==null)throw new Error(`Bad dependency of ${c}: ${d}`);return c})})}),n})(),J=n=>{let e=[],r=[],o=new Set,t=new Set,a=s=>{var p;if(t.has(s)){if(!o.has(s))return;r.push(s);let d=r.indexOf(s),c=r.slice(d,r.length).join(" -> ");throw new Error(`Cyclic dependency detected: ${c}`)}let i=(p=b.get(s))==null?void 0:p.dependencies;if(i==null)throw new Error(`Workspace ${n} is not found!`);t.add(s),r.push(s),o.add(s),i.forEach(a),o.delete(s),r.pop(),e.push(s)};return a(n),e},un={__type__:"@generated",information:Object.fromEntries(Array.from(b.entries()).map(o=>{var[n,t]=o,a=t,{dependencies:e}=a,r=x(a,["dependencies"]);return[n,Y(T({},r),{dependencyChain:J(n)})]}).sort(([n],[e])=>n.localeCompare(e))),topologicallyOrdered:(()=>{let n=[],e=new Set;return Array.from(b.keys()).forEach(r=>{J(r).forEach(t=>{e.has(t)||(n.push(t),e.add(t))})}),n})()},gn=un});var hn={};A(hn,{default:()=>yn});var H=u(m("fs")),q=u(m("clipanion"));var l=u(m("fs")),S=u(m("path")),y=`
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+        with:
+          fetch-depth: "2"
+      - uses: actions/setup-node@v2
+      - uses: actions/cache@v2
+        with:
+          path: ".yarn/cache\\n.pnp.js"
+          key: "yarn-berry-\${{ hashFiles('**/yarn.lock') }}"
+          restore-keys: "yarn-berry-"
+      - name: Yarn Install
+        run: yarn install --immutable`,rn=(n,e,r)=>{var t,a;let o=n.information[e];if(o==null)throw new Error;return((a=(t=JSON.parse((0,l.readFileSync)((0,S.join)(o.workspaceLocation,"package.json")).toString()))==null?void 0:t.scripts)==null?void 0:a[r])!=null},on=n=>[[".github/workflows/generated-general.yml",`# @generated
+
+name: General
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+
+jobs:
+  lint:${y}
+      - name: Format Check
+        run: yarn format:check
+      - name: Lint
+        run: yarn lint
+  build:${y}
+      - name: Compile
+        run: yarn c
+  validate:${y}
+      - name: Check changed
+        run: if [[ \`git status --porcelain\` ]]; then exit 1; fi
+  test:${y}
+      - name: Test
+        run: yarn test
+`],...n.topologicallyOrdered.filter(e=>rn(n,e,"deploy")).map(e=>{var o,t;return[`.github/workflows/generated-${`cd-${e}`}.yml`,`# @generated
+
+name: CD ${e}
+on:
+  push:
+    paths:${((t=(o=n.information[e])==null?void 0:o.dependencyChain)!=null?t:[]).map(a=>{var s;return`
+      - '${(s=n.information[a])==null?void 0:s.workspaceLocation}/**'`}).join("")}
+      - 'configuration/**'
+      - '.github/workflows/generated-*-${e}.yml'
+    branches:
+      - main
+env:
+  NETLIFY_AUTH_TOKEN: \${{ secrets.NETLIFY_AUTH_TOKEN }}
+
+jobs:
+  deploy:${y}
+      - name: Build
+        run: yarn workspace ${e} build
+      - name: Deploy
+        run: yarn workspace ${e} deploy
+`]})],k=(0,S.join)(".github","workflows"),tn=async n=>{(0,l.existsSync)(k)&&(0,l.readdirSync)(k).forEach(e=>{e.startsWith("generated-")&&(0,l.unlinkSync)((0,S.join)(k,e))}),(0,l.mkdirSync)(k,{recursive:!0}),on(n).forEach(([e,r])=>{(0,l.writeFileSync)(e,r)})},L=tn;var w=u(m("child_process")),D=u(m("fs")),E=u(m("path"));var g=n=>process.stderr.isTTY?e=>`[${n}m${e}[0m`:e=>e,I=g(31),v=g(32),O=g(33),N=g(34),_=g(35),wn=g(36);var sn=["\u280B","\u2819","\u2839","\u2838","\u283C","\u2834","\u2826","\u2827","\u2807","\u280F"],an=n=>{let e=0,r=new Date().getTime();return setInterval(()=>{let o=`${((new Date().getTime()-r)/1e3).toFixed(1)}s`,t=n(o),a=sn[e%10];process.stderr.write(O(`${t} ${a}\r`)),e+=1},process.stderr.isTTY?40:1e3)},j=an;var cn=n=>{let e=(r,o)=>{let t=(0,w.spawnSync)("git",["diff",r,...o?[o]:[],"--name-only","--",n]).stdout.toString().trim();return t===""?[]:t.split(`
+`)};return process.env.CI?e("HEAD^","HEAD"):e("origin/main")},ln=n=>JSON.parse((0,D.readFileSync)(n).toString()),pn=(n,e)=>{var t,a;let r=s=>{var i,p;return(0,E.dirname)(s)!==(0,E.join)((p=(i=n.information[e])==null?void 0:i.workspaceLocation)!=null?p:".","bin")};return((a=(t=n.information[e])==null?void 0:t.dependencyChain)!=null?a:[]).some(s=>{var d,c;let i=(c=(d=n.information[s])==null?void 0:d.workspaceLocation)!=null?c:".";return cn(i).some(r)})},dn=n=>n.topologicallyOrdered.map(e=>{let r=pn(n,e);return[e,r]}).filter(([,e])=>e).map(([e])=>e),mn=async()=>{let n=ln("workspaces.json"),e=dn(n);e.forEach(i=>{console.error(N(`[i] \`${i}\` needs to be recompiled.`))});let r=j(i=>`[?] Compiling (${i})`),t=await Promise.all(e.map(i=>{let p=(0,w.spawn)("yarn",["workspace",i,"compile"],{shell:!0,stdio:["ignore","pipe","ignore"]}),d="";return p.stdout.on("data",c=>{d+=c.toString()}),new Promise(c=>{p.on("exit",M=>c([i,M===0,d]))})}));clearInterval(r);let a=t.map(i=>i[2]).join(""),s=t.filter(i=>!i[1]).map(i=>i[0]);return s.length===0?(console.error(v("[\u2713] All workspaces have been successfully compiled!")),!0):(console.error(_("[!] Compilation finished with some errors.")),console.error(a.trim()),s.forEach(i=>{console.error(I(`[x] \`${i}\` failed to exit with 0.`))}),!1)},P=mn;var $=class extends q.Command{async execute(){return await P()?0:1}};$.paths=[["c"]];var fn={hooks:{afterAllInstalled:()=>{let n=(G(),B).default;(0,H.writeFileSync)("workspaces.json",`${JSON.stringify(n,void 0,2)}
+`),L(n)}},commands:[$]},yn=fn;return hn;})();
 return plugin;
 }
 };
