c2229548b8b965f29a88289a782720d42ad0dc9e
Sam Zhou <sam@developersam.com>
2021-10-23 15:21:28 -0400

    [ast][wasm] Instruction constructors (#726)

--- samlang-core/ast/wasm-nodes.ts
+++ samlang-core/ast/wasm-nodes.ts
@@ -40,7 +40,7 @@ export interface WebAssemblyFunctionIndirectCallInstruction extends WebAssemblyB
 }
 
 export interface WebAssemblyIfElseInstruction extends WebAssemblyBaseInstruction {
-  readonly __type__: 'WebAssemblyFunctionIndirectCallInstruction';
+  readonly __type__: 'WebAssemblyIfElseInstruction';
   readonly s1: readonly WebAssemblyInstruction[];
   readonly s2: readonly WebAssemblyInstruction[];
 }
@@ -68,3 +68,66 @@ export type WebAssemblyInstruction =
   | WebAssemblyIfElseInstruction
   | WebAssemblyUnconditionalJumpInstruction
   | WebAssemblyLoopInstruction;
+
+export const WasmConst = (value: number): WebAssemblyConstInstruction => ({
+  __type__: 'WebAssemblyConstInstruction',
+  value,
+});
+
+export const WasmDrop: WebAssemblyDropInstruction = {
+  __type__: 'WebAssemblyDropInstruction',
+};
+
+export const WasmBinary = (operator: IROperator): WebAssemblyBinaryInstruction => ({
+  __type__: 'WebAssemblyBinaryInstruction',
+  operator,
+});
+
+export const WasmLoad = (index: number): WebAssemblyLoadInstruction => ({
+  __type__: 'WebAssemblyLoadInstruction',
+  index,
+});
+
+export const WasmStore = (index: number): WebAssemblyStoreInstruction => ({
+  __type__: 'WebAssemblyStoreInstruction',
+  index,
+});
+
+export const WasmDirectCall = (functionName: string): WebAssemblyFunctionDirectCallInstruction => ({
+  __type__: 'WebAssemblyFunctionDirectCallInstruction',
+  functionName,
+});
+
+export const WasmIndirectCall = (
+  functionNameIndex: number,
+  functionTypeString: string
+): WebAssemblyFunctionIndirectCallInstruction => ({
+  __type__: 'WebAssemblyFunctionIndirectCallInstruction',
+  functionNameIndex,
+  functionTypeString,
+});
+
+export const WasmIfElse = (
+  s1: readonly WebAssemblyInstruction[],
+  s2: readonly WebAssemblyInstruction[]
+): WebAssemblyIfElseInstruction => ({
+  __type__: 'WebAssemblyIfElseInstruction',
+  s1,
+  s2,
+});
+
+export const WasmJump = (label: string): WebAssemblyUnconditionalJumpInstruction => ({
+  __type__: 'WebAssemblyUnconditionalJumpInstruction',
+  label,
+});
+
+export const WasmLoop = ({
+  continueLabel,
+  exitLabel,
+  instructions,
+}: Omit<WebAssemblyLoopInstruction, '__type__'>): WebAssemblyLoopInstruction => ({
+  __type__: 'WebAssemblyLoopInstruction',
+  continueLabel,
+  exitLabel,
+  instructions,
+});
