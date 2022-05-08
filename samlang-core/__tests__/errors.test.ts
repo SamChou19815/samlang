77d5dd28850e558d8d8ed3d094f310e649789442
Sam Zhou <sam@developersam.com>
2022-05-08 11:26:52 -0700

    [errors] Refactor error reporting and add `MissingDefinitionsError`

--- samlang-core/__tests__/errors.test.ts
+++ samlang-core/__tests__/errors.test.ts
@@ -1,75 +1,51 @@
 import { DummySourceReason, Location } from '../ast/common-nodes';
 import { SourceBoolType, SourceIntType } from '../ast/samlang-nodes';
-import {
-  ArityMismatchError,
-  CollisionError,
-  CompileTimeError,
-  IllegalOtherClassMatch,
-  IllegalThisError,
-  InsufficientTypeInferenceContextError,
-  NonExhausiveMatchError,
-  SyntaxError,
-  UnexpectedTypeError,
-  UnexpectedTypeKindError,
-  UnresolvedNameError,
-  UnsupportedClassTypeDefinitionError,
-} from '../errors';
+import { createGlobalErrorCollector } from '../errors';
 
-const testCases: readonly (readonly [CompileTimeError, string])[] = [
-  [new SyntaxError(Location.DUMMY, 'bad code'), '__DUMMY__.sam:0:0-0:0: [SyntaxError]: bad code'],
-  [
-    new UnexpectedTypeError(
+describe('samlang-core/errors', () => {
+  it('error toString() test', () => {
+    const collector = createGlobalErrorCollector();
+
+    const reporter = collector.getErrorReporter();
+    reporter.reportSyntaxError(Location.DUMMY, 'bad code');
+    reporter.reportUnexpectedTypeError(
       Location.DUMMY,
       SourceIntType(DummySourceReason),
       SourceBoolType(DummySourceReason)
-    ),
-    '__DUMMY__.sam:0:0-0:0: [UnexpectedType]: Expected: `int`, actual: `bool`.',
-  ],
-  [
-    new UnresolvedNameError(Location.DUMMY, 'unresolvedName'),
-    '__DUMMY__.sam:0:0-0:0: [UnresolvedName]: Name `unresolvedName` is not resolved.',
-  ],
-  [
-    new UnsupportedClassTypeDefinitionError(Location.DUMMY, 'object'),
-    "__DUMMY__.sam:0:0-0:0: [UnsupportedClassTypeDefinition]: Expect the current class to have `object` type definition, but it doesn't.",
-  ],
-  [
-    new UnexpectedTypeKindError(Location.DUMMY, 'array', 'object'),
-    '__DUMMY__.sam:0:0-0:0: [UnexpectedTypeKind]: Expected kind: `array`, actual: `object`.',
-  ],
-  [
-    new UnexpectedTypeKindError(Location.DUMMY, 'array', SourceIntType(DummySourceReason)),
-    '__DUMMY__.sam:0:0-0:0: [UnexpectedTypeKind]: Expected kind: `array`, actual: `int`.',
-  ],
-  [
-    new ArityMismatchError(Location.DUMMY, 'pair', 1, 2),
-    '__DUMMY__.sam:0:0-0:0: [ArityMismatchError]: Incorrect pair size. Expected: 1, actual: 2.',
-  ],
-  [
-    new InsufficientTypeInferenceContextError(Location.DUMMY),
-    '__DUMMY__.sam:0:0-0:0: [InsufficientTypeInferenceContext]: There is not enough context information to decide the type of this expression.',
-  ],
-  [
-    new CollisionError(Location.DUMMY, 'a'),
-    '__DUMMY__.sam:0:0-0:0: [Collision]: Name `a` collides with a previously defined name.',
-  ],
-  [
-    new IllegalOtherClassMatch(Location.DUMMY),
-    "__DUMMY__.sam:0:0-0:0: [IllegalOtherClassMatch]: It is illegal to match on a value of other class's type.",
-  ],
-  [
-    new IllegalThisError(Location.DUMMY),
-    '__DUMMY__.sam:0:0-0:0: [IllegalThis]: Keyword `this` cannot be used in this context.',
-  ],
-  [
-    new NonExhausiveMatchError(Location.DUMMY, ['A', 'B']),
-    '__DUMMY__.sam:0:0-0:0: [NonExhausiveMatch]: The following tags are not considered in the match: [A, B].',
-  ],
-];
+    );
+    reporter.reportUnresolvedNameError(Location.DUMMY, 'global');
+    reporter.reportUnsupportedClassTypeDefinitionError(Location.DUMMY, 'object');
+    reporter.reportUnexpectedTypeKindError(Location.DUMMY, 'array', 'object');
+    reporter.reportUnexpectedTypeKindError(
+      Location.DUMMY,
+      'array',
+      SourceIntType(DummySourceReason)
+    );
+    reporter.reportArityMismatchError(Location.DUMMY, 'pair', 1, 2);
+    reporter.reportInsufficientTypeInferenceContextError(Location.DUMMY);
+    reporter.reportCollisionError(Location.DUMMY, 'a');
+    reporter.reportIllegalOtherClassMatch(Location.DUMMY);
+    reporter.reportIllegalThisError(Location.DUMMY);
+    reporter.reportNonExhausiveMatchError(Location.DUMMY, ['A', 'B']);
+    reporter.reportMissingDefinitionsError(Location.DUMMY, ['foo', 'bar']);
 
-describe('samlang-core/errors', () => {
-  testCases.forEach(([error, expectedErrorString], index) =>
-    it(`error toString() test ${index + 1}`, () =>
-      expect(error.toString()).toBe(expectedErrorString))
-  );
+    const errors = collector
+      .getErrors()
+      .map((it) => it.toString().substring('__DUMMY__.sam:0:0-0:0: '.length));
+    expect(errors).toEqual([
+      '[SyntaxError]: bad code',
+      '[UnexpectedType]: Expected: `int`, actual: `bool`.',
+      '[UnresolvedName]: Name `global` is not resolved.',
+      "[UnsupportedClassTypeDefinition]: Expect the current class to have `object` type definition, but it doesn't.",
+      '[UnexpectedTypeKind]: Expected kind: `array`, actual: `object`.',
+      '[UnexpectedTypeKind]: Expected kind: `array`, actual: `int`.',
+      '[ArityMismatchError]: Incorrect pair size. Expected: 1, actual: 2.',
+      '[InsufficientTypeInferenceContext]: There is not enough context information to decide the type of this expression.',
+      '[Collision]: Name `a` collides with a previously defined name.',
+      "[IllegalOtherClassMatch]: It is illegal to match on a value of other class's type.",
+      '[IllegalThis]: Keyword `this` cannot be used in this context.',
+      '[NonExhausiveMatch]: The following tags are not considered in the match: [A, B].',
+      '[MissingDefinitions]: Missing definitions for [foo, bar].',
+    ]);
+  });
 });
