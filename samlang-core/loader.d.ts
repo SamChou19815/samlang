ee3fff540897c4d2630a9e37df53be0e43f1dd7f
Sam Zhou <sam@developersam.com>
2022-07-17 11:43:44 -0700

    move suppression

--- samlang-core/loader.d.ts
+++ samlang-core/loader.d.ts
@@ -1,8 +1,8 @@
 export default function samlangGeneratedWebAssemblyLoader(
   bytes: ArrayBufferView | ArrayBuffer,
-  // eslint-disable-next-line @typescript-eslint/ban-types
   builtinsPatch: (
     pointerToString: (p: number) => string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
   ) => Readonly<Record<string, Function>> = {},
   // eslint-disable-next-line @typescript-eslint/ban-types
 ): Record<string, Function>;
