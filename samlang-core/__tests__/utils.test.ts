0972c8609f49b56f3548afa697f46acbf5b8a92b
Sam Zhou <sam@developersam.com>
2022-09-18 16:22:01 -0700

    [parser][cleanup] Inline `parseClassHeader` (#877)
    There is only one call site. Not inlining it makes the logic
    complicated.

--- samlang-core/__tests__/utils.test.ts
+++ samlang-core/__tests__/utils.test.ts
@@ -12,7 +12,6 @@ import {
   LocalStackedContext,
   mapEquals,
   setEquals,
-  UnionFind,
   zip,
   zip3,
 } from '../utils';
@@ -229,60 +228,5 @@ describe('samlang-utils', () => {
     });
 
     expect(Array.from(capturedOuter.keys())).toEqual(['a', 'b']);
-  });
-
-  it('UnionFind basic methods test', () => {
-    const unionFind = new UnionFind();
-    unionFind.getParent(0);
-    unionFind.getTreeSize(1);
-    unionFind.link(2, 3);
-    unionFind.link(1, 3);
-    unionFind.link(1, 1);
-  });
-
-  it('UnionFind disjoint test', () => {
-    const unionFind = new UnionFind();
-    unionFind.link(1, 2);
-    unionFind.link(3, 4);
-    expect(unionFind.isLinked(1, 2)).toBeTruthy();
-    expect(unionFind.isLinked(2, 1)).toBeTruthy();
-    expect(unionFind.isLinked(1, 2)).toBeTruthy();
-    expect(unionFind.isLinked(3, 4)).toBeTruthy();
-    expect(unionFind.isLinked(4, 3)).toBeTruthy();
-    expect(unionFind.isLinked(1, 3)).toBeFalsy();
-    expect(unionFind.isLinked(1, 4)).toBeFalsy();
-    expect(unionFind.isLinked(3, 1)).toBeFalsy();
-    expect(unionFind.isLinked(4, 1)).toBeFalsy();
-    expect(unionFind.isLinked(2, 3)).toBeFalsy();
-    expect(unionFind.isLinked(2, 4)).toBeFalsy();
-    expect(unionFind.isLinked(3, 2)).toBeFalsy();
-    expect(unionFind.isLinked(4, 2)).toBeFalsy();
-  });
-
-  it('UnionFind chain test', () => {
-    const unionFind = new UnionFind();
-    unionFind.link(1, 2);
-    unionFind.link(3, 4);
-    unionFind.link(4, 1);
-    for (let i = 1; i <= 4; i += 1) {
-      for (let j = 1; j <= 4; j += 1) {
-        expect(unionFind.isLinked(i, j)).toBeTruthy();
-      }
-    }
-    expect(unionFind.isLinked(1, 5)).toBeFalsy();
-    expect(unionFind.isLinked(5, 1)).toBeFalsy();
-  });
-
-  it('UnionFind stress test', () => {
-    const unionFind = new UnionFind();
-    for (let i = 0; i < 10000; i += 1) {
-      unionFind.link(i, i + 1);
-    }
-    for (let i = 20000; i < 30000; i += 1) {
-      unionFind.link(i, i + 1);
-    }
-    expect(unionFind.isLinked(0, 10000)).toBeTruthy();
-    expect(unionFind.isLinked(20000, 30000)).toBeTruthy();
-    expect(unionFind.isLinked(41, 11213)).toBeFalsy();
   });
 });
