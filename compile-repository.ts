f8085d628a153a86e7a83535111f241cca8386d6
Sam Zhou <sam@developersam.com>
2022-12-30 09:43:49 -0800

    [rust-rewrite] Make integration test runnable under rust (#902)

--- compile-repository.ts
+++ compile-repository.ts
@@ -37,19 +37,35 @@ function timed(runner: () => void): number {
   return new Date().getTime() - start;
 }
 
-console.error("Bundling...");
-const bundleTime = timed(() => runWithErrorCheck("pnpm", ["bundle"]));
-console.error(`Bundled in ${bundleTime}ms!`);
-console.error("Compiling...");
-runWithErrorCheck("rm", ["-rf", basePath]);
-const compileTime = timed(() => runWithErrorCheck("./samlang-dev"));
-console.error(`Compiled in ${compileTime}ms!`);
-console.error("Checking generated TS code...");
-const r1 = runWithErrorCheck("pnpm", ["esr", path.join(basePath, "tests.AllTests.ts")]);
-if (!compare(read("./tests/snapshot.txt"), r1.resultString)) process.exit(1);
-console.error(`Generated TS code is good and takes ${r1.time}ms to run.`);
+function bundle() {
+  console.error("Compiling samlang CLI...");
+  const bundleTime = timed(() =>
+    process.env.RUST
+      ? runWithErrorCheck("pnpm", ["compile-rust"])
+      : runWithErrorCheck("pnpm", ["bundle"]),
+  );
+  console.error(`Compiled samlanng CLI in ${bundleTime}ms!`);
+}
 
-console.error("Checking generated WebAssembly code...");
-const r2 = runWithErrorCheck("node", [path.join(basePath, "tests.AllTests.wasm.js")]);
-if (!compare(read("./tests/snapshot.txt"), r2.resultString)) process.exit(1);
-console.error(`Generated WebAssembly code is good and takes ${r2.time}ms to run.`);
+function compile() {
+  console.error("Compiling samlang source code...");
+  runWithErrorCheck("rm", ["-rf", basePath]);
+  const compileTime = timed(() => runWithErrorCheck("./samlang-dev"));
+  console.error(`Compiled samlang source code in ${compileTime}ms!`);
+}
+
+function checkGeneratedCode() {
+  console.error("Checking generated TS code...");
+  const r1 = runWithErrorCheck("pnpm", ["esr", path.join(basePath, "tests.AllTests.ts")]);
+  if (!compare(read("./tests/snapshot.txt"), r1.resultString)) process.exit(1);
+  console.error(`Generated TS code is good and takes ${r1.time}ms to run.`);
+
+  console.error("Checking generated WebAssembly code...");
+  const r2 = runWithErrorCheck("node", [path.join(basePath, "tests.AllTests.wasm.js")]);
+  if (!compare(read("./tests/snapshot.txt"), r2.resultString)) process.exit(1);
+  console.error(`Generated WebAssembly code is good and takes ${r2.time}ms to run.`);
+}
+
+bundle();
+compile();
+checkGeneratedCode();
