cd6a738d08d68d955e4f305daccf9f7d1194a431
Sam Zhou <sam@developersam.com>
2023-01-01 09:36:14 -0800

    more fixes

---
+++ benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+function repeat(){
+  for ((i=0;i<$1;i++)); do
+    eval ${*:2}
+  done
+}
+
+echo "Compiling samlang CLIs..."
+pnpm bundle > /dev/null
+cargo b -p samlang-cli --release 2> /dev/null
+
+echo "Run with TS-Based CLI..."
+time repeat 10 ./samlang-dev
+echo "Run with Rust-Based CLI..."
+time RUST=1 repeat 10 ./target/release/samlang-cli
