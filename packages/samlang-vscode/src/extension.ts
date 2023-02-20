5f2b86cd7c623ecd1b18e8966f48a20a7c9d47ac
Sam Zhou <sam@developersam.com>
2023-02-20 14:40:18 -0800

    [vscode] Support restarting client (#955)
    This makes developing LSP much easier. No need to reload VSCode to restart language server.

--- packages/samlang-vscode/src/extension.ts
+++ packages/samlang-vscode/src/extension.ts
@@ -1,30 +1,43 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as vscode from "vscode";
-import { LanguageClient, TransportKind } from "vscode-languageclient/node";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node';
 
 let languageClient: LanguageClient | undefined;
 
-export function activate(): void {
-  const serverModule = vscode.workspace.getConfiguration().get("samlang.programPath");
-  if (typeof serverModule !== "string") {
+function createLanguageClient(absoluteServerModule: string) {
+  return new LanguageClient(
+    'samlang',
+    'samlang Language Client',
+    { command: absoluteServerModule, args: ['lsp'], transport: TransportKind.stdio },
+    { documentSelector: [{ scheme: 'file', language: 'samlang' }] }
+  );
+}
+
+export function activate(context: vscode.ExtensionContext): void {
+  const serverModule = vscode.workspace.getConfiguration().get('samlang.programPath');
+  if (typeof serverModule !== 'string') {
     throw new Error(`Invalid LSP program path: ${serverModule}.`);
   }
   const resolvedServerModules = (vscode.workspace.workspaceFolders ?? [])
     .map((folder) => path.join(folder.uri.fsPath, serverModule))
     .filter((it) => fs.existsSync(it));
-  if (resolvedServerModules.length > 1) throw new Error("Too many samlang LSP programs found.");
+  if (resolvedServerModules.length > 1) throw new Error('Too many samlang LSP programs found.');
   const absoluteServerModule = resolvedServerModules[0];
-  if (absoluteServerModule == null) throw new Error("No valid samlang LSP program found.");
+  if (absoluteServerModule == null) throw new Error('No valid samlang LSP program found.');
 
-  languageClient = new LanguageClient(
-    "samlang",
-    "samlang Language Client",
-    { command: absoluteServerModule, args: ["lsp"], transport: TransportKind.stdio },
-    { documentSelector: [{ scheme: "file", language: "samlang" }] },
-  );
+  languageClient = createLanguageClient(absoluteServerModule);
 
   languageClient.start();
+  context.subscriptions.push(
+    vscode.commands.registerCommand('samlang.restartClient', async () => {
+      console.info('Restarting client...');
+      await languageClient?.stop();
+      languageClient = createLanguageClient(absoluteServerModule);
+      await languageClient.start();
+      console.info('Client restarted');
+    })
+  );
   // eslint-disable-next-line no-console
   console.info('Congratulations, your extension "vscode-samlang" is now active!');
 }
@@ -36,6 +49,6 @@ export function deactivate(): void {
       // eslint-disable-next-line no-console
       .catch((error) => console.error(error))
       // eslint-disable-next-line no-console
-      .then(() => console.error("samlang client dead."));
+      .then(() => console.error('samlang client dead.'));
   }
 }
