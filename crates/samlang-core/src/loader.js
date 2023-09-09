ba8df01076508bed0d2623328d691ff40c28f640
Sam Zhou <sam@developersam.com>
2023-09-09 16:33:49 -0700

    [ci] Reduce reliance on pnpm (#1070)
    pnpm is great, but I'd like to have all the CI jobs work without any
    installs, with the exception for website builds in the future.

--- crates/samlang-core/src/loader.js
+++ crates/samlang-core/src/loader.js
@@ -1,11 +1,8 @@
-function samlangGeneratedWebAssemblyLoader(
-  /** @type {ArrayBufferView | ArrayBuffer} */ bytes,
-  builtinsPatch = () => ({})
-) {
+function samlangGeneratedWebAssemblyLoader(bytes, builtinsPatch = () => ({})) {
   const memory = new WebAssembly.Memory({ initial: 2, maximum: 65536 });
   const codeModule = new WebAssembly.Module(bytes);
 
-  function pointerToString(/** @type {number} */ p) {
+  function pointerToString(p) {
     const mem = new Uint8Array(memory.buffer);
     const length = mem[p + 4] | (mem[p + 5] << 8) | (mem[p + 6] << 16) | (mem[p + 7] << 24);
     const characterCodes = Array.from(mem.subarray(p + 8, p + 8 + length).values());
@@ -14,7 +11,6 @@ function samlangGeneratedWebAssemblyLoader(
 
   const builtins = {
     __Process$println(_, p) {
-      // eslint-disable-next-line no-console
       console.log(pointerToString(p));
       return 0;
     },
