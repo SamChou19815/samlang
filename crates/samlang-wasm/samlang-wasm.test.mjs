2317a45b443857d1dbf9e3c7f105300ec4cadef2
Sam Zhou <sam@developersam.com>
2024-01-28 11:02:20 -0800

    [wasm][test] Stop depend on bun specific test (#1179)

--- crates/samlang-wasm/samlang-wasm.test.mjs
+++ crates/samlang-wasm/samlang-wasm.test.mjs
@@ -1,14 +1,37 @@
 // @ts-check
 
-import * as samlang from './samlang-demo';
-import { test, expect } from 'bun:test';
+import * as fs from 'fs/promises';
 
-test('Program with errors fail to compile.', async () => {
+// @ts-ignore
+fetch = function (/** @type {URL} */ input) {
+  return fs.readFile(input);
+};
+await fs
+  .readFile('samlang-demo/package.json')
+  .then((text) => {
+    const json = JSON.parse(text.toString());
+    json.type = 'module';
+    return `${JSON.stringify(json, undefined, 2)}\n`;
+  })
+  .then((text) => fs.writeFile('samlang-demo/package.json', text));
+
+const samlang = await import('./samlang-demo/index.js');
+
+await test('Program with errors fail to compile.', programWithErrorsFailToCompile);
+await test('Simple programs can be interpreted.', simpleProgramsCanBeInterpreted);
+await test('Good programs have no type errors', goodProgramsHasNoTypeErrors);
+
+async function programWithErrorsFailToCompile() {
   await samlang.init();
-  expect(await samlang.compile('class')).toStartWith('Error');
-});
+  const result = await samlang.compile('class');
+  if (typeof result === 'string') {
+    assertTrue(result.startsWith('Error'));
+  } else {
+    assertTrue(false, 'Compilation result has no errors.');
+  }
+}
 
-test('Simple programs can be interpreted.', async () => {
+async function simpleProgramsCanBeInterpreted() {
   await samlang.init();
   const result = await samlang.compile(
     'class Main { function main(): unit = Process.println("Hi") }'
@@ -17,11 +40,25 @@ test('Simple programs can be interpreted.', async () => {
     throw result;
   }
   const { interpreterResult, tsCode } = result;
-  expect(interpreterResult).toBe('Hi\n');
-  expect(tsCode.length).toBeGreaterThan(0);
-});
+  assertEquals('Hi\n', interpreterResult);
+  assertTrue(tsCode.length > 0, 'Has TS code output');
+}
 
-test('Good programs have no type errors', async () => {
+async function goodProgramsHasNoTypeErrors() {
   await samlang.init();
-  expect(JSON.stringify(samlang.typeCheck('class Foo {}'))).toBe('[]');
-});
+  assertEquals('[]', JSON.stringify(samlang.typeCheck('class Foo {}')));
+}
+
+function assertEquals(/** @type {string} */ expected, /** @type {string} */ actual) {
+  assertTrue(expected === actual, `Expected:\n${expected}\nActual:\n${actual}`);
+}
+
+function assertTrue(/** @type {boolean} */ condition, /** @type {string} */ message) {
+  if (!condition) throw message;
+}
+
+async function test(/** @type {string} */ testName, /** @type {() => Promise<void>} */ f) {
+  console.error('\x1b[32m' + testName);
+  await f();
+  console.error('\x1b[32mPassing');
+}
