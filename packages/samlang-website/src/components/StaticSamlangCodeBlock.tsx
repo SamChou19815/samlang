00a8edba8e3db0fb04a4c55be6b5ff592cb95f14
Sam Zhou <sam@developersam.com>
2024-02-11 10:11:56 -0800

    [website] Adopt shiki 1.0 to enable turbopack (#1181)

--- packages/samlang-website/src/components/StaticSamlangCodeBlock.tsx
+++ packages/samlang-website/src/components/StaticSamlangCodeBlock.tsx
@@ -1,27 +1,34 @@
-import * as path from 'path';
 import * as shiki from 'shiki';
 import devSamTheme from 'dev-sam-theme/themes/dev-sam-theme.json';
+import samlangGrammar from '../../../samlang-vscode/syntaxes/samlang.json';
 
 type Props = {
   readonly language?: string;
   readonly children: string;
 };
 
+const theme: shiki.ThemeInput = {
+  ...devSamTheme,
+  name: 'dev-sam-theme',
+  fg: '#38484F',
+  bg: '#F7F7F7',
+  type: 'light',
+};
+
+const lang: shiki.LanguageRegistration = {
+  ...(samlangGrammar as any),
+  name: 'samlang',
+  scopeName: 'text.samlang',
+};
+
 export default async function StaticSamlangCodeBlock({ children }: Props): Promise<JSX.Element> {
-  const highlighter = await shiki.getHighlighter({
-    theme: { ...devSamTheme, settings: [], fg: '#38484F', bg: '#F7F7F7', type: 'light' },
-    langs: [
-      {
-        id: 'samlang',
-        scopeName: 'text.samlang',
-        path: path.join(__dirname, '../../../../samlang-vscode/syntaxes/samlang.json'),
-      },
-    ],
-  });
+  const highlighter = await shiki.getHighlighter({ themes: ['andromeeda', theme], langs: [lang] });
 
   return (
     <div
-      dangerouslySetInnerHTML={{ __html: highlighter.codeToHtml(children, { lang: 'samlang' }) }}
+      dangerouslySetInnerHTML={{
+        __html: highlighter.codeToHtml(children, { theme: 'dev-sam-theme', lang: 'samlang' }),
+      }}
     />
   );
 }
